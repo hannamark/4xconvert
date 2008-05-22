7cd02f12d8a77c0cac20f74c6917420fcc8f8a1f
harsha <harsha@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-05-22 10:44:48 +0000

    cleaned up the code to remove references to spring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@159 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/src/main/java/gov/nih/nci/coppa/pa/bo/Entity.java
+++ pa/src/main/java/gov/nih/nci/coppa/pa/bo/Entity.java
@@ -4,10 +4,6 @@ import gov.nih.nci.coppa.pa.bo.Auditable;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
-
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
  */
@@ -36,46 +32,6 @@ public abstract class Entity implements Serializable, Auditable {
         return (this.id == null);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public String toString() {
-        return new ToStringBuilder(this).append("id", getId()).toString();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-
-        if (isNew()) {
-            return System.identityHashCode(this);
-        }
-
-        return new HashCodeBuilder().append(getClass().getName()).append(getId()).hashCode();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (obj instanceof Entity == false) {
-            return false;
-        }
-        if (this == obj) {
-            return true;
-        }
-
-        if (this.isNew()) {
-            return false;
-        }
-        Entity rhs = (Entity) obj;
-        return new EqualsBuilder().append(this.getId(), rhs.getId()).isEquals();
-    }
+    
+ 
 }
--- pa/src/main/java/gov/nih/nci/coppa/pa/dao/hibernate/ProtocolServiceImpl.java
+++ pa/src/main/java/gov/nih/nci/coppa/pa/dao/hibernate/ProtocolServiceImpl.java
@@ -2,19 +2,14 @@ package gov.nih.nci.coppa.pa.dao.hibernate;
 
 import gov.nih.nci.coppa.pa.service.IProtocolService;
 
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Harsha
  */
-@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-public class ProtocolServiceImpl extends HibernateDaoSupport implements IProtocolService {
+
+public class ProtocolServiceImpl implements IProtocolService {
 
 	public void getProtocol() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
-
 }
