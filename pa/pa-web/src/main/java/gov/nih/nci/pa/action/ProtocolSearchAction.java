fc5191c395794ac19626af5ad0de74d97c0973ba
harsha <harsha@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-05-30 17:53:14 +0000

    Action class
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@225 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/main/java/gov/nih/nci/pa/action/ProtocolSearchAction.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.pa.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 
+ * @author hjayanna
+ * 
+ */
+public class ProtocolSearchAction extends ActionSupport {
+
+    /**
+     * @return action result
+     */ 
+    public String execute() {
+        return SUCCESS;
+    }
+}
