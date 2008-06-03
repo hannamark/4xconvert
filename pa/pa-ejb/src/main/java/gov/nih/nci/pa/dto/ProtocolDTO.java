637519facf316f7d8fb5599ce9e4a8bf4d2114c8
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-03 17:06:11 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@238 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/dto/ProtocolDTO.java
@@ -0,0 +1,219 @@
+package gov.nih.nci.pa.dto;
+
+import gov.nih.nci.pa.enums.SponsorMonitorCode;
+import gov.nih.nci.pa.enums.StudyPhaseCode;
+import gov.nih.nci.pa.enums.StudyStatusCode;
+import gov.nih.nci.pa.enums.StudyTypeCode;
+
+/**
+ * ProtocolDto for transferring protocol object .
+ * @author Naveen Amiruddin
+ * @since 05/22/2007
+ 
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the copyright holder, NCI.
+ */
+
+
+public class ProtocolDTO {
+    
+    private Long protocolId;
+    private String longTitleText;
+    private StudyTypeCode studyTypeCode;
+    private SponsorMonitorCode sponsorMonitorCode;
+    private StudyPhaseCode studyPhaseCode;
+    private StudyStatusCode studyStatusCode;
+    private String leadOrganizationProtocolId;
+    private String nciIdentifier;
+    private SponsorMonitorCode sponsorMonitorcode;
+    private String statusDate;
+    private String leadOrganizationName;
+    private Long leadOrganizationId;
+    private String principalInvestigatorFullName;
+
+    /**
+     * 
+     * @return protocolId
+     */
+    public Long getProtocolId() {
+        return protocolId;
+    }
+    /**
+     * 
+     * @param protocolId protocolId
+     */
+    public void setProtocolId(Long protocolId) {
+        this.protocolId = protocolId;
+    }
+    /**
+     * 
+     * @return longTitleText
+     */
+    public String getLongTitleText() {
+        return longTitleText;
+    }
+    /**
+     * 
+     * @param longTitleText longTitleText
+     */
+    public void setLongTitleText(String longTitleText) {
+        this.longTitleText = longTitleText;
+    }
+    /**
+     * 
+     * @return StudyTypeCode studyTypeCode
+     */
+    public StudyTypeCode getStudyTypeCode() {
+        return studyTypeCode;
+    }
+    /**
+     * 
+     * @param studyTypeCode studyTypeCode
+     */
+    public void setStudyTypeCode(StudyTypeCode studyTypeCode) {
+        this.studyTypeCode = studyTypeCode;
+    }
+    /**
+     * 
+     * @return sponsorMonitorCode
+     */
+    public SponsorMonitorCode getSponsorMonitorCode() {
+        return sponsorMonitorCode;
+    }
+    /**
+     * 
+     * @param sponsorMonitorCode sponsorMonitorCode
+     */
+    public void setSponsorMonitorCode(SponsorMonitorCode sponsorMonitorCode) {
+        this.sponsorMonitorCode = sponsorMonitorCode;
+    }
+    /**
+     * 
+     * @return StudyPhaseCode
+     */
+    public StudyPhaseCode getStudyPhaseCode() {
+        return studyPhaseCode;
+    }
+    /**
+     * 
+     * @param studyPhaseCode studyPhaseCode
+     */
+    public void setStudyPhaseCode(StudyPhaseCode studyPhaseCode) {
+        this.studyPhaseCode = studyPhaseCode;
+    }
+    /**
+     * 
+     * @return studyStatusCode
+     */
+    public StudyStatusCode getStudyStatusCode() {
+        return studyStatusCode;
+    }
+    /**
+     * 
+     * @param studyStatusCode studyStatusCode
+     */
+    public void setStudyStatusCode(StudyStatusCode studyStatusCode) {
+        this.studyStatusCode = studyStatusCode;
+    }
+    /**
+     * 
+     * @return leadOrganizationProtocolId
+     */
+    public String getLeadOrganizationProtocolId() {
+        return leadOrganizationProtocolId;
+    }
+    /**
+     * 
+     * @param leadOrganizationProtocolId leadOrganizationProtocolId
+     */
+    public void setLeadOrganizationProtocolId(String leadOrganizationProtocolId) {
+        this.leadOrganizationProtocolId = leadOrganizationProtocolId;
+    }
+    /**
+     * 
+     * @return String nciIdentifier
+     */
+    public String getNciIdentifier() {
+        return nciIdentifier;
+    }
+    /**
+     * 
+     * @param nciIdentifier nciIdentifier
+     */
+    public void setNciIdentifier(String nciIdentifier) {
+        this.nciIdentifier = nciIdentifier;
+    }
+    /**
+     * 
+     * @return sponsorMonitorcode
+     */
+    public SponsorMonitorCode getSponsorMonitorcode() {
+        return sponsorMonitorcode;
+    }
+    /**
+     * 
+     * @param sponsorMonitorcode sponsorMonitorcode
+     */
+    public void setSponsorMonitorcode(SponsorMonitorCode sponsorMonitorcode) {
+        this.sponsorMonitorcode = sponsorMonitorcode;
+    }
+    /**
+     * 
+     * @return statusDate
+     */
+    public String getStatusDate() {
+        return statusDate;
+    }
+    /**
+     * 
+     * @param statusDate statusDate
+     */
+    public void setStatusDate(String statusDate) {
+        this.statusDate = statusDate;
+    }
+    /**
+     * 
+     * @return leadOrganizationName
+     */
+    public String getLeadOrganizationName() {
+        return leadOrganizationName;
+    }
+    /**
+     * 
+     * @param leadOrganizationName leadOrganizationName
+     */
+    public void setLeadOrganizationName(String leadOrganizationName) {
+        this.leadOrganizationName = leadOrganizationName;
+    }
+    /**
+     * 
+     * @return principalInvestigatorFullName
+     */
+    public String getPrincipalInvestigatorFullName() {
+        return principalInvestigatorFullName;
+    }
+    /**
+     * 
+     * @param principalInvestigatorFullName principalInvestigatorFullName
+     */
+    public void setPrincipalInvestigatorFullName(
+            String principalInvestigatorFullName) {
+        this.principalInvestigatorFullName = principalInvestigatorFullName;
+    }
+    /**
+     * 
+     * @return leadOrganizationId
+     */
+    public Long getLeadOrganizationId() {
+        return leadOrganizationId;
+    }
+    /**
+     * 
+     * @param leadOrganizationId leadOrganizationId
+     */
+    public void setLeadOrganizationId(Long leadOrganizationId) {
+        this.leadOrganizationId = leadOrganizationId;
+    }
+    
+    
+}
