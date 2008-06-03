af1b4d087db67974dbe8d6de5cab67865bbe2a99
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-03 16:47:51 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@236 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/main/java/gov/nih/nci/pa/service/ProtocolSearchCriteria.java
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/service/ProtocolSearchCriteria.java
@@ -1,38 +1,139 @@
 package gov.nih.nci.pa.service;
 
+import gov.nih.nci.pa.enums.StudyTypeCode;
+import gov.nih.nci.pa.enums.StudyPhaseCode;
+import gov.nih.nci.pa.enums.StudyStatusCode;
+
 /**
  * @author Hugh
  *
  */
 public class ProtocolSearchCriteria {    
-    private String accessionNumber = "NCI_IDENTIFIER";
-    private String officialTitle = "LONG_TITLE_TEXT";
+    private Long protocolId;
+    private String nciIdentifier;
+    private String longTitleText;
+    private String leadOrganizationId;
+    private String leadOrganizationProtocolId;
+    private StudyPhaseCode studyPhaseCode;
+    private StudyStatusCode studyStatusCode;
+    private StudyTypeCode studyTypeCode;
  
+    /**
+     * @return protocolId
+     */
+    public Long getProtocolId() {
+        return protocolId;
+    }
+
+    /**
+     * @param protocolId protocolId
+     */
+    public void setProtocolId(Long protocolId) {
+        this.protocolId = protocolId;
+    }
+
     /**
      * @return accession number
      */
-    public String getAccessionNumber() {
-        return accessionNumber;
+    public String getNciIdentifier() {
+        return nciIdentifier;
     }
     
     /**
-     * @param accessionNumber accession number
+     * @param nciIdentifier nciIdentifier number
      */
-    public void setAccessionNumber(String accessionNumber) {
-        this.accessionNumber = accessionNumber;
+    public void setNciIdentifier(String nciIdentifier) {
+        this.nciIdentifier = nciIdentifier;
     }
 
     /**
      * @return title
      */
-    public String getOfficialTitle() {
-        return officialTitle;
+    public String getLongTitleText() {
+        return longTitleText;
     }
     
     /**
-     * @param officialTitle title
+     * @param longTitleText title
      */
-    public void setOfficialTitle(String officialTitle) {
-        this.officialTitle = officialTitle;
+    public void setLongTitleText(String longTitleText) {
+        this.longTitleText = longTitleText;
     }
+
+    /**
+     * @return leadOrganizationId
+     */
+    public String getLeadOrganizationId() {
+        return leadOrganizationId;
+    }
+
+    /**
+     * 
+     * @param leadOrganizationId leadOrganizationId
+     */
+    public void setLeadOrganizationId(String leadOrganizationId) {
+        this.leadOrganizationId = leadOrganizationId;
+    }
+
+    /**
+     * @return leadOrganizationProtocolId
+     */
+    public String getLeadOrganizationProtocolId() {
+        return leadOrganizationProtocolId;
+    }
+
+    /**
+     * 
+     * @param leadOrganizationProtocolId leadOrganizationProtocolId
+     */
+    public void setLeadOrganizationProtocolId(String leadOrganizationProtocolId) {
+        this.leadOrganizationProtocolId = leadOrganizationProtocolId;
+    }
+
+    /**
+     * 
+     * @return StudyPhaseCode
+     */
+    public StudyPhaseCode getStudyPhaseCode() {
+        return studyPhaseCode;
+    }
+
+    /**
+     * @param studyPhaseCode studyPhaseCode
+     */
+    public void setStudyPhaseCode(StudyPhaseCode studyPhaseCode) {
+        this.studyPhaseCode = studyPhaseCode;
+    }
+
+    /**
+     * @return studyStatusCode
+     */
+    public StudyStatusCode getStudyStatusCode() {
+        return studyStatusCode;
+    }
+
+    /**
+     * 
+     * @param studyStatusCode studyStatusCode
+     */
+    public void setStudyStatusCode(StudyStatusCode studyStatusCode) {
+        this.studyStatusCode = studyStatusCode;
+    }
+
+    /**
+     * @return studyTypeCode
+     */
+    public StudyTypeCode getStudyTypeCode() {
+        return studyTypeCode;
+    }
+
+    /**
+     * @param studyTypeCode studyTypeCode
+     */
+    public void setStudyTypeCode(StudyTypeCode studyTypeCode) {
+        this.studyTypeCode = studyTypeCode;
+    }
+    
+    
+    
 }
