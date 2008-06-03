68039085d189d6b9c4765e3d9a04b07dd2ef96dc
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-03 16:39:35 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@233 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/service/PAException.java
@@ -0,0 +1,50 @@
+package gov.nih.nci.pa.service;
+
+import org.apache.log4j.Logger;
+
+/**
+ * PA Exception for error handling.
+ * 
+ * @author Naveen Amiruddin
+ * @since 05/30/2007 copyright NCI 2007. All rights reserved. This code may not
+ *        be used without the express written permission of the copyright
+ *        holder, NCI.
+ */
+public class PAException extends Exception {
+
+    private static Logger log = Logger.getLogger(PAException.class);
+    /**
+     * no argument constructor.
+     */
+    public PAException() {
+    }
+
+    /**
+     * String constructor.
+     * @param message message
+     */
+    public PAException(String message) {
+        super(message);
+        log.error(message);
+    }
+
+    /**
+     * String and Throwable constructor.
+     * @param message message
+     * @param t t
+     */
+    public PAException(String message, Throwable t) {
+        super(message, t);
+        log.error(message, t);        
+    }
+
+    /**
+     * 
+     * @param t t
+     */
+    public PAException(Throwable t) {
+        super(t);
+        log.error(t);
+    }
+}
+
