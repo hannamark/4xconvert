0ca71f51019508f49fa7f362b19028814628cbd9
harsha <harsha@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-09 14:35:38 +0000

    Reduced the footprint of the ear file.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@262 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/main/java/gov/nih/nci/pa/action/ProtocolSearchAction.java
+++ pa/pa-web/src/main/java/gov/nih/nci/pa/action/ProtocolSearchAction.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.pa.action;
 
+import javax.naming.InitialContext;
+
+import gov.nih.nci.pa.service.IProtocolService;
 import gov.nih.nci.pa.service.ProtocolSearchCriteria;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -13,7 +16,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class ProtocolSearchAction extends ActionSupport {
     
     private ProtocolSearchCriteria srchCri = new ProtocolSearchCriteria();
-    
+    private static final long TEST = 234234; 
   
     /**
      * @return action result
@@ -27,7 +30,26 @@ public class ProtocolSearchAction extends ActionSupport {
      * @return res
      */
     public String query() {
-        
+        IProtocolService pServ = (IProtocolService) getEJB("pa/ProtocolServiceBean/local");
+        pServ.getProtocolLongTitleText(TEST);
         return SUCCESS;
     }
+    
+    /**
+     * 
+     * @param jndiName String
+     * @return object
+     */
+    public static Object getEJB(String jndiName) {
+        Object object = null;
+        try {
+            InitialContext ctx = new InitialContext();
+            object = ctx.lookup(jndiName);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            object = null;
+        }
+        return object;
+    }
+
 }
