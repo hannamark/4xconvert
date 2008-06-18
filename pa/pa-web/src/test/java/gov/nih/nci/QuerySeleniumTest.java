c83e87a89f49579fff7a00a9aacd9d6c7e045510
harsha <harsha@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-18 14:04:34 +0000

    changed the userid /pwd fields
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@356 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/test/java/gov/nih/nci/QuerySeleniumTest.java
+++ pa/pa-web/src/test/java/gov/nih/nci/QuerySeleniumTest.java
@@ -4,10 +4,10 @@ import com.thoughtworks.selenium.*;
 
 public class QuerySeleniumTest extends SeleneseTestCase  {
     public void testQuery() throws Exception {
-        super.setUp("http://localhost:8280" , "*firefox");
+        super.setUp("http://localhost:8280" , "*chrome");
         selenium.open("/pa/index.action");
-        selenium.type("authenticate_userName", "admin");
-        selenium.type("authenticate_password", "admin");
+        selenium.type("userName", "admin");
+        selenium.type("password", "admin");
         selenium.click("authenticate_0");
         selenium.waitForPageToLoad("30000");
         selenium.click("submit");
