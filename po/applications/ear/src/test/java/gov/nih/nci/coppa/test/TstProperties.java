b106c676debac4325fbc2fe22a3325bfc2639648
smatyas <smatyas@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-06-30 21:16:59 +0000

    PO-384; -peer review smiller
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@505 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ po/applications/ear/src/test/java/gov/nih/nci/coppa/test/TstProperties.java
@@ -0,0 +1,51 @@
+package gov.nih.nci.coppa.test;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+/**
+ * Environment properties passed in to tests.
+ */
+public final class TstProperties {
+
+    public static final String SERVER_HOSTNAME_KEY = "server.hostname";
+    public static final String SERVER_PORT_KEY = "server.port";
+    public static final String SERVER_JNDI_PORT_KEY = "server.jndi.port";
+
+    public static final String SERVER_HOSTNAME_DEFAULT = "localhost";
+    public static final String SERVER_PORT_DEFAULT = "8080";
+    public static final String SERVER_JNDI_PORT_DEFAULT = "1099";
+
+    public static final String SELENIUM_SERVER_PORT_KEY = "selenium.server.port";
+    public static final String SELENIUM_SERVER_PORT_DEFAULT = "4444";
+
+    private static Properties properties = new Properties();
+    static {
+        try {
+            InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("test.properties");;
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static String getServerHostname() {
+        return properties.getProperty(SERVER_HOSTNAME_KEY, SERVER_HOSTNAME_DEFAULT);
+    }
+
+    public static int getServerPort() {
+        return Integer.parseInt(properties.getProperty(SERVER_PORT_KEY, SERVER_PORT_DEFAULT));
+    }
+
+    public static int getServerJndiPort() {
+        return Integer.parseInt(properties.getProperty(SERVER_JNDI_PORT_KEY, SERVER_JNDI_PORT_DEFAULT));
+    }
+
+    public static int getSeleniumServerPort() {
+        return Integer.parseInt(properties.getProperty(SELENIUM_SERVER_PORT_KEY, SELENIUM_SERVER_PORT_DEFAULT));
+    }
+
+}
