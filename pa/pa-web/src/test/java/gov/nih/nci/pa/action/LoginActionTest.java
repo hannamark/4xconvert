ba8bfba228ab174f87419fadb436747cec73ae7f
gnaveh <gnaveh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-02 14:43:22 +0000

    Added Junit code for login page
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@533 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/test/java/gov/nih/nci/pa/action/LoginActionTest.java
@@ -0,0 +1,61 @@
+package gov.nih.nci.pa.action;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginActionTest extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final String TEST_LOGIN_CONFIG = "test.java.security.login.config";    
+	private static Properties properties = new Properties();
+    static {
+        try {
+            InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("test.properties");
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static String getLoginConfig() {
+        return properties.getProperty(TEST_LOGIN_CONFIG
+            , "Error:  java.security.auth.login.config not set in build.properties.");
+    }
+    
+	@Test
+	public void testLoginSuccess() throws Exception {
+		System.setProperty("java.security.auth.login.config", getLoginConfig());
+		LoginAction login = new LoginAction();
+
+		login.setUserName("curator");
+		login.setPassword("pass");
+		String result = login.execute();
+
+		assertTrue("Expected a success result! result is:" + result +";", result.equals(SUCCESS));
+	}
+	
+	@Test
+	public void testLoginFailure() throws Exception {
+		System.setProperty("java.security.auth.login.config", getLoginConfig());
+		
+		LoginAction login = new LoginAction();
+
+		login.setUserName("userNotExist");
+		login.setPassword("passwordNotExist");
+		String result = login.execute();
+
+		assertTrue("Expected a failure result! result is:" + result +";", result.equals(ERROR));
+	}
+
+}
