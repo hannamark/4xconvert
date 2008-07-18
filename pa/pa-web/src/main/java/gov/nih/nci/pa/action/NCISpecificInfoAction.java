32350d04afd786d299af447068efa7358b1ff5f8
gnaveh <gnaveh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-18 21:05:17 +0000

    new use case for nci specification information
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@771 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/main/java/gov/nih/nci/pa/action/NCISpecificInfoAction.java
@@ -0,0 +1,86 @@
+package gov.nih.nci.pa.action;
+
+import org.apache.log4j.Logger;
+
+import gov.nih.nci.pa.dto.NCISpecificInfoDTO;
+import gov.nih.nci.pa.service.NCISpecificInfoData;
+import gov.nih.nci.pa.util.PaRegistry;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+/**
+ * 
+ * @author gnaveh
+ *
+ */
+@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
+@Validation
+public class NCISpecificInfoAction  extends ActionSupport {
+     private static final Logger LOG  = Logger.getLogger(NCISpecificInfoAction.class);
+
+    /**
+     * 
+     */
+     private static final long serialVersionUID = 1L;
+     //TO-DO: This value should extracted from session variable
+     private static String protocolID = "2";  
+     private NCISpecificInfoDTO nciSpecificInfoDTO;
+     private NCISpecificInfoData nciSpecificInfoData = new NCISpecificInfoData();
+  
+     /**  
+     * @return res
+     */
+    public String queryCheck()  {
+        try {              
+            this.nciSpecificInfoDTO = 
+                      PaRegistry.getNCISpecificInfoService().getNCISpecificInfo(protocolID);              
+            return SUCCESS;
+        } catch (Exception e) {
+            addActionError(e.getLocalizedMessage());
+            return ERROR;
+        }
+    }
+    
+    /**  
+     * @return res
+     */
+    public String queryUpdate()  {
+        try {              
+            this.nciSpecificInfoDTO = 
+                      PaRegistry.getNCISpecificInfoService().updateNCISpecificData(nciSpecificInfoData);              
+            return SUCCESS;
+        } catch (Exception e) {
+            addActionError(e.getLocalizedMessage());
+            return ERROR;
+        }
+    }
+    
+/**
+ * 
+ * @return nciSpecificInfoDTO dto
+ */
+   public NCISpecificInfoDTO getNciSpecificInfoDTO() {
+    return nciSpecificInfoDTO;
+   }
+/**
+ * 
+ * @param nciSpecificInfoDTO ref
+ */
+  public void setNciSpecificInfoDTO(NCISpecificInfoDTO nciSpecificInfoDTO) {
+     this.nciSpecificInfoDTO = nciSpecificInfoDTO;
+  }
+  
+  /**
+   * 
+   * @return nciSpecificInfoData dto
+   */
+     public NCISpecificInfoData getNciSpecificInfoData() {
+      return nciSpecificInfoData;
+     }
+  /**
+   * 
+   * @param nciSpecificInfoData ref
+   */
+    public void setNciSpecificInfoData(NCISpecificInfoData nciSpecificInfoData) {
+       this.nciSpecificInfoData = nciSpecificInfoData;
+    }
+}
