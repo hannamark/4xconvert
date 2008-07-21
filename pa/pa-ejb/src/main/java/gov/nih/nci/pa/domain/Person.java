d074c12bbe3f9563c3f1bc1ab54c4173fb628e10
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-21 18:43:29 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@820 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/domain/Person.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.pa.domain;
+
+/**
+ * A human being. 
+ * 
+ * @author Naveen Amiruddin
+ * @since 07/07/2007
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI.
+ */
+public class Person extends AbstractEntity {
+    
+    private static final long serialVersionUID = 1234567890L;
+    
+  
+    
+    
+    
+
+}
