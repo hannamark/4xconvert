a74ed8aac0f0ce26c60cab56867f545bb2c40493
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-21 18:49:33 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@830 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/dto/QueryStudyProtocolDTO.java
@@ -0,0 +1,127 @@
+package gov.nih.nci.pa.dto;
+
+import gov.nih.nci.pa.enums.DocumentWorkflowStatusCode;
+import gov.nih.nci.pa.enums.StudyStatusCode;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * QueryStudyProtocolDto for transferring Study Protocol object .
+ * @author Naveen Amiruddin
+ * @since 07/22/2007
+ 
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the copyright holder, NCI.
+ */
+
+
+public class QueryStudyProtocolDTO implements Serializable {
+    static final long serialVersionUID = 283476876L;
+    
+    private Long studyProtocolId;
+    private String nciIdentifier;    
+    private String officialTitle;
+    private StudyStatusCode studyStatusCode;
+    private Date studyStatusDate;
+    private DocumentWorkflowStatusCode documentWorkflowStatusCode;
+    private Date documentWorkflowStatusDate;
+    /**
+     * 
+     * @return studyProtocolId
+     */
+    public Long getStudyProtocolId() {
+        return studyProtocolId;
+    }
+    /**
+     * 
+     * @param studyProtocolId studyProtocolId
+     */
+    public void setStudyProtocolId(Long studyProtocolId) {
+        this.studyProtocolId = studyProtocolId;
+    }
+    /**
+     * 
+     * @return nciIdentifier
+     */
+    public String getNciIdentifier() {
+        return nciIdentifier;
+    }
+    /**
+     * 
+     * @param nciIdentifier nciIdentifier
+     */
+    public void setNciIdentifier(String nciIdentifier) {
+        this.nciIdentifier = nciIdentifier;
+    }
+    /**
+     * 
+     * @return officialTitle
+     */
+    public String getOfficialTitle() {
+        return officialTitle;
+    }
+    /**
+     * 
+     * @param officialTitle officialTitle
+     */
+    public void setOfficialTitle(String officialTitle) {
+        this.officialTitle = officialTitle;
+    }
+    /**
+     * 
+     * @return studyStatusCode
+     */
+    public StudyStatusCode getStudyStatusCode() {
+        return studyStatusCode;
+    }
+    /**
+     * 
+     * @param studyStatusCode studyStatusCode
+     */
+    public void setStudyStatusCode(StudyStatusCode studyStatusCode) {
+        this.studyStatusCode = studyStatusCode;
+    }
+    /**
+     * 
+     * @return studyStatusDate
+     */
+    public Date getStudyStatusDate() {
+        return studyStatusDate;
+    }
+    /**
+     * 
+     * @param studyStatusDate studyStatusDate
+     */
+    public void setStudyStatusDate(Date studyStatusDate) {
+        this.studyStatusDate = studyStatusDate;
+    }
+    /**
+     * 
+     * @return documentWorkflowStatusCode
+     */
+    public DocumentWorkflowStatusCode getDocumentWorkflowStatusCode() {
+        return documentWorkflowStatusCode;
+    }
+    /**
+     * 
+     * @param documentWorkflowStatusCode documentWorkflowStatusCode
+     */
+    public void setDocumentWorkflowStatusCode(
+            DocumentWorkflowStatusCode documentWorkflowStatusCode) {
+        this.documentWorkflowStatusCode = documentWorkflowStatusCode;
+    }
+    /**
+     * 
+     * @return documentWorkflowStatusDate
+     */
+    public Date getDocumentWorkflowStatusDate() {
+        return documentWorkflowStatusDate;
+    }
+    /**
+     * 
+     * @param documentWorkflowStatusDate documentWorkflowStatusDate
+     */
+    public void setDocumentWorkflowStatusDate(Date documentWorkflowStatusDate) {
+        this.documentWorkflowStatusDate = documentWorkflowStatusDate;
+    }
+}
