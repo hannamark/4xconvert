c35a3708dce93e8b36d31856f0fcdeffe7aa62cf
gaxzero <gaxzero@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-21 21:36:46 +0000

    PO-213, increase branche coverage
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@863 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/services/src/test/java/gov/nih/nci/po/data/audit/AuditLogInterceptorTest.java
+++ po/services/src/test/java/gov/nih/nci/po/data/audit/AuditLogInterceptorTest.java
@@ -1,21 +1,28 @@
 package gov.nih.nci.po.data.audit;
 
 import gov.nih.nci.po.audit.AuditLogDetail;
+import gov.nih.nci.po.audit.AuditLogInterceptor;
 import gov.nih.nci.po.audit.AuditLogRecord;
 import gov.nih.nci.po.audit.AuditType;
+import gov.nih.nci.po.audit.Auditable;
 import gov.nih.nci.po.data.bo.Address;
 import gov.nih.nci.po.data.bo.ContactInfo;
 import gov.nih.nci.po.data.bo.Email;
 import gov.nih.nci.po.data.common.Country;
 import gov.nih.nci.po.service.AbstractHibernateTestCase;
-import gov.nih.nci.po.service.PersonServiceBeanTest;
 import gov.nih.nci.po.util.PoHibernateUtil;
-import java.util.ArrayList;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
+
+import java.util.Map;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
+import org.hibernate.collection.PersistentList;
 import org.junit.Test;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -48,9 +55,9 @@ public class AuditLogInterceptorTest extends AbstractHibernateTestCase {
        
         List<AuditLogRecord> alr = find(ContactInfo.class, ci.getId());
         //System.out.println(""+alr);
-        for (AuditLogRecord a :alr) {
-            System.out.println(a.toString());
-        }
+//        for (AuditLogRecord a :alr) {
+//            System.out.println(a.toString());
+//        }
         assertDetail(alr, AuditType.UPDATE, "email", ci.getEmail().get(0).getId().toString(), 
             ci.getEmail().get(0).getId() + ", " + ci.getEmail().get(1).getId(), true);
   
@@ -90,6 +97,69 @@ public class AuditLogInterceptorTest extends AbstractHibernateTestCase {
             }
         }
         assertTrue(false);
+    }
+    
+    @Test
+    public void onCollectionUpdate() {
+        AuditLogInterceptor interceptor = new AuditLogInterceptor();
+        interceptor.onCollectionUpdate(null, null);
+        Map m = callGetRecords(interceptor).get();
+        assertTrue(m==null || m.isEmpty());
+        
+        PersistentList dummy = new PersistentList();
+        dummy.setOwner(null);
+        interceptor.onCollectionUpdate(dummy, null);
+        m = callGetRecords(interceptor).get();
+        assertTrue(m==null || m.isEmpty());
+    }
+    
+    private static AuditLogRecord callGetOrCreateRecord(AuditLogInterceptor interceptor, Auditable entity, String entityName, Long id,  AuditType eventToLog) {
+        try {
+            Method m = AuditLogInterceptor.class.getDeclaredMethod("getOrCreateRecord", Auditable.class, String.class, Long.class, AuditType.class);
+            m.setAccessible(true);
+            return (AuditLogRecord) m.invoke(interceptor, entity, entityName, id, eventToLog);
+        } catch (NoSuchMethodException ex) {
+            throw new RuntimeException(ex);
+        } catch (SecurityException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalArgumentException ex) {
+            throw new RuntimeException(ex);
+        } catch (InvocationTargetException ex) {
+            Throwable t = ex.getTargetException();
+            if(t instanceof RuntimeException)
+                throw (RuntimeException) t;
+            throw new RuntimeException(t);
+        }
+    }
+    
+    private static ThreadLocal<Map<Object, AuditLogRecord>> callGetRecords(AuditLogInterceptor interceptor) {
+        try {
+            Field f = AuditLogInterceptor.class.getDeclaredField("records");
+            f.setAccessible(true);
+            return (ThreadLocal<Map<Object, AuditLogRecord>>) f.get(interceptor);
+        } catch (NoSuchFieldException ex) {
+            throw new RuntimeException(ex);
+        } catch (SecurityException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalArgumentException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    @Test
+    public void getOrCreateRecord() {
+        AuditLogInterceptor interceptor = new AuditLogInterceptor();
+        Auditable a = new ContactInfo();
+        String entityName = "ContactInfo";
+        Long id = null;
+        AuditType eventToLog = AuditType.UPDATE;
+        AuditLogRecord r = callGetOrCreateRecord(interceptor, a, entityName, id, eventToLog);
+        AuditLogRecord r2 = callGetOrCreateRecord(interceptor, a, entityName, id, eventToLog);
+        assertSame(r, r2);
     }
 
 }
