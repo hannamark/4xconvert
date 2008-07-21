6dcaef528a0fe8230a58e081547d898c72a1efad
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-21 18:47:00 +0000

    merged obs.StudyProtocol.java inv.StudyProtocol.java to StudyProtocol.java
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@826 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/main/java/gov/nih/nci/pa/domain/StudyProtocol.java
+++ pa/pa-ejb/src/main/java/gov/nih/nci/pa/domain/StudyProtocol.java
@@ -1,19 +1,21 @@
 package gov.nih.nci.pa.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import gov.nih.nci.pa.enums.AllocationCode;
+import gov.nih.nci.pa.enums.BiospecimenRetentionCode;
+import gov.nih.nci.pa.enums.ControlConcurrencyTypeCode;
+import gov.nih.nci.pa.enums.ControlTypeCode;
+import gov.nih.nci.pa.enums.DesignConfigurationCode;
+import gov.nih.nci.pa.enums.InterventionTypeCode;
 import gov.nih.nci.pa.enums.MonitorCode;
 import gov.nih.nci.pa.enums.PhaseCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -31,17 +33,41 @@ import javax.persistence.Table;
  * copyright holder, NCI.
  */
 @Entity
+@SuppressWarnings({"PMD.TooManyFields", "PMD.AvoidDuplicateLiterals" })
 @Table(name =  "STUDY_PROTOCOL")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type")
-@DiscriminatorValue(value = "SP")
 public class StudyProtocol extends Document {
     private static final long serialVersionUID = 1234567890L;
     
     private String acronym;
     private MonitorCode monitorCode;
     private PhaseCode phaseCode;
+    private AllocationCode allocationCode;
+    private ControlConcurrencyTypeCode controlConcurrencyTypeCode;
+    private ControlTypeCode controlTypeCode;
+    private Boolean delayedpostingIndicator;
+    private DesignConfigurationCode designConfigurationCode;
+    private Boolean fdaRegulatedIndicator;
+    private InterventionTypeCode interventionTypeCode;
+    private Integer numberOfInterventionGroups;
+    private Boolean section801Indicator;
+    
+    private String biospecimenDescription;
+    private BiospecimenRetentionCode biospecimenRetentionCode;
+
+    /** Number of study groups/cohorts. Enter 1 for a single-group study. Many observational
+     *  studies have one group/cohort; case control studies typically have two* */
+    private Integer groupNumber;
+    //@todo below two to be changed to enum, once we know the values
+    private String studyModelCode;
+    private String timePerspectiveCode;
+
+    
     private List<StudyCondition> studyConditions = new ArrayList<StudyCondition>();
+    private List<StudyOverallStatus> studyOverallStatuses = new ArrayList<StudyOverallStatus>();
+    private List<DocumentWorkflowStatus> documentWorkflowStatuses = 
+                        new ArrayList<DocumentWorkflowStatus>();
+    private List<StudyCoordinatingCenter> studyCoordinatingCenters  =
+                        new ArrayList<StudyCoordinatingCenter>(); 
     
     /**
      * 
@@ -90,6 +116,234 @@ public class StudyProtocol extends Document {
         this.phaseCode = phaseCode;
     }
     
+    
+    /**
+     * 
+     * @return allocationCode 
+     */
+    @Column(name = "ALLOCATION_CODE")
+    @Enumerated(EnumType.STRING)
+    public AllocationCode getAllocationCode() {
+        return allocationCode;
+    }
+    /**
+     * 
+     * @param allocationCode allocation Code
+     */
+    public void setAllocationCode(AllocationCode allocationCode) {
+        this.allocationCode = allocationCode;
+    }
+    
+    /**
+     * 
+     * @return ControlConcurrencyTypeCode
+     */
+    @Column(name = "CONTROL_CONCURRENCY_TYPE_CODE")
+    @Enumerated(EnumType.STRING)
+    public ControlConcurrencyTypeCode getControlConcurrencyTypeCode() {
+        return controlConcurrencyTypeCode;
+    }
+    
+    /**
+     * 
+     * @param controlConcurrencyTypeCode control ConcurrencyType Code
+     */
+    public void setControlConcurrencyTypeCode(
+            ControlConcurrencyTypeCode controlConcurrencyTypeCode) {
+        this.controlConcurrencyTypeCode = controlConcurrencyTypeCode;
+    }
+    /**
+     * 
+     * @return ControlTypeCode
+     */
+    @Column(name = "CONTROL_TYPE_CODE")
+    @Enumerated(EnumType.STRING)
+    public ControlTypeCode getControlTypeCode() {
+        return controlTypeCode;
+    }
+    /**
+     * 
+     * @param controlTypeCode controlType Code
+     */
+    public void setControlTypeCode(ControlTypeCode controlTypeCode) {
+        this.controlTypeCode = controlTypeCode;
+    }
+    
+    /**
+     * 
+     * @return delayedpostingIndicator
+     */
+    @Column(name = "DELAYED_POSTING_INDICATOR")
+    public Boolean getDelayedpostingIndicator() {
+        return delayedpostingIndicator;
+    }
+    /**
+     * 
+     * @param delayedpostingIndicator delayedposting Indicator
+     */
+    public void setDelayedpostingIndicator(Boolean delayedpostingIndicator) {
+        this.delayedpostingIndicator = delayedpostingIndicator;
+    }
+    /**
+     * 
+     * @return DesignConfigurationCode
+     */
+    @Column(name = "DESIGN_CONFIGURATION_CODE")
+    @Enumerated(EnumType.STRING)
+    public DesignConfigurationCode getDesignConfigurationCode() {
+        return designConfigurationCode;
+    }
+    /**
+     * 
+     * @param designConfigurationCode designConfiguration Code
+     */
+    public void setDesignConfigurationCode(
+            DesignConfigurationCode designConfigurationCode) {
+        this.designConfigurationCode = designConfigurationCode;
+    }
+    /**
+     * 
+     * @return fdaRegulatedIndicator
+     */
+    @Column(name = "FDA_REGULATED_INDICATOR")
+    public Boolean getFdaRegulatedIndicator() {
+        return fdaRegulatedIndicator;
+    }
+    /**
+     * 
+     * @param fdaRegulatedIndicator fdaRegulatedIndicator
+     */
+    public void setFdaRegulatedIndicator(Boolean fdaRegulatedIndicator) {
+        this.fdaRegulatedIndicator = fdaRegulatedIndicator;
+    }
+    /**
+     * 
+     * @return InterventionTypeCode
+     */
+    @Column(name = "INTERVENTION_TYPE_CODE")
+    @Enumerated(EnumType.STRING)
+    public InterventionTypeCode getInterventionTypeCode() {
+        return interventionTypeCode;
+    }
+    /**
+     * 
+     * @param interventionTypeCode interventionType Code
+     */
+    public void setInterventionTypeCode(InterventionTypeCode interventionTypeCode) {
+        this.interventionTypeCode = interventionTypeCode;
+    }
+    
+    /**
+     * 
+     * @return numberOfInterventionGroups
+     */
+    @Column(name = "NUMBER_OF_INTERVENTION_GROUPS")
+    public Integer getNumberOfInterventionGroups() {
+        return numberOfInterventionGroups;
+    }
+    
+    /**
+     * 
+     * @param numberOfInterventionGroups numberOfInterventionGroups
+     */
+    public void setNumberOfInterventionGroups(Integer numberOfInterventionGroups) {
+        this.numberOfInterventionGroups = numberOfInterventionGroups;
+    }
+    
+    /**
+     * 
+     * @return section801Indicator
+     */
+    @Column(name = "SECTION801_INDICATOR")
+    public Boolean getSection801Indicator() {
+        return section801Indicator;
+    }
+    /**
+     * 
+     * @param section801Indicator section801Indicator
+     */
+    public void setSection801Indicator(Boolean section801Indicator) {
+        this.section801Indicator = section801Indicator;
+    }    
+    
+    /**
+     * 
+     * @return biospecimenDescription
+     */
+    @Column(name = "BIOSPECIMEN_DESCRIPTION")
+    public String getBiospecimenDescription() {
+        return biospecimenDescription;
+    }
+    /**
+     * 
+     * @param biospecimenDescription biospecimenDescription
+     */
+    public void setBiospecimenDescription(String biospecimenDescription) {
+        this.biospecimenDescription = biospecimenDescription;
+    }
+    /**
+     * 
+     * @return biospecimenRetentionCode
+     */
+    @Column(name = "BIOSPECIMEN_RETENTION_CODE")
+    @Enumerated(EnumType.STRING)
+    public BiospecimenRetentionCode getBiospecimenRetentionCode() {
+        return biospecimenRetentionCode;
+    }
+    /**
+     * 
+     * @param biospecimenRetentionCode biospecimenRetentionCode
+     */
+    public void setBiospecimenRetentionCode(
+            BiospecimenRetentionCode biospecimenRetentionCode) {
+        this.biospecimenRetentionCode = biospecimenRetentionCode;
+    }
+    /**
+     * 
+     * @return groupNumber
+     */
+    @Column(name = "GROUP_NUMBER")
+    public Integer getGroupNumber() {
+        return groupNumber;
+    }
+    /**
+     * 
+     * @param groupNumber Number of study groups/cohorts. 
+     */
+    public void setGroupNumber(Integer groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+    /**
+     * 
+     * @return studyModelCode
+     */
+    @Column(name = "STUDY_MODEL_CODE")
+    public String getStudyModelCode() {
+        return studyModelCode;
+    }
+    /**
+     * 
+     * @param studyModelCode primary strategy for subject identification and follow-up
+     */
+    public void setStudyModelCode(String studyModelCode) {
+        this.studyModelCode = studyModelCode;
+    }
+    /**
+     * 
+     * @return timePerspectiveCode
+     */
+    @Column(name = "TIME_PRESPECTIVE_CODE")
+    public String getTimePerspectiveCode() {
+        return timePerspectiveCode;
+    }
+    /**
+     * 
+     * @param timePerspectiveCode temporal relationship of observation period to time of subject enrollment
+     */
+    public void setTimePerspectiveCode(String timePerspectiveCode) {
+        this.timePerspectiveCode = timePerspectiveCode;
+    }
+
     /**
      * 
      * @return studyConditions
@@ -106,5 +360,58 @@ public class StudyProtocol extends Document {
     public void setStudyConditions(List<StudyCondition> studyConditions) {
         this.studyConditions = studyConditions;
     }
+    
+    /**
+     * 
+     * @return studyOverallStatuses
+     */
+    @OneToMany(mappedBy = "studyProtocol")
+    public List<StudyOverallStatus> getStudyOverallStatuses() {
+        return studyOverallStatuses;
+    }
+    /**
+     * 
+     * @param studyOverallStatuses studyOverallStatuses
+     */
+    public void setStudyOverallStatuses(
+            List<StudyOverallStatus> studyOverallStatuses) {
+        this.studyOverallStatuses = studyOverallStatuses;
+    }
+    /**
+     * 
+     * @return documentWorkflowStatuses documentWorkflowStatuses
+     */
+    @OneToMany(mappedBy = "studyProtocol")
+    public List<DocumentWorkflowStatus> getDocumentWorkflowStatuses() {
+        return documentWorkflowStatuses;
+    }
+    /**
+     * 
+     * @param documentWorkflowStatuses documentWorkflowStatuses
+     */
+    public void setDocumentWorkflowStatuses(
+            List<DocumentWorkflowStatus> documentWorkflowStatuses) {
+        this.documentWorkflowStatuses = documentWorkflowStatuses;
+    }
+    
+    /**
+     * 
+     * @return studyCoordinatingCenters
+     */
+    @OneToMany(mappedBy = "studyProtocol")
+    public List<StudyCoordinatingCenter> getStudyCoordinatingCenters() {
+        return studyCoordinatingCenters;
+    }
+    /**
+     * 
+     * @param studyCoordinatingCenters studyCoordinatingCenters
+     */
+    public void setStudyCoordinatingCenters(
+            List<StudyCoordinatingCenter> studyCoordinatingCenters) {
+        this.studyCoordinatingCenters = studyCoordinatingCenters;
+    }
+    
+    
+    
     
 }
