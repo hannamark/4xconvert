4bd72d6b4404895e8e9e5d3b55a4fea02466ca89
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-23 15:27:31 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@901 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/main/java/gov/nih/nci/pa/action/StudyProtocolQueryAction.java
@@ -0,0 +1,69 @@
+package gov.nih.nci.pa.action;
+
+import gov.nih.nci.pa.dto.StudyProtocolQueryCriteria;
+import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+import gov.nih.nci.pa.util.PaRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+
+/**
+ * 
+ * @author Harsha
+ * 
+ */
+@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
+@Validation
+public class StudyProtocolQueryAction extends ActionSupport {    
+    
+    private List<StudyProtocolQueryDTO> records = new ArrayList<StudyProtocolQueryDTO>();
+    
+    private StudyProtocolQueryCriteria criteria = new StudyProtocolQueryCriteria();
+
+    /**  
+     * @return res
+     */ 
+    public String execute() {
+        return SUCCESS;
+    }
+    
+    /**  
+     * @return res
+     */
+    public String query()  {
+        try {            
+            records = PaRegistry.getStudyProtocolService().getStudyProtocolByCriteria(criteria);       
+            return SUCCESS;
+        } catch (Exception e) {
+            addActionError(e.getLocalizedMessage());
+            return ERROR;
+        }
+    }
+    /**
+     * 
+     * @return records
+     */
+    public List<StudyProtocolQueryDTO>  getRecords() {
+        return records;
+    }
+
+    /**
+     * 
+     * @return StudyProtocolQueryCriteria StudyProtocolQueryCriteria
+     */
+    public StudyProtocolQueryCriteria getCriteria() {
+        return criteria;
+    }
+    
+    /**
+     * 
+     * @param criteria StudyProtocolQueryCriteria
+     */
+    public void setCriteria(StudyProtocolQueryCriteria criteria) {
+        this.criteria = criteria;
+    }
+
+}
