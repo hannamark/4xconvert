d8c2b5ee496708c8ff26f59b8ebbef6c5ec44979
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-23 22:19:08 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@935 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/test/java/gov/nih/nci/pa/service/impl/StudyProtocolServiceImplTest.java
@@ -0,0 +1,43 @@
+package gov.nih.nci.pa.service.impl;
+
+import static org.junit.Assert.assertNotNull;
+import gov.nih.nci.pa.domain.StudyProtocol;
+import gov.nih.nci.pa.domain.StudyProtocolTest;
+import gov.nih.nci.pa.dto.StudyProtocolQueryCriteria;
+import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+import gov.nih.nci.pa.service.PAException;
+import gov.nih.nci.pa.test.util.TestSchema;
+
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyProtocolServiceImplTest {
+    
+    @Before
+    public void setUp() throws Exception {
+        TestSchema.reset();
+               
+    }
+    
+    @Test
+    public void getStudyProtocolByCriteriaTest() throws PAException {
+        List<Object> obj = null ;
+        StudyProtocol sp = StudyProtocolTest.createStudyProtocolObj();
+        TestSchema.addUpdObject(sp);
+        Long id = sp.getId();
+        
+        StudyProtocolServiceImpl spsImpl = new StudyProtocolServiceImpl();
+        StudyProtocolQueryCriteria spqc = new StudyProtocolQueryCriteria();
+        spqc.setStudyProtocolId(id);
+        List<StudyProtocolQueryDTO> spDtos = spsImpl.getStudyProtocolByCriteria(spqc); 
+        assertNotNull(spDtos);
+        assertEquals(" size of StudyProtocolQueryDTO does not match " , spDtos.size() , 1 );
+        assertEquals(" protocol title  does not match " , spDtos.get(0).getOfficialTitle() , sp.getOfficialTitle() );
+        
+    }
+    
+    
+}
+
