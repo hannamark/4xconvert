54459860219fd8cdd04ade858412fdcf8342aacb
gnaveh <gnaveh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-30 17:07:51 +0000

    changed name info to information convention + other changes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1070 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
+++ pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
@@ -2,7 +2,7 @@ package gov.nih.nci.pa.action;
 
 import org.apache.log4j.Logger;
 
-import gov.nih.nci.pa.dto.NCISpecificInfoDTO;
+import gov.nih.nci.pa.dto.NCISpecificInformationDTO;
 import gov.nih.nci.pa.dto.NCISpecificInformationData;
 import gov.nih.nci.pa.util.PaRegistry;
 import com.opensymphony.xwork2.ActionSupport;
@@ -22,19 +22,23 @@ public class NCISpecificInformationAction  extends ActionSupport {
      */
      private static final long serialVersionUID = 1L;
      //TO-DO: This value should extracted from session variable
-     private Long studyProtocolID = 2L;  
-     private NCISpecificInfoDTO nciSpecificInfoDTO;
+     private Long studyProtocolID;  
+     private NCISpecificInformationDTO nciSpecificInformationDTO;
      private NCISpecificInformationData nciSpecificInformationData = new NCISpecificInformationData();
   
      /**  
      * @return res
      */
     public String query()  {
-        try {              
-            this.nciSpecificInfoDTO = 
-                      PaRegistry.getNCISpecificInformationService().getNCISpecificInfo(studyProtocolID);
-            LOG.info("1.) nciSpecificInfoDTO.getStudyProtocolID is: " + nciSpecificInfoDTO.getStudyProtocolID());
-            LOG.info("2.) nciSpecificInfoDTO.getMonitorCode is: " + nciSpecificInfoDTO.getMonitorCode());
+        try {
+            studyProtocolID = 2L; //get the protocol ID from session
+            this.nciSpecificInformationDTO = 
+                      PaRegistry.getNCISpecificInformationService().getNCISpecificInformation(
+                                                                         studyProtocolID);
+            LOG.info("1.) nciSpecificInformationDTO.getStudyProtocolID is: " 
+                                            + nciSpecificInformationDTO.getStudyProtocolID());
+            LOG.info("2.) nciSpecificInformationDTO.getMonitorCode is: " 
+                                            + nciSpecificInformationDTO.getMonitorCode());
             return SUCCESS;
         } catch (Exception e) {
             addActionError(e.getLocalizedMessage());
@@ -47,8 +51,9 @@ public class NCISpecificInformationAction  extends ActionSupport {
      */
     public String update()  {
         try {              
-            this.nciSpecificInfoDTO = 
-         PaRegistry.getNCISpecificInformationService().updateNCISpecificInfo(nciSpecificInformationData);
+            this.nciSpecificInformationDTO = 
+         PaRegistry.getNCISpecificInformationService().updateNCISpecificInformation(
+                                nciSpecificInformationData);
             
             
             return SUCCESS;
@@ -60,17 +65,17 @@ public class NCISpecificInformationAction  extends ActionSupport {
     
 /**
  * 
- * @return nciSpecificInfoDTO dto
+ * @return nciSpecificInformationDTO dto
  */
-   public NCISpecificInfoDTO getNciSpecificInfoDTO() {
-    return nciSpecificInfoDTO;
+   public NCISpecificInformationDTO getNciSpecificInformationDTO() {
+    return nciSpecificInformationDTO;
    }
 /**
  * 
- * @param nciSpecificInfoDTO ref
+ * @param nciSpecificInformationDTO ref
  */
-  public void setNciSpecificInfoDTO(NCISpecificInfoDTO nciSpecificInfoDTO) {
-     this.nciSpecificInfoDTO = nciSpecificInfoDTO;
+  public void setNciSpecificInformationDTO(NCISpecificInformationDTO nciSpecificInformationDTO) {
+     this.nciSpecificInformationDTO = nciSpecificInformationDTO;
   }
   
   /**
