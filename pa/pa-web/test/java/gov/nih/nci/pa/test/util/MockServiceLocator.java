20217e7ec558c768099197852a6a0f9c425a39e9
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-30 15:48:28 +0000

    added a new ejb
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1064 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/test/java/gov/nih/nci/pa/test/util/MockServiceLocator.java
+++ pa/pa-web/test/java/gov/nih/nci/pa/test/util/MockServiceLocator.java
@@ -2,7 +2,9 @@ package gov.nih.nci.pa.test.util;
 
 import gov.nih.nci.pa.service.DiseaseCondServiceLocal;
 import gov.nih.nci.pa.service.NCISpecificInformationServiceRemote;
+import gov.nih.nci.pa.service.PAPersonServiceRemote;
 import gov.nih.nci.pa.service.ProtocolOrganizationServiceRemote;
+
 import gov.nih.nci.pa.service.StudyProtocolServiceRemote;
 import gov.nih.nci.pa.util.ServiceLocator;
 import gov.nih.nci.service.MockDiseaseConditionService;
@@ -12,30 +14,45 @@ import gov.nih.nci.service.MockDiseaseConditionService;
  * @author Harsha
  *
  */
-public class MockServiceLocator implements ServiceLocator{
+public class MockServiceLocator implements ServiceLocator {
     
     private final DiseaseCondServiceLocal condServiceLocal = new MockDiseaseConditionService();
 
+    /**
+     * @return condServiceLocal
+     */
     public DiseaseCondServiceLocal getDiseaseConditionService() {
-        
         return condServiceLocal;
     }
 
+    /**
+     * @return null
+     */
     public ProtocolOrganizationServiceRemote getProtocolOrganizationService() {
-      
         return null;
     }
 
+    /**
+     * @return null
+     */
     public StudyProtocolServiceRemote getStudyProtocolService() {
-      
         return null;
     }
 
 
-    
+    /**
+     * @return null
+     */
    public NCISpecificInformationServiceRemote getNCISpecificInformationService() {
-        
         return null;
     }
+
+   /**
+   *
+   * @return PAPersonServiceRemote
+   */
+   public PAPersonServiceRemote getPAPersonService() {
+       return null;
+   }
 
 }
