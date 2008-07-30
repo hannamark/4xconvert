6210fdc84beb6d3f7dd50c049e424be711f02fcd
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-07-30 15:42:30 +0000

    added new enums
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1058 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/enums/PrimaryPurposeCode.java
@@ -0,0 +1,87 @@
+package gov.nih.nci.pa.enums;
+
+import static gov.nih.nci.pa.enums.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.pa.enums.CodedEnumHelper.register;
+import static gov.nih.nci.pa.enums.EnumHelper.sentenceCasedName;
+/**
+ * Code to identify a type of study protocol based upon the intent of the study's activities. 
+ *
+ * @author Naveen Amiruddin
+ * @since 07/22/2008
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI.
+ */
+
+public enum PrimaryPurposeCode implements CodedEnum<String> {
+
+    /** Treatment. */
+    TREATMENT("Treatment"),
+    /** Prevention. */
+    PREVENTION("Prevention"),
+    /** Diagnostic. */
+    DIAGNOSTIC("Diagnostic"),
+    /** Supportive Care. */
+    SUPPORTIVE_CARE("Supportive Care"),
+    /** Screening. */
+    SCREENING("Screening"),
+    /** Health Services Research. */
+    HEALTH_SERVICES_RESEARCH("Health Services Research"),
+    /** Basic Science. */
+    BASIC_SCIENCE("Basic Science"),
+    /** Other. */
+    OTHER("Other");
+
+    private String code;
+    /**
+     *
+     * @param code
+     */
+    private PrimaryPurposeCode(String code) {
+        this.code = code;
+        register(this);
+    }
+    /**
+     * @return code code
+     */
+    public String getCode() {
+        return code;
+    }
+    /**
+    *@return String DisplayName
+    */
+    public String getDisplayName() {
+        return sentenceCasedName(this);
+    }
+    /**
+     *
+     * @return String name
+     */
+    public String getName() {
+        return name();
+    }
+
+    /**
+     *
+     * @param code code
+     * @return ResponsibilityCode
+     */
+    public static PrimaryPurposeCode getByCode(String code) {
+        return getByClassAndCode(PrimaryPurposeCode.class, code);
+    }
+
+    /**
+     * @return String[] display names of enums
+     */
+    public static String[]  getDisplayNames() {
+        PrimaryPurposeCode[] l = PrimaryPurposeCode.values();
+        String[] a = new String[l.length];
+        for (int i = 0; i < l.length; i++) {
+            a[i] = l[i].getCode();
+        }
+        return a;
+    }
+}
+
+
+
