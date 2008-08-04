92905594b0c0e04d7a0bd0d5eb8d2adbfb400505
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-08-04 17:44:18 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1159 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/java/gov/nih/nci/pa/action/StudyProtocolViewAction.java
@@ -0,0 +1,58 @@
+package gov.nih.nci.pa.action;
+
+import org.apache.struts2.ServletActionContext;
+
+import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+import gov.nih.nci.pa.service.PAException;
+import gov.nih.nci.pa.util.Constants;
+import gov.nih.nci.pa.util.PaRegistry;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Action class for viewing study protocol.
+ *
+ * @author Naveen Amiruddin
+ * @since 07/09/2007
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI.
+ */
+public class StudyProtocolViewAction extends ActionSupport {   
+    
+    private Long studyProtocolId = null;
+    
+    /**
+     * 
+     * @return studyProtocolId
+     */
+    public Long getStudyProtocolId() {
+        return studyProtocolId;
+    }
+
+    /**
+     * 
+     * @param studyProtocolId studyProtocolId
+     */
+    public void setStudyProtocolId(Long studyProtocolId) {
+        this.studyProtocolId = studyProtocolId;
+    }
+
+    /**  
+     * @return res
+     */
+    public String getStudyProtocol() {
+        try {            
+            StudyProtocolQueryDTO  studyProtocolQueryDTO = 
+                PaRegistry.getStudyProtocolService().getTrialSummaryByStudyProtocolId(studyProtocolId);
+            // put an entry in the session and store StudyProtocolQueryDTO 
+            ServletActionContext.getRequest().getSession().setAttribute(Constants.TRIAL_SUMMARY, studyProtocolQueryDTO);
+            return SUCCESS;
+        } catch (PAException e) {
+            addActionError(e.getLocalizedMessage());
+            return ERROR;
+        }        
+    }
+
+
+}
