5072f1caa5e8795ad80b7aa46e0c178d84a3e64f
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-08-29 05:24:17 +0000

    added a utility method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1486 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/util/PAUtil.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/util/PAUtil.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.pa.util;
 
+import gov.nih.nci.coppa.iso.Ii;
+
 
 /**
  * Protocol DAO for accessing DAO.
@@ -31,6 +33,28 @@ public class PAUtil {
             return true;
         }
         return false;
+    }
+
+    /**
+     * checks if Ii is null.
+     * @param ii ii
+     * @return boolean
+     */
+    public static boolean isIiNull(Ii ii) { 
+        boolean isNull = false;
+        if (ii == null || ii.getExtension() == null) {
+            isNull = true;
+        }
+        if (ii.getExtension().trim().length() == 0) {
+            isNull = true;
+        }
+        try {
+           new Long(ii.getExtension());
+        } catch (NumberFormatException  nfe) {
+            // if cannot be converted, consider as null
+            isNull = true;
+        }
+        return isNull;
     }
 
 }
