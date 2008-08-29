57671859b025299fa5516404aa04acdc1ec43bb7
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-08-29 05:26:55 +0000

    added iso utils
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1489 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
+++ pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
@@ -3,10 +3,20 @@ package gov.nih.nci.pa.action;
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 
-import gov.nih.nci.pa.dto.NCISpecificInformationDTO;
-import gov.nih.nci.pa.dto.NCISpecificInformationData;
+import gov.nih.nci.coppa.iso.Ii;
+
+
+import gov.nih.nci.pa.dto.StudyProtocolDTO;
 import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+//import gov.nih.nci.pa.service.PAException;
+import gov.nih.nci.pa.service.PAException;
 import gov.nih.nci.pa.util.Constants;
+import gov.nih.nci.pa.dto.NCISpecificInformationWebDTO;
+import gov.nih.nci.pa.enums.AccrualReportingMethodCode;
+import gov.nih.nci.pa.enums.MonitorCode;
+import gov.nih.nci.pa.enums.SummaryFourFundingCategoryCode;
+import gov.nih.nci.pa.util.IsoConverter;
+import gov.nih.nci.pa.util.PAUtil;
 import gov.nih.nci.pa.util.PaRegistry;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.validator.annotations.Validation;
@@ -15,99 +25,139 @@ import com.opensymphony.xwork2.validator.annotations.Validation;
  * @author gnaveh
  *
  */
-@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
-@Validation
-public class NCISpecificInformationAction  extends ActionSupport {
-     private static final Logger LOG  = Logger.getLogger(NCISpecificInformationAction.class);
 
-    /**
-     * 
-     */
-     private static final long serialVersionUID = 1L;
-     //TO-DO: This value should extracted from session variable
-     private Long studyProtocolID;  
-     private NCISpecificInformationDTO nciSpecificInformationDTO;
-     private NCISpecificInformationData nciSpecificInformationData = new NCISpecificInformationData();
+@Validation
+@SuppressWarnings({"PMD.CyclomaticComplexity" })
+
+public class NCISpecificInformationAction  extends ActionSupport {
+
+    private static final Logger LOG  = Logger.getLogger(NCISpecificInformationAction.class);
+    private NCISpecificInformationWebDTO nciSpecificInformationWebDTO = 
+            new NCISpecificInformationWebDTO();
   
      /**  
-     * @return res
+     * @return result
      */
     public String query()  {
+        LOG.info("Entering query");
+        String ret = null;
         try {
-            StudyProtocolQueryDTO spDTO =  (StudyProtocolQueryDTO)
-                ServletActionContext.getRequest().getSession().getAttribute(Constants.TRIAL_SUMMARY);
-
-            studyProtocolID = spDTO.getStudyProtocolId();
-            this.nciSpecificInformationDTO =
-                      PaRegistry.getNCISpecificInformationService().getNCISpecificInformation(
-                                                                         studyProtocolID);
-            LOG.info("1.) nciSpecificInformationDTO.getStudyProtocolID is: " 
-                                            + nciSpecificInformationDTO.getStudyProtocolID());
-            LOG.info("2.) nciSpecificInformationDTO.getMonitorCode is: " 
-                                            + nciSpecificInformationDTO.getMonitorCode());
-
-            if (nciSpecificInformationDTO.getMonitorCode() != null) {
-               nciSpecificInformationData.setMonitorCode(nciSpecificInformationDTO.getMonitorCode().getCode());
-            }
-            if (nciSpecificInformationDTO.getSummaryFourFundingCategoryCode() != null) {
-                nciSpecificInformationData.setSummaryFourFundingCategoryCode(
-                         nciSpecificInformationDTO.getSummaryFourFundingCategoryCode().getCode());
-            }
-            if (nciSpecificInformationDTO.getReportingDataSetMethodCode() != null) {
-                nciSpecificInformationData.setReportingDataSetMethodCode(
-                          nciSpecificInformationDTO.getReportingDataSetMethodCode().getCode());
-            }
-            return SUCCESS;
+            nciSpecificInformationWebDTO = setNciSpDto(getStudyProtocol());
+            ret = SUCCESS;    
+            
         } catch (Exception e) {
             addActionError(e.getLocalizedMessage());
-            return ERROR;
+            ret = ERROR;
         }
+        LOG.info("Leaving query");
+        return ret;
     }
     
     /**  
      * @return res
      */
     public String update()  {
-        try {              
-            this.nciSpecificInformationDTO = 
-         PaRegistry.getNCISpecificInformationService().updateNCISpecificInformation(
-                                nciSpecificInformationData);
-            
-            
-            return SUCCESS;
-        } catch (Exception e) {
-            addActionError(e.getLocalizedMessage());
+        boolean error = false;
+        //Step1 : check for any errors
+        //@todo: perform this error only for Interventional trial type, currently trial type
+        //is not yet added to the code
+        
+        //if (true) {
+        if (!PAUtil.isNotNullOrNotEmpty(nciSpecificInformationWebDTO.getMonitorCode())) {
+            addActionError(getText("error.studyProtocol.monitorCode"));
+            error = true;
+        }
+        if (!PAUtil.isNotNullOrNotEmpty(nciSpecificInformationWebDTO.getAccrualReportingMethodCode())) {
+            addActionError(getText("error.studyProtocol.accrualReportingMethodCode"));
+            error = true;
+        }
+        if (!PAUtil.isNotNullOrNotEmpty(nciSpecificInformationWebDTO.getSummaryFourFundingCategoryCode())) {
+            addActionError(getText("error.studyProtocol.summaryFourFundingCategoryCode"));
+            error = true;
+        }
+        if (error) {
             return ERROR;
+        }
+         
+//        }
+        
+        //Step2 : retrieve the studyprotocol
+        StudyProtocolDTO spIsoDTO = null;
+        try {
+             spIsoDTO = getStudyProtocol();
+             //Step3: overwrite the new values
+             spIsoDTO.setMonitorCode(IsoConverter.convertEnumCodeToIsoCd(
+                     MonitorCode.getByCode(nciSpecificInformationWebDTO.getMonitorCode())));
+             spIsoDTO.setAccrualReportingMethodCode(IsoConverter.convertEnumCodeToIsoCd(
+                     AccrualReportingMethodCode.getByCode(
+                             nciSpecificInformationWebDTO.getAccrualReportingMethodCode())));
+             spIsoDTO.setSummaryFourFundingCategoryCode(IsoConverter.convertEnumCodeToIsoCd(
+                     SummaryFourFundingCategoryCode.getByCode(
+                             nciSpecificInformationWebDTO.getSummaryFourFundingCategoryCode())));
+             //Step4: update studyprotocol 
+             spIsoDTO = PaRegistry.getStudyProtocolService().updateStudyProtocol(spIsoDTO);
+        } catch (Exception e) {
+            addActionError(e.getMessage());
+            return ERROR;
+        }    
+        
+        nciSpecificInformationWebDTO = setNciSpDto(spIsoDTO);
+        return SUCCESS;
+    }
+
+    /**
+     * 
+     * @return nciSpecificInformationWebDTO
+     */
+    public NCISpecificInformationWebDTO getNciSpecificInformationWebDTO() {
+        return nciSpecificInformationWebDTO;
+    }
+
+    /**
+     * 
+     * @param nciSpecificInformationWebDTO nciSpecificInformationWebDTO
+     */
+    public void setNciSpecificInformationWebDTO(
+            NCISpecificInformationWebDTO nciSpecificInformationWebDTO) {
+        this.nciSpecificInformationWebDTO = nciSpecificInformationWebDTO;
+    }
+    
+    //@todo : catch and throw paexception
+    private StudyProtocolDTO getStudyProtocol()  {
+        StudyProtocolQueryDTO spDTO =  (StudyProtocolQueryDTO)
+        ServletActionContext.getRequest().getSession().getAttribute(Constants.TRIAL_SUMMARY);
+        Long studyProtocolId = spDTO.getStudyProtocolId();
+        
+        // Step2 : convert from long to Ii
+        Ii ii = IsoConverter.convertIdToIsoIi(studyProtocolId);
+    
+        // Step3 : get the StudyProtocol Remote interface and call getStudyProtocol
+        try {
+            return PaRegistry.getStudyProtocolService().getStudyProtocol(ii);
+        } catch (PAException e) {
+            return null;
         }
     }
     
-/**
- * 
- * @return nciSpecificInformationDTO dto
- */
-   public NCISpecificInformationDTO getNciSpecificInformationDTO() {
-    return nciSpecificInformationDTO;
-   }
-/**
- * 
- * @param nciSpecificInformationDTO ref
- */
-  public void setNciSpecificInformationDTO(NCISpecificInformationDTO nciSpecificInformationDTO) {
-     this.nciSpecificInformationDTO = nciSpecificInformationDTO;
-  }
-  
-  /**
-   * 
-   * @return nciSpecificInformationData dto
-   */
-     public NCISpecificInformationData getNciSpecificInformationData() {
-      return nciSpecificInformationData;
-     }
-  /**
-   * 
-   * @param nciSpecificInformationData ref
-   */
-    public void setNciSpecificInformationData(NCISpecificInformationData nciSpecificInformationData) {
-       this.nciSpecificInformationData = nciSpecificInformationData;
+    private NCISpecificInformationWebDTO setNciSpDto(StudyProtocolDTO spIsoDTO) {
+        NCISpecificInformationWebDTO nciSpDTO = new NCISpecificInformationWebDTO();
+        
+        // Step2 : Assign the values to the action form
+        if (spIsoDTO.getAccrualReportingMethodCode() != null) {
+            nciSpDTO.setAccrualReportingMethodCode(
+                spIsoDTO.getAccrualReportingMethodCode().getCode());
+        }
+        if (spIsoDTO.getMonitorCode() != null) {
+            nciSpDTO.setMonitorCode(spIsoDTO.getMonitorCode().getCode());
+        }
+        if (spIsoDTO.getSummaryFourFundingCategoryCode() != null) {
+            nciSpDTO.setSummaryFourFundingCategoryCode(
+                    spIsoDTO.getSummaryFourFundingCategoryCode().getCode());
+        }
+        
+        return nciSpDTO;
+        
     }
+
+
 }
