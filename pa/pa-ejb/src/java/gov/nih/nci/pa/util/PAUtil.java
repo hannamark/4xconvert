3b95916d84c583e32ad3c75a849cd279c2f1d182
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-02 12:33:31 +0000

    Add PAUtil method for normalizing date strings.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1522 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/util/PAUtil.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/util/PAUtil.java
@@ -2,6 +2,9 @@ package gov.nih.nci.pa.util;
 
 import gov.nih.nci.coppa.iso.Ii;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Protocol DAO for accessing DAO.
@@ -27,7 +30,7 @@ public class PAUtil {
             String s = obj.toString();
             if (s != null && s.trim().length() > 0) {
                 return true;
-            } 
+            }
             return false;
         } else if (obj != null) {
             return true;
@@ -40,7 +43,7 @@ public class PAUtil {
      * @param ii ii
      * @return boolean
      */
-    public static boolean isIiNull(Ii ii) { 
+    public static boolean isIiNull(Ii ii) {
         boolean isNull = false;
         if (ii == null || ii.getExtension() == null) {
             isNull = true;
@@ -49,12 +52,69 @@ public class PAUtil {
             isNull = true;
         }
         try {
-           new Long(ii.getExtension());
-        } catch (NumberFormatException  nfe) {
+            new Long(ii.getExtension());
+        } catch (NumberFormatException nfe) {
             // if cannot be converted, consider as null
             isNull = true;
         }
         return isNull;
     }
 
+    /**
+     * Private class used to decode and normalize date strings.
+     */
+    private static class ValidDateFormat {
+        String pattern;
+        int endIndex;
+
+        public ValidDateFormat(String pattern) {
+            this.pattern = pattern;
+            this.endIndex = pattern.length();
+        }
+        public ValidDateFormat(String pattern, Integer length) {
+            this.pattern = pattern;
+            this.endIndex = length;
+        }
+    }
+
+    /**
+     * Static ordered list of valid date format patterns.
+     */
+    private static ValidDateFormat[] dateFormats;
+    static {
+        dateFormats = new ValidDateFormat[] { 
+                new ValidDateFormat("MM/dd/yyyy"),
+                new ValidDateFormat("yyyy-MM-dd")
+        };
+    }
+
+    /**
+     * Convert an input string to a normalized date string.
+     * The output format is determined by the first element in
+     * the static dateFormats array.
+     * 
+     * @param inDate string to be normalized
+     * @return normalized string
+     */
+    public static String normalizeDateString(String inDate) {
+        Date outDate = null;
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        for (ValidDateFormat fm : dateFormats) {
+            if (outDate != null) {
+                continue; 
+            }
+            sdf.applyPattern(fm.pattern);
+            try {
+                outDate = sdf.parse(inDate.trim().substring(0, fm.endIndex));
+            } catch (ParseException e) {
+                outDate = null;
+            }
+        }
+       
+        if (outDate == null) {
+            return null;
+        }
+        sdf.applyPattern(dateFormats[0].pattern);
+        return sdf.format(outDate);
+    }
 }
