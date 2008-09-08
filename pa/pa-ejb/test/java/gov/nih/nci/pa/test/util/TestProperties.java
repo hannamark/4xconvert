05f00ebf526c1fdef30d392d5addcc37ff222255
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-08 21:21:10 +0000

    added an initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1642 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/test/java/gov/nih/nci/pa/test/util/TestProperties.java
@@ -0,0 +1,27 @@
+package gov.nih.nci.pa.test.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+/**
+ * Environment properties passed in to tests.
+ */
+public class TestProperties {
+    public static final String TEST_SCHEMA_SHOWSQL = "test.schema.showsql";
+
+    private static Properties properties = new Properties();
+    static {
+        try {
+            InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("test.properties");;
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static String getShowSQL() {
+        return properties.getProperty(TEST_SCHEMA_SHOWSQL, "false");
+    }
+}
