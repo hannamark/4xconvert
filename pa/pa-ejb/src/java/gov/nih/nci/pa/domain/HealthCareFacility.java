5ea195881c04d2a721638a781417b82c408f4d5a
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-08 20:33:08 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1609 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/domain/HealthCareFacility.java
@@ -0,0 +1,73 @@
+package gov.nih.nci.pa.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.validator.NotNull;
+
+
+/**
+ * An organization that devotes some or all of its resources (people, places, things) to the 
+ * delivery of healthcare services (including the financial and administrative management of those resources).  
+ * A healthcare facility may be manifest as a single physical location (e.g. building), or, 
+ * alternatively, as a distributed collection of physical spaces.. 
+ * 
+ * @author Naveen Amiruddin
+ * @since 07/24/2007
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without  express written permission of the
+ * copyright holder, NCI.
+ */
+
+@Entity
+@Table(name = "HEALTHCARE_FACILITY")
+public class HealthCareFacility  extends AbstractEntity {
+    
+    private Organization organization;
+    private List<StudyParticipation> studyParticipations = new ArrayList<StudyParticipation>();
+    
+    /**
+     * 
+     * @return organization
+     */
+    @ManyToOne
+    @JoinColumn(name = "ORGANIZATION_ID", updatable = false)
+    @NotNull    
+    public Organization getOrganization() {
+        return organization;
+    }
+    /**
+     * 
+     * @param organization organization
+     */
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+    /**
+     * 
+     * @return studyParticipations
+     */
+    @OneToMany(mappedBy = "healthCareFacility")
+    public List<StudyParticipation> getStudyParticipations() {
+        return studyParticipations;
+    }
+
+    /**
+     * 
+     * @param studyParticipations studyParticipations
+     */
+    public void setStudyParticipations(List<StudyParticipation> studyParticipations) {
+        this.studyParticipations = studyParticipations;
+    }
+    
+    
+    
+    
+
+}
