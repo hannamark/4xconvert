ceb39fd86cdf896912fc32cfdbe922904231e178
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-13 07:01:54 +0000

    changes for NCISpecificInformation
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1763 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
+++ pa/pa-web/src/java/gov/nih/nci/pa/action/NCISpecificInformationAction.java
@@ -1,6 +1,10 @@
 package gov.nih.nci.pa.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.pa.domain.Organization;
 import gov.nih.nci.pa.dto.NCISpecificInformationWebDTO;
 import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
 import gov.nih.nci.pa.enums.AccrualReportingMethodCode;
@@ -9,11 +13,14 @@ import gov.nih.nci.pa.iso.dto.StudyProtocolDTO;
 import gov.nih.nci.pa.iso.dto.StudyResourcingDTO;
 import gov.nih.nci.pa.iso.util.BlConverter;
 import gov.nih.nci.pa.iso.util.CdConverter;
+import gov.nih.nci.pa.iso.util.EnOnConverter;
+import gov.nih.nci.pa.iso.util.IiConverter;
 import gov.nih.nci.pa.iso.util.StConverter;
 import gov.nih.nci.pa.util.Constants;
 import gov.nih.nci.pa.util.IsoConverter;
 import gov.nih.nci.pa.util.PAUtil;
 import gov.nih.nci.pa.util.PaRegistry;
+import gov.nih.nci.services.organization.OrganizationDTO;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
@@ -35,6 +42,9 @@ public class NCISpecificInformationAction  extends ActionSupport {
     private NCISpecificInformationWebDTO nciSpecificInformationWebDTO = 
             new NCISpecificInformationWebDTO();
   
+    private List<OrganizationDTO> orgs = new ArrayList<OrganizationDTO>();
+    
+    private String chosenOrg;
      /**  
      * @return result
      */
@@ -52,6 +62,18 @@ public class NCISpecificInformationAction  extends ActionSupport {
                     PaRegistry.getStudyResourcingService().getsummary4ReportedResource(studyProtocolIi);
             
             nciSpecificInformationWebDTO = setNCISpecificDTO(studyProtocolDTO , studyResourcingDTO);
+            
+            if (studyResourcingDTO != null && studyResourcingDTO.getOrganizationIdentifier() != null) {
+                // get the name of the organization using primary id 
+                Organization o = new Organization();
+                o.setId(Long.valueOf(studyResourcingDTO.getOrganizationIdentifier().getExtension()));
+                Organization org = PaRegistry.getPAOrganizationService().getOrganizationByIndetifers(o);
+                // set the name
+                nciSpecificInformationWebDTO.setOrganizationName(org.getName());
+                // set the organization identifer
+                nciSpecificInformationWebDTO.setOrganizationIi(org.getIdentifier());
+            }
+            
             return SUCCESS;    
             
         } catch (Exception e) {
@@ -104,6 +126,28 @@ public class NCISpecificInformationAction  extends ActionSupport {
             //Step3: update studyprotocol 
             spDTO = PaRegistry.getStudyProtocolService().updateStudyProtocol(spDTO);
 
+            // Step 4: check if we have an organization for PO id 
+            String poIdentifer = nciSpecificInformationWebDTO.getOrganizationIi();
+            Long orgId = null;
+            if (PAUtil.isNotNullOrNotEmpty(poIdentifer)) {
+                Organization o = new Organization();
+                o.setIdentifier(poIdentifer);
+                Organization org = PaRegistry.getPAOrganizationService().getOrganizationByIndetifers(o);
+                if (org == null) {
+                    // create a new org if its null
+                    org = new Organization();
+                    org.setIdentifier(poIdentifer);
+                    org.setName(nciSpecificInformationWebDTO.getOrganizationName());
+                    Organization crOrg = PaRegistry.getPAOrganizationService().createOrganization(org);
+                    orgId = crOrg.getId();
+                } else {
+                    // get the org from the database
+                    orgId = org.getId();
+                }
+                
+            }
+            
+            
             //Step4 : find out if summary 4 records already exists
             StudyResourcingDTO summary4ResoureDTO = 
                 PaRegistry.getStudyResourcingService().getsummary4ReportedResource(studyProtocolIi);
@@ -117,6 +161,7 @@ public class NCISpecificInformationAction  extends ActionSupport {
                                                     nciSpecificInformationWebDTO.getSummaryFourFundingCategoryCode())));
                 summary4ResoureDTO.setUserLastUpdated((StConverter.convertToSt(
                         ServletActionContext.getRequest().getRemoteUser())));
+                summary4ResoureDTO.setOrganizationIdentifier(IiConverter.convertToIi(orgId));
                 
                 PaRegistry.getStudyResourcingService().createStudyResourcing(summary4ResoureDTO);
             } else {
@@ -127,13 +172,14 @@ public class NCISpecificInformationAction  extends ActionSupport {
                                 nciSpecificInformationWebDTO.getSummaryFourFundingCategoryCode())));
                 summary4ResoureDTO.setUserLastUpdated((StConverter.convertToSt(
                     ServletActionContext.getRequest().getRemoteUser())));
+                summary4ResoureDTO.setOrganizationIdentifier(IiConverter.convertToIi(orgId));
                 PaRegistry.getStudyResourcingService().updateStudyResourcing(summary4ResoureDTO);
             }
         } catch (Exception e) {
             addActionError(e.getMessage());
             return ERROR;
         }    
-        nciSpecificInformationWebDTO = setNCISpecificDTO(spDTO , srDTO);
+        //nciSpecificInformationWebDTO = setNCISpecificDTO(spDTO , srDTO);
         return SUCCESS;
     }
 
@@ -179,12 +225,80 @@ public class NCISpecificInformationAction  extends ActionSupport {
             nciSpDTO.setAccrualReportingMethodCode(spDTO.getAccrualReportingMethodCode().getCode());
         }
 
-        if (srDTO != null && srDTO.getTypeCode() != null) {
+        if (srDTO != null) {
+            if (srDTO.getTypeCode() != null) {
                 nciSpDTO.setSummaryFourFundingCategoryCode(srDTO.getTypeCode().getCode());
+            }
+            if (srDTO.getOrganizationIdentifier() != null) {
+                nciSpDTO.setOrganizationIi(srDTO.getOrganizationIdentifier().getExtension());
+            }
+                
         }
         return nciSpDTO;
         
     }
+    
+    /**
+    *
+    * @return result
+    */
+    public String getPoOrganizations() {
+        
+        String orgName = ServletActionContext.getRequest().getParameter("orgName");
+        OrganizationDTO criteria = new OrganizationDTO();
+        criteria.setName(EnOnConverter.convertToEnOn(orgName));
+        orgs = PaRegistry.getPoOrganizationEntityService().search(criteria);
+    return SUCCESS;
+
+    }
+    
+    /**
+    *
+    * @return result
+    */
+    public String displayOrg() {
+    String orgId = ServletActionContext.getRequest().getParameter("orgId");
+    OrganizationDTO criteria = new OrganizationDTO();
+    criteria.setIdentifier(EnOnConverter.convertToOrgIi(Long.valueOf(orgId)));
+
+    chosenOrg = PaRegistry.getPoOrganizationEntityService().search(criteria).    
+    get(0).getName().getPart().get(0).getValue();
+    
+    nciSpecificInformationWebDTO.setOrganizationName(chosenOrg);
+    nciSpecificInformationWebDTO.setOrganizationIi(orgId);
+
+    return SUCCESS;
+    }
+
+    /**
+     * @return the orgs
+     */
+    public List<OrganizationDTO> getOrgs() {
+        return orgs;
+    }
+
+    /**
+     * @param orgs the orgs to set
+     */
+    public void setOrgs(List<OrganizationDTO> orgs) {
+        this.orgs = orgs;
+    }
+
+    /**
+     * @return the chosenOrg
+     */
+    public String getChosenOrg() {
+        return chosenOrg;
+    }
+
+    /**
+     * @param chosenOrg the chosenOrg to set
+     */
+    public void setChosenOrg(String chosenOrg) {
+        this.chosenOrg = chosenOrg;
+    }
+    
+    
 
 
 }
