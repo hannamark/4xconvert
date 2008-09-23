6481942bc724dd274875a080fd7ce08f49b97a2d
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-23 16:44:43 +0000

    moved from service to service\util folder
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@1947 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/test/java/gov/nih/nci/pa/test/util/MockServiceLocator.java
+++ pa/pa-web/test/java/gov/nih/nci/pa/test/util/MockServiceLocator.java
@@ -1,15 +1,15 @@
 package gov.nih.nci.pa.test.util;
 
 import gov.nih.nci.pa.service.DiseaseCondServiceRemote;
-import gov.nih.nci.pa.service.PAOrganizationServiceRemote;
-import gov.nih.nci.pa.service.PAPersonServiceRemote;
-import gov.nih.nci.pa.service.RegulatoryInformationServiceRemote;
 import gov.nih.nci.pa.service.StudyOverallStatusServiceRemote;
 import gov.nih.nci.pa.service.StudyProtocolServiceRemote;
 import gov.nih.nci.pa.service.StudyRegulatoryAuthorityServiceRemote;
 import gov.nih.nci.pa.service.StudyResourcingServiceRemote;
 import gov.nih.nci.pa.service.util.LookUpTableServiceRemote;
+import gov.nih.nci.pa.service.util.PAOrganizationServiceRemote;
+import gov.nih.nci.pa.service.util.PAPersonServiceRemote;
 import gov.nih.nci.pa.service.util.ProtocolQueryServiceLocal;
+import gov.nih.nci.pa.service.util.RegulatoryInformationServiceRemote;
 import gov.nih.nci.pa.util.ServiceLocator;
 import gov.nih.nci.service.MockDiseaseConditionService;
 import gov.nih.nci.service.MockStudyOverallStatusService;
