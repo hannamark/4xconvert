3098781760e022491cbbfe45783613c37bfe6f4f
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-09-28 23:33:44 +0000

    Registry web source files initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2042 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/action/SearchTrialAction.java
@@ -0,0 +1,22 @@
+package gov.nih.nci.registry.action;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+
+/**
+ *
+ * @author Harsha
+ *
+ */
+@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
+@Validation
+public class SearchTrialAction extends ActionSupport {
+
+
+}
