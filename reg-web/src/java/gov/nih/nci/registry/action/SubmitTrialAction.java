68b21062213cf3697e84a5a06bbfb7c6259e2cb9
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-03 14:55:05 +0000

    Action  class for Submit Trial initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2130 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/action/SubmitTrialAction.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.registry.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+
+/**
+ *
+ * @author Bala Nair
+ *
+ */
+@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
+@Validation
+public class SubmitTrialAction extends ActionSupport {
+
+
+
+}
