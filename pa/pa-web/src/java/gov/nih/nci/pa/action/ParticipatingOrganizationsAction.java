6fe22c211fbe90d72e5b9cf58b5658874446b4ef
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-04 10:46:51 +0000

    Implement facilitySave().  Remove createTest().
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2156 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/src/java/gov/nih/nci/pa/action/ParticipatingOrganizationsAction.java
+++ pa/pa-web/src/java/gov/nih/nci/pa/action/ParticipatingOrganizationsAction.java
@@ -7,6 +7,7 @@ import gov.nih.nci.coppa.iso.Ii;
 import gov.nih.nci.pa.domain.Organization;
 import gov.nih.nci.pa.dto.CountryRegAuthorityDTO;
 import gov.nih.nci.pa.dto.OrganizationWebDTO;
+import gov.nih.nci.pa.dto.ParticipatingOrganizationsTabWebDTO;
 import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
 import gov.nih.nci.pa.enums.RecruitmentStatusCode;
 import gov.nih.nci.pa.enums.StatusCode;
@@ -51,6 +52,10 @@ import com.opensymphony.xwork2.Preparable;
 public class ParticipatingOrganizationsAction extends ActionSupport 
         implements Preparable {
     private static final long serialVersionUID = 123412653L;
+    private static final int AD_CITY_IDX = 1;
+    private static final int AD_COUNTRY_IDX = 3;
+    private static final int AD_ZIP_IDX = 2;
+    private static final String ACT_FACILITY_SAVE = "facilitySave";
 
     private StudyProtocolServiceRemote sProService;
     private StudyParticipationServiceRemote sPartService;
@@ -97,30 +102,80 @@ public class ParticipatingOrganizationsAction extends ActionSupport
      * @throws Exception exception
      */
     public String create() throws Exception {
-        clearErrorsAndMessages();
-        enforceBusinessRules();
-        if (hasActionErrors()) {
-            return Action.SUCCESS;
-        }
-        if (!hasActionErrors()) {
-//            addActionError("Create succeeded.");
-        }
         loadForm();
         return Action.SUCCESS;
     }
-
 
     /**  
      * @return result
      * @throws Exception exception
      */
-    public String createTest() throws Exception {
+    public String facilitySave() throws Exception {
         clearErrorsAndMessages();
         enforceBusinessRules();
         
+        // 1) get the po identifer from input field ( user selected from pop-up)
+        ParticipatingOrganizationsTabWebDTO tab = (ParticipatingOrganizationsTabWebDTO) 
+                ServletActionContext.getRequest().getSession().getAttribute(Constants.PARTICIPATING_ORGANIZATIONS_TAB);
+        if (tab == null) {
+            loadForm();
+            addActionError("You must select an organization before adding.");
+            return ACT_FACILITY_SAVE;
+        }
+        String poOrgId = tab.getFacilityOrganization().getIdentifier();
+
+        // 2) using the poIdentifer call po.getHealthcareFacility(poOrganizationid) (this 
+        //      method is NOT yet implemented in PO)
+        //
+        //  if (poHealthcareFacilityIi == null) {
+        //      poHealthcareFacilityIi = pa.regisitry.poCreateHealthcareFacility(poISOHealthcareFacilityDTO) 
+        //
+        // note : this method is NOT yet imple. stub it for now
+        String poHfId = null;
+
+        // 3) at this point we have both poOrganizationIi & poHealthcareFacilityIi
+        // 4) check if PA has organization and healthcarefacitly (local copy)
+        // 5) paOrganizationIi = PAOrganizationServiceRemote.getOrganizationByIndetifers(poOrganizationIi)
+        //      (this method is implemented )
+        Organization org = new Organization();
+        org.setIdentifier(poOrgId);
+        Organization paOrg = paoService.getOrganizationByIndetifers(org);
+
+        // 6) if (paOrganizationIi == null ) {
+        //      create an organization in pa
+        //      paOrganizationIi = PAOrganizationServiceRemote.createOrganization(Organization) 
+        //          (this method is implemented)
+        //    }
+        if (paOrg == null) {
+            org.setCity(tab.getFacilityOrganization().getCity());
+            org.setCountryName(tab.getFacilityOrganization().getCountryName());
+            org.setName(tab.getFacilityOrganization().getName());
+            org.setPostalCode(tab.getFacilityOrganization().getPostalCode());
+            paOrg = paoService.createOrganization(org);
+        }
+
+        // 7) paHealthcareFacilityIi = PAHealthcareFacilityServiceRemote.getHealthcareFacility(poHealthcareFacilityIi)
+        // 8) if (paHealthcareFacilityIi == null ) {
+        //      create an HealthcareFacilityIi in pa
+        //      paHealthcareFacilityIi = PAHealthcareFacilityServiceRemote.createHealthcareFacility(HealthcareFacility) 
+        //          (this method is NOT impemented)
+        //    }
+        HealthCareFacilityDTO hfDto = null;
+        List<HealthCareFacilityDTO> hfList = pahfService.getByOrganization(IiConverter.convertToIi(poOrgId));
+        if (hfList.size() > 0) {
+            hfDto = hfList.get(0);
+        } else {
+            hfDto = new HealthCareFacilityDTO();
+            hfDto.setIdentifier(StConverter.convertToSt("1234"));
+            hfDto.setOrganizationIi(IiConverter.convertToIi(paOrg.getId()));
+            hfDto = pahfService.create(hfDto);
+        }
+
+        // 9) now you have paOrganizationIi & paHealthcareFacilityIi and you can 
+        //      create Participations and Studyprotocol   
         StudyParticipationDTO sp = new StudyParticipationDTO();
         sp.setFunctionalCode(CdConverter.convertToCd(StudyParticipationFunctionalCode.TREATING_SITE));
-        sp.setHealthcareFacilityIi(IiConverter.convertToIi(1L));
+        sp.setHealthcareFacilityIi(hfDto.getIi());
         sp.setIi(IiConverter.convertToIi((Long) null));
         sp.setLocalStudyProtocolIdentifier(StConverter.convertToSt("Local SP Identifier"));
         sp.setStatusCode(CdConverter.convertToCd(StatusCode.ACTIVE));
@@ -135,9 +190,10 @@ public class ParticipatingOrganizationsAction extends ActionSupport
         ssas.setStudyParticipationIi(sp.getIi());
         ssas = ssasService.createStudySiteAccrualStatus(ssas);
         
-        addActionError("createTest() completed");
+        ServletActionContext.getRequest().getSession().removeAttribute(Constants.PARTICIPATING_ORGANIZATIONS_TAB);
+        ServletActionContext.getRequest().setAttribute(Constants.SUCCESS_MESSAGE, Constants.CREATE_MESSAGE);
         loadForm();
-        return Action.SUCCESS;
+        return ACT_FACILITY_SAVE;
     }
 
     /**  
@@ -159,7 +215,7 @@ public class ParticipatingOrganizationsAction extends ActionSupport
            ssas = ssasService.createStudySiteAccrualStatus(ssas);
         }
         
-        addActionError("updateTest() completed");
+        ServletActionContext.getRequest().setAttribute(Constants.SUCCESS_MESSAGE, Constants.UPDATE_MESSAGE);
         loadForm();
         return Action.SUCCESS;
     }
@@ -177,7 +233,7 @@ public class ParticipatingOrganizationsAction extends ActionSupport
            sPartService.delete(sp.getIi());
         }
         
-        addActionError("deleteTest() completed");
+        ServletActionContext.getRequest().setAttribute(Constants.SUCCESS_MESSAGE, Constants.DELETE_MESSAGE);
         loadForm();
         return Action.SUCCESS;
     }
@@ -188,7 +244,7 @@ public class ParticipatingOrganizationsAction extends ActionSupport
         for (StudyParticipationDTO sp : spList) {
             List<StudySiteAccrualStatusDTO> ssasList = 
                 ssasService.getCurrentStudySiteAccrualStatusByStudyParticipation(sp.getIi());
-            HealthCareFacilityDTO hf = pahfService.getHealthCareFacility(sp.getHealthcareFacilityIi());
+            HealthCareFacilityDTO hf = pahfService.get(sp.getHealthcareFacilityIi());
             Organization orgBo = new Organization();
             orgBo.setId(IiConverter.convertToLong(hf.getOrganizationIi()));
             orgBo = paoService.getOrganizationByIndetifers(orgBo);
@@ -219,6 +275,18 @@ public class ParticipatingOrganizationsAction extends ActionSupport
         OrganizationDTO criteria = new OrganizationDTO();
         criteria.setIdentifier(EnOnConverter.convertToOrgIi(Long.valueOf(orgId)));        
         selectedOrgDTO = PaRegistry.getPoOrganizationEntityService().search(criteria).get(0);
+
+        // store selection
+        Organization org = new Organization();
+        org.setCity(selectedOrgDTO.getPostalAddress().getPart().get(AD_CITY_IDX).getValue());
+        org.setCountryName(selectedOrgDTO.getPostalAddress().getPart().get(AD_COUNTRY_IDX).getCode());
+        org.setIdentifier(IiConverter.convertToString(selectedOrgDTO.getIdentifier()));
+        org.setName(selectedOrgDTO.getName().getPart().get(0).getValue());
+        org.setPostalCode(selectedOrgDTO.getPostalAddress().getPart().get(AD_ZIP_IDX).getValue());
+        
+        ParticipatingOrganizationsTabWebDTO tab =  new ParticipatingOrganizationsTabWebDTO();
+        tab.setFacilityOrganization(org);
+        ServletActionContext.getRequest().getSession().setAttribute(Constants.PARTICIPATING_ORGANIZATIONS_TAB, tab);
         return DISPLAYJSP;
     }
 
