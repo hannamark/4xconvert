9591d3c1cb4220f90470d8113ffd091d38cc2079
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-07 02:08:40 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2177 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/session/BindHibernateSession.java
@@ -0,0 +1,47 @@
+package gov.nih.nci.registry.session;
+
+import gov.nih.nci.pa.util.HibernateUtil;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author Bala Nair
+ *
+ */
+public class BindHibernateSession implements Filter {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void destroy() {
+        //HibernateUtil.getHibernateHelper().unbindAndCleanupSession();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("PMD.SystemPrintln")
+    public void doFilter(ServletRequest arg0, ServletResponse arg1,
+            FilterChain arg2) throws IOException, ServletException {
+        HibernateUtil.getHibernateHelper().openAndBindSession();
+        arg2.doFilter(arg0, arg1);
+        HibernateUtil.getHibernateHelper().unbindAndCleanupSession();
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void init(FilterConfig arg0) throws ServletException {
+     // no-op
+    }
+
+}
