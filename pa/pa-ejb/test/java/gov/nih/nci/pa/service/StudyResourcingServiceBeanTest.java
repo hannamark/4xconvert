2cfe853c753201ac176b0e84452c714de36d250e
guthikondak <guthikondak@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-07 14:27:13 +0000

    converted serial number from St to Int data type
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2184 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/test/java/gov/nih/nci/pa/service/StudyResourcingServiceBeanTest.java
+++ pa/pa-ejb/test/java/gov/nih/nci/pa/service/StudyResourcingServiceBeanTest.java
@@ -2,20 +2,16 @@ package gov.nih.nci.pa.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import java.io.Serializable;
-
 import gov.nih.nci.pa.domain.StudyProtocol;
 import gov.nih.nci.pa.domain.StudyProtocolTest;
 import gov.nih.nci.pa.domain.StudyResourcing;
 import gov.nih.nci.pa.domain.StudyResourcingTest;
-import gov.nih.nci.pa.iso.convert.StudyResourcingConverter;
 import gov.nih.nci.pa.iso.dto.StudyResourcingDTO;
 import gov.nih.nci.pa.iso.util.IiConverter;
+import gov.nih.nci.pa.iso.util.IntConverter;
 import gov.nih.nci.pa.iso.util.StConverter;
 import gov.nih.nci.pa.util.TestSchema;
 
-import org.hibernate.Session;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -55,7 +51,7 @@ public class StudyResourcingServiceBeanTest {
         assertNotNull(sr.getId());
         
         StudyResourcingDTO srDTO1 = new StudyResourcingDTO();
-        srDTO1.setSerialNumber(StConverter.convertToSt("123"));
+        srDTO1.setSerialNumber(IntConverter.convertToInt("123"));
         srDTO1.setStudyProtocolIi(IiConverter.convertToIi(sp.getId()));
         
         StudyResourcingDTO srDTO2 = remoteEjb.createStudyResourcing(srDTO1);
@@ -78,7 +74,7 @@ public class StudyResourcingServiceBeanTest {
         assertNotNull(sr.getId());
         
         StudyResourcingDTO srDTO1 = new StudyResourcingDTO();
-        srDTO1.setSerialNumber(StConverter.convertToSt("123"));
+        srDTO1.setSerialNumber(IntConverter.convertToInt("123"));
         srDTO1.setStudyProtocolIi(IiConverter.convertToIi(sp.getId()));
         
         StudyResourcingDTO srDTO2 = remoteEjb.createStudyResourcing(srDTO1);
@@ -86,10 +82,10 @@ public class StudyResourcingServiceBeanTest {
         assertEquals (srDTO1.getSerialNumber().getValue(), srDTO2.getSerialNumber().getValue());
 
         srDTO2.setStudyProtocolIi(IiConverter.convertToIi(sp.getId()));
-        srDTO2.setSerialNumber(StConverter.convertToSt("123123"));
+        srDTO2.setSerialNumber(IntConverter.convertToInt("123123"));
         StudyResourcingDTO srDTO3 = remoteEjb.createStudyResourcing(srDTO2);
         assertNotNull(srDTO3);
-        assertEquals (srDTO3.getSerialNumber().getValue(), "123123");
+        assertEquals (srDTO3.getSerialNumber().getValue().toString(), "123123");
 
     
     }
