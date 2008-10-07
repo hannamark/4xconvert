7ce4c02528d510f9b5bbd027998f82e98531ff42
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-07 02:11:24 +0000

    modified the SubmitTrial method mappings
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2178 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- reg-web/src/java/gov/nih/nci/registry/action/SubmitTrialAction.java
+++ reg-web/src/java/gov/nih/nci/registry/action/SubmitTrialAction.java
@@ -1,17 +1,40 @@
 package gov.nih.nci.registry.action;
 
+import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.pa.enums.PhaseCode;
+import gov.nih.nci.pa.iso.util.CdConverter;
+import gov.nih.nci.pa.iso.util.StConverter;
+import gov.nih.nci.pa.iso.util.IiConverter;
+import gov.nih.nci.registry.util.RegistryServiceLocator;
+
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.validator.annotations.Validation;
+import org.apache.struts2.ServletActionContext;
+
+import gov.nih.nci.pa.iso.dto.InterventionalStudyProtocolDTO;
 
 /**
- *
+ * 
  * @author Bala Nair
- *
+ * 
  */
-@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.ImmutableField", "PMD.SingularField" })
 @Validation
 public class SubmitTrialAction extends ActionSupport {
-
-
-
+    /**
+     * create protocol.
+     */
+    public void create() {
+        try {
+            InterventionalStudyProtocolDTO protocolDTO = new InterventionalStudyProtocolDTO();
+            String phaseCode = ServletActionContext.getRequest().getParameter("trialPhase");
+            String officialTitle = ServletActionContext.getRequest().getParameter("trialTitle");
+            protocolDTO.setPhaseCode(CdConverter.convertToCd(PhaseCode.getByCode(phaseCode)));
+            protocolDTO.setOfficialTitle(StConverter.convertToSt(officialTitle));
+            Ii isoIdentifier = RegistryServiceLocator.getStudyProtocolService()
+                                    .createInterventionalStudyProtocol(protocolDTO);
+            LOG.info("Trial is registered with ID: " + IiConverter.convertToString(isoIdentifier));
+        } catch (Exception e) {
+            LOG.error("Exception occured while submitting trial");
+        }
+    }
 }
