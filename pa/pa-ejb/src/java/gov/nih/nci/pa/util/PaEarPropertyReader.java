cf0992368ca89afa920b17e5728668c14cd41f1f
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-10 14:07:46 +0000

    initial version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2244 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/util/PaEarPropertyReader.java
@@ -0,0 +1,55 @@
+package gov.nih.nci.pa.util;
+
+import gov.nih.nci.pa.service.PAException;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Property File reader which can be extended for passing in the run time
+ * properties for PA.
+ * 
+ * @author Harsha
+ * @since 09/14/2008 copyright NCI 2007. All rights reserved. This code may not
+ *        be used without the express written permission of the copyright
+ *        holder, NCI.
+ * 
+ */
+@SuppressWarnings("PMD")
+public class PaEarPropertyReader {
+    private static final Logger LOG = Logger.getLogger(PaEarPropertyReader.class);
+    private static final String RESOURCE_NAME = "paear.properties";
+    private static Properties props = null;
+    private static String docUploadPath = "doc.upload.path";
+    static {
+        try {
+            props = new Properties();
+            props.load(PaEarPropertyReader.class.getClassLoader().getResourceAsStream(RESOURCE_NAME));
+        } catch (Exception e) {
+            LOG.error("Unable to read paear.properties ", e);
+            throw new IllegalStateException(e);
+        }
+    }
+
+
+    /**
+     * 
+     * @return folder path
+     * @throws PAException e
+     */
+    public static String getDocUploadPath() throws PAException {
+        String folderPath = props.getProperty(docUploadPath);
+        if (folderPath == null) {
+            LOG.error(" doc.upload.path does not have value in paear.properties ");
+            throw new PAException("doc.upload.path does not have value in paear.properties");
+        }
+        File f = new File(folderPath);
+        if (!f.isDirectory()) {
+            LOG.error(folderPath + " is not a valid directory ");
+            throw new PAException(folderPath + " is not a valid directory ");
+        }    
+        return folderPath;
+    }
+}
