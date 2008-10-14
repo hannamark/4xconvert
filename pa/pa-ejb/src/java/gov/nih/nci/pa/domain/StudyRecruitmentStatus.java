8a0b16275e3bbb1441ba8f8b1b89f653f6642488
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-14 13:38:54 +0000

    Initial check-in of StudyRecruitmentStatus class with hibernate annotations.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2300 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/domain/StudyRecruitmentStatus.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package gov.nih.nci.pa.domain;
+
+import gov.nih.nci.pa.enums.RecruitmentStatusCode;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.validator.NotNull;
+
+/**
+ * Describes the comprehensive state of the study.
+ * 
+ * @author Hugh Reinhart
+ * @since 10/10/2008
+ * copyright NCI 2007.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI.
+ */
+@Entity
+@Table(name = "STUDY_RECRUITMENT_STATUS")
+public class StudyRecruitmentStatus extends AbstractEntity {
+    private static final long serialVersionUID = 1234567890L;
+    
+    private RecruitmentStatusCode statusCode;
+    private Timestamp statusDate;
+    private StudyProtocol studyProtocol;
+    
+    
+    /**
+     * @return statusCode
+     */
+    @Column(name = "STATUS_CODE")
+    @Enumerated(EnumType.STRING)
+    public RecruitmentStatusCode getStatusCode() {
+        return statusCode;
+    }
+    /**
+     * 
+     * @param statusCode status code
+     */
+    public void setStatusCode(RecruitmentStatusCode statusCode) {
+       this.statusCode = statusCode;
+    }
+
+    /**
+     * 
+     * @return statusDate
+     */
+    @Column(name = "STATUS_DATE")
+    public Timestamp  getStatusDate() {
+        return statusDate;
+    }
+    
+    /**
+     * 
+     * @param statusDate status Date
+     */
+    public void setStatusDate(Timestamp statusDate) {
+        this.statusDate = statusDate;
+    }
+
+    /**
+     * 
+     * @return protocol
+     */
+    @ManyToOne
+    @JoinColumn(name = "STUDY_PROTOCOL_ID", updatable = false)
+    @NotNull
+    public StudyProtocol getStudyProtocol() {
+       return studyProtocol;
+    }
+    /**
+     * 
+     * @param studyProtocol studyProtocol
+     */
+    public void setStudyProtocol(StudyProtocol studyProtocol) {
+        this.studyProtocol = studyProtocol;
+    }
+}
