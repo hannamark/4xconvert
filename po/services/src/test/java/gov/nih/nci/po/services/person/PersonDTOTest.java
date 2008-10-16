bfcc8d61f3a1a11bdc731c15bf12efabe43ec3de
parnellt <parnellt@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-16 21:00:49 +0000

    added CTEP person test case
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2407 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/services/src/test/java/gov/nih/nci/po/services/person/PersonDTOTest.java
+++ po/services/src/test/java/gov/nih/nci/po/services/person/PersonDTOTest.java
@@ -1,35 +1,67 @@
 package gov.nih.nci.po.services.person;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import gov.nih.nci.coppa.iso.Ad;
+import gov.nih.nci.coppa.iso.Adxp;
+import gov.nih.nci.coppa.iso.AdxpAdl;
+import gov.nih.nci.coppa.iso.AdxpAl;
+import gov.nih.nci.coppa.iso.AdxpCnt;
+import gov.nih.nci.coppa.iso.AdxpCty;
+import gov.nih.nci.coppa.iso.AdxpSta;
+import gov.nih.nci.coppa.iso.AdxpZip;
 import gov.nih.nci.coppa.iso.Cd;
 import gov.nih.nci.coppa.iso.DSet;
 import gov.nih.nci.coppa.iso.EnPn;
+import gov.nih.nci.coppa.iso.EntityNamePartType;
+import gov.nih.nci.coppa.iso.Enxp;
+import gov.nih.nci.coppa.iso.Ii;
 import gov.nih.nci.coppa.iso.NullFlavor;
+import gov.nih.nci.coppa.iso.Tel;
+import gov.nih.nci.coppa.iso.TelEmail;
+import gov.nih.nci.coppa.iso.TelPhone;
 import gov.nih.nci.po.data.bo.EntityStatus;
 import gov.nih.nci.po.data.bo.Person;
 import gov.nih.nci.po.data.bo.RaceCode;
 import gov.nih.nci.po.data.bo.SexCode;
 import gov.nih.nci.po.data.convert.IdConverter.PersonIdConverter;
 import gov.nih.nci.po.data.convert.util.PersonNameConverterUtil;
+import gov.nih.nci.po.util.MockCountryServiceLocator;
+import gov.nih.nci.po.util.PoHibernateUtil;
+import gov.nih.nci.po.util.PoRegistry;
 import gov.nih.nci.po.util.PoXsnapshotHelper;
+import gov.nih.nci.po.util.ServiceLocator;
 import gov.nih.nci.services.PoIsoConstraintException;
 import gov.nih.nci.services.person.PersonDTO;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.builder.EqualsBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PersonDTOTest {
     PersonDTO dto;
+    ServiceLocator oldLocator = null;
 
     @Before
     public void setup() {
         dto = new PersonDTO();
+        oldLocator = PoRegistry.getInstance().getServiceLocator();
+        PoRegistry.getInstance().setServiceLocator(new MockCountryServiceLocator());
+    }
+
+    @After
+    public void after() {
+        PoRegistry.getInstance().setServiceLocator(oldLocator);
     }
 
     @Test
@@ -334,11 +366,124 @@ public class PersonDTOTest {
             assertEquals("unsupported code " + sex.getCode(), e.getMessage());
         }
     }
-    
+
+    /**
+     * This is the data from the CTEP mapping document.  If this test does not pass, we need
+     * to talk to CTEP about why the data they are providing isn't enough for us to persist.
+     *
+     * @throws Exception on error
+     */
+    @Test
+    public void testCTEPDataset() throws Exception {
+        dto.setIdentifier(new Ii());
+        dto.getIdentifier().setDisplayable(Boolean.TRUE);
+        dto.getIdentifier().setExtension("30");
+        dto.getIdentifier().setIdentifierName("CTEP_IDENTIFIER");
+        dto.getIdentifier().setRoot("CTEP_ROOT");
+
+        Ad ad = new Ad();
+        List<Adxp> part = new ArrayList<Adxp>();
+        ad.setPart(part);
+
+        AdxpAl al = new AdxpAl();
+        al.setValue("777 Preston Research Building");
+        part.add(al);
+
+        AdxpAdl adl = new AdxpAdl();
+        adl.setValue("Division of Hematology/Oncology");
+        part.add(adl);
+
+        AdxpCty cty = new AdxpCty();
+        cty.setValue("Nashville");
+        part.add(cty);
+
+        AdxpSta sta = new AdxpSta();
+        sta.setValue("TN");
+        part.add(sta);
+
+        AdxpZip zip = new AdxpZip();
+        zip.setValue("37232-6307");
+        part.add(zip);
+
+        AdxpCnt cnt = new AdxpCnt();
+        cnt.setCode("USA");
+        part.add(cnt);
+
+        dto.setPostalAddress(ad);
+
+        Cd statusCode = new Cd();
+        statusCode.setCode("active");
+        dto.setStatusCode(statusCode);
+
+        dto.setRaceCode(null);
+        dto.setSexCode(null); // TODO
+
+        EnPn pn = new EnPn();
+        List<Enxp> part2 = pn.getPart();
+
+        Enxp enxp = new Enxp(EntityNamePartType.FAM);
+        enxp.setValue("Sosman");
+        part2.add(enxp);
+
+        enxp = new Enxp(EntityNamePartType.GIV);
+        enxp.setValue("Jeffrey");
+        part2.add(enxp);
+
+        enxp = new Enxp(EntityNamePartType.GIV);
+        enxp.setValue("A.");
+        part2.add(enxp);
+
+        enxp = new Enxp(EntityNamePartType.PFX);
+        enxp.setValue("Dr.");
+        part2.add(enxp);
+
+        dto.setName(pn);
+
+        DSet<Tel> telecomAddresses = new DSet<Tel>();
+        telecomAddresses.setItem(new HashSet<Tel>());
+        TelEmail email = new TelEmail();
+        email.setValue(new URI("mailto:jeff.sosman@vanderbilt.edu"));
+        telecomAddresses.getItem().add(email);
+
+        TelPhone phone = new TelPhone();
+        phone.setValue(new URI("x-text-fax:(615)%20343-7602"));
+        telecomAddresses.getItem().add(phone);
+
+        phone = new TelPhone();
+        phone.setValue(new URI("tel:(615)%20322-4967"));
+        telecomAddresses.getItem().add(phone);
+
+        dto.setTelecomAddress(telecomAddresses);
+
+        Person p = (Person) PoXsnapshotHelper.createModel(dto);
+        assertNotNull(p);
+        assertTrue(PoHibernateUtil.validate(p).isEmpty());
+        assertEquals(30, p.getId().longValue());
+        assertEquals("777 Preston Research Building", p.getPostalAddress().getStreetAddressLine());
+        assertEquals("Division of Hematology/Oncology", p.getPostalAddress().getDeliveryAddressLine());
+        assertEquals("Nashville", p.getPostalAddress().getCityOrMunicipality());
+        assertEquals("TN", p.getPostalAddress().getStateOrProvince());
+        assertEquals("USA", p.getPostalAddress().getCountry().getAlpha3());
+        assertEquals("37232-6307", p.getPostalAddress().getPostalCode());
+        assertEquals(EntityStatus.ACTIVE, p.getStatusCode());
+        assertTrue(CollectionUtils.isEmpty(p.getRaces()));
+        assertNull(p.getSex());
+        assertEquals("Sosman", p.getLastName());
+        assertEquals("Jeffrey", p.getFirstName());
+        assertEquals("A.", p.getMiddleName());
+        assertEquals("Dr.", p.getPrefix());
+        assertEquals(1, p.getEmail().size());
+        assertEquals("jeff.sosman@vanderbilt.edu", p.getEmail().get(0).getValue());
+        assertEquals(1, p.getFax().size());
+        assertEquals("(615) 343-7602", p.getFax().get(0).getValue()); // notice %20 converted to space
+        assertEquals(1, p.getPhone().size());
+        assertEquals("(615) 322-4967", p.getPhone().get(0).getValue()); // notice %20 converted to space
+    }
+
     // regression test for https://jira.5amsolutions.com/browse/PO-601
     @Test
     public void roundTripIdentifier() {
-        dto.setIdentifier(new PersonIdConverter().convertToIi(1l));        
+        dto.setIdentifier(new PersonIdConverter().convertToIi(1l));
         Person bo = (Person) PoXsnapshotHelper.createModel(dto);
         PersonDTO copy = (PersonDTO) PoXsnapshotHelper.createSnapshot(bo);
         EqualsBuilder.reflectionEquals(dto.getIdentifier(), copy.getIdentifier());
