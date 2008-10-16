cded8e7ade8d010529389627c4da0ddf2c18d2d6
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-16 00:06:59 +0000

    Test StudyRecruitmentStatus.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2381 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/test/java/gov/nih/nci/pa/enums/StudyRecruitmentStatusTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package gov.nih.nci.pa.enums;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * @author hreinhart
+ *
+ */
+public class StudyRecruitmentStatusTest {
+    @Test
+    public void getByStudyStatusCodeTest() {
+        assertEquals(StudyRecruitmentStatusCode.NOT_YET_RECRUITING.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.APPROVED).getCode());
+        assertEquals(StudyRecruitmentStatusCode.RECRUITING_ACTIVE.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.ACTIVE).getCode());
+        assertEquals(StudyRecruitmentStatusCode.NOT_RECRUITING.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.CLOSED_TO_ACCRUAL).getCode());
+        assertEquals(StudyRecruitmentStatusCode.NOT_RECRUITING.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.CLOSED_TO_ACCRUAL_AND_INTERVENTION).getCode());
+        assertEquals(StudyRecruitmentStatusCode.SUSPENDED.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.TEMPORARILY_CLOSED_TO_ACCRUAL).getCode());
+        assertEquals(StudyRecruitmentStatusCode.SUSPENDED.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.TEMPORARILY_CLOSED_TO_ACCRUAL_AND_INTERVENTION).getCode());
+        assertEquals(StudyRecruitmentStatusCode.WITHDRAWN.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.WITHDRAWN).getCode());
+        assertEquals(StudyRecruitmentStatusCode.COMPLETED.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.COMPLETE).getCode());
+        assertEquals(StudyRecruitmentStatusCode.TERMINATED.getCode(), 
+                StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.ADMINISTRATIVELY_COMPLETE).getCode());
+        assertNull(StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.IN_REVIEW));
+        assertNull(StudyRecruitmentStatusCode.getByStudyStatusCode(StudyStatusCode.DISAPPROVED));
+    }
+}
