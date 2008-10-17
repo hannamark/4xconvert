bf7a64de7438be7c1e08b3fb7055e57a63f4a52c
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-10-17 15:31:13 +0000

    added attributes for Study Overall Status
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2414 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- reg-web/src/java/gov/nih/nci/registry/dto/InterventionalStudyProtocolWebDTO.java
+++ reg-web/src/java/gov/nih/nci/registry/dto/InterventionalStudyProtocolWebDTO.java
@@ -4,7 +4,10 @@
 package gov.nih.nci.registry.dto;
 
 import gov.nih.nci.pa.iso.dto.InterventionalStudyProtocolDTO;
+import gov.nih.nci.pa.iso.util.CdConverter;
 import gov.nih.nci.pa.iso.util.IiConverter;
+import gov.nih.nci.pa.iso.util.TsConverter;
+import gov.nih.nci.pa.util.PAUtil;
 
 /**
  * @author Bala Nair
@@ -15,8 +18,10 @@ public class InterventionalStudyProtocolWebDTO {
     private String trialTitle;
     private String trialPhase;
     private String nciAccessionNumber;
-
-
+    private String startDate;
+    private String completionDate;
+    private String startDateType;
+    private String completionDateType;
 
     /**
      * @param iso
@@ -27,6 +32,18 @@ public class InterventionalStudyProtocolWebDTO {
         this.trialPhase = iso.getPhaseCode().getCode();
         this.trialTitle = iso.getOfficialTitle().getValue();
         this.nciAccessionNumber =  IiConverter.convertToString(iso.getIdentifier());
+        if (iso.getStartDate() != null) {
+            this.startDate = PAUtil.normalizeDateString(
+                           TsConverter.convertToTimestamp(iso.getStartDate()).toString());
+            this.startDateType = CdConverter.convertCdToString(iso.getStartDateTypeCode());
+        }
+        if (iso.getPrimaryCompletionDate() != null) {
+            this.completionDate = PAUtil.normalizeDateString(
+                    TsConverter.convertToTimestamp(iso.getPrimaryCompletionDate()).toString());
+            
+            this.completionDateType = CdConverter.convertCdToString(
+                                        iso.getPrimaryCompletionDateTypeCode());
+        }
     }
 
     /**
@@ -65,6 +82,62 @@ public class InterventionalStudyProtocolWebDTO {
      */
     public void setNciAccessionNumber(String nciAccessionNumber) {
         this.nciAccessionNumber = nciAccessionNumber;
+    }
+    
+    /**
+     * @return the startDate
+     */
+    public String getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the completionDate
+     */
+    public String getCompletionDate() {
+        return completionDate;
+    }
+
+    /**
+     * @param completionDate the completionDate to set
+     */
+    public void setCompletionDate(String completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    /**
+     * @return the startDateType
+     */
+    public String getStartDateType() {
+        return startDateType;
+    }
+
+    /**
+     * @param startDateType the startDateType to set
+     */
+    public void setStartDateType(String startDateType) {
+        this.startDateType = startDateType;
+    }
+
+    /**
+     * @return the completionDateType
+     */
+    public String getCompletionDateType() {
+        return completionDateType;
+    }
+
+    /**
+     * @param completionDateType the completionDateType to set
+     */
+    public void setCompletionDateType(String completionDateType) {
+        this.completionDateType = completionDateType;
     }
 
 
