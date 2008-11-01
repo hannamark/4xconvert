582d7a29e2a90b923c7057f916d1cfc11eb9a482
guthikondak <guthikondak@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-01 22:14:44 +0000

    added studyoutcomemeasure and studyindlde
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2719 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/domain/StudyIndlde.java
@@ -0,0 +1,170 @@
+package gov.nih.nci.pa.domain;
+
+import gov.nih.nci.pa.enums.ExpandedAccessStatusCode;
+import gov.nih.nci.pa.enums.GrantorCode;
+import gov.nih.nci.pa.enums.HolderTypeCode;
+import gov.nih.nci.pa.enums.IndldeTypeCode;
+import gov.nih.nci.pa.enums.NciDivProgHolderCode;
+import gov.nih.nci.pa.enums.NihInstHolderCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.validator.NotNull;
+
+/**
+ * 
+ * @author Kalpana Guthikonda
+ * @since 10/31/2008
+ * copyright NCI 2008.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI. 
+ */
+@Entity
+@Table(name =  "STUDY_INDLDE")
+public class StudyIndlde extends AbstractEntity {
+
+    private StudyProtocol studyProtocol;
+    private ExpandedAccessStatusCode expandedAccessStatusCode;
+    private Boolean expandedAccessIndicator;
+    private GrantorCode grantorCode;
+    private NihInstHolderCode nihInstHolderCode;
+    private NciDivProgHolderCode nciDivProgHolderCode;
+    private HolderTypeCode holderTypeCode;
+    private String indldeNumber;
+    private IndldeTypeCode indldeTypeCode;
+    
+    /**
+     * @return studyProtocol
+     */
+    @ManyToOne
+    @JoinColumn(name = "STUDY_PROTOCOL_ID", updatable = false)
+    @NotNull
+    public StudyProtocol getStudyProtocol() {
+        return studyProtocol;
+    }
+    /**
+     * @param studyProtocol studyProtocol
+     */
+    public void setStudyProtocol(StudyProtocol studyProtocol) {
+        this.studyProtocol = studyProtocol;
+    }
+    
+    /**
+     * @return expandedAccessStatusCode
+     */
+    @Column(name = "EXPANDED_ACCESS_STATUS_CODE")
+    @Enumerated(EnumType.STRING)
+    public ExpandedAccessStatusCode getExpandedAccessStatusCode() {
+        return expandedAccessStatusCode;
+    }
+    /** 
+     * @param expandedAccessStatusCode expandedAccessStatusCode
+     */
+    public void setExpandedAccessStatusCode(
+            ExpandedAccessStatusCode expandedAccessStatusCode) {
+        this.expandedAccessStatusCode = expandedAccessStatusCode;
+    }
+    /**
+     * @return expandedAccessIndicator
+     */
+    @Column(name = "EXPANDED_ACCESS_INDICATOR")
+    public Boolean getExpandedAccessIndicator() {
+        return expandedAccessIndicator;
+    }
+    /**
+     * @param expandedAccessIndicator expandedAccessIndicator
+     */
+    public void setExpandedAccessIndicator(Boolean expandedAccessIndicator) {
+        this.expandedAccessIndicator = expandedAccessIndicator;
+    }
+    /**
+     * @return grantorCode
+     */
+    @Column(name = "GRANTOR_CODE")
+    @Enumerated(EnumType.STRING)
+    public GrantorCode getGrantorCode() {
+        return grantorCode;
+    }
+    /**
+     * @param grantorCode grantorCode
+     */
+    public void setGrantorCode(GrantorCode grantorCode) {
+        this.grantorCode = grantorCode;
+    }
+    
+    /**
+     * @return nihInstHolderCode
+     */
+    @Column(name = "NIH_INST_HOLDER_CODE")
+    @Enumerated(EnumType.STRING)
+    public NihInstHolderCode getNihInstHolderCode() {
+        return nihInstHolderCode;
+    }
+    /**
+     * @param nihInstHolderCode nihInstHolderCode
+     */
+    public void setNihInstHolderCode(NihInstHolderCode nihInstHolderCode) {
+        this.nihInstHolderCode = nihInstHolderCode;
+    }
+    /**
+     * @return nciDivProgHolderCode
+     */
+    @Column(name = "NCI_DIV_PROG_HOLDER_CODE")
+    @Enumerated(EnumType.STRING)
+    public NciDivProgHolderCode getNciDivProgHolderCode() {
+        return nciDivProgHolderCode;
+    }
+    /**
+     * @param nciDivProgHolderCode nciDivProgHolderCode
+     */
+    public void setNciDivProgHolderCode(NciDivProgHolderCode nciDivProgHolderCode) {
+        this.nciDivProgHolderCode = nciDivProgHolderCode;
+    }
+    /**
+     * @return holderTypeCode
+     */
+    @Column(name = "HOLDER_TYPE_CODE")
+    @Enumerated(EnumType.STRING)
+    public HolderTypeCode getHolderTypeCode() {
+        return holderTypeCode;
+    }
+    /**
+     * @param holderTypeCode holderTypeCode
+     */
+    public void setHolderTypeCode(HolderTypeCode holderTypeCode) {
+        this.holderTypeCode = holderTypeCode;
+    }
+    /**
+     * @return indldeNumber
+     */
+    @Column(name = "INDLDE_NUMBER")
+    public String getIndldeNumber() {
+        return indldeNumber;
+    }
+    /**
+     * @param indldeNumber indldeNumber
+     */
+    public void setIndldeNumber(String indldeNumber) {
+        this.indldeNumber = indldeNumber;
+    }
+    /**
+     * @return indldeTypeCode
+     */
+    @Column(name = "INDLDE_TYPE_CODE")
+    @Enumerated(EnumType.STRING)
+    public IndldeTypeCode getIndldeTypeCode() {
+        return indldeTypeCode;
+    }
+    /**
+     * @param indldeTypeCode indldeTypeCode
+     */
+    public void setIndldeTypeCode(IndldeTypeCode indldeTypeCode) {
+        this.indldeTypeCode = indldeTypeCode;
+    }
+}
---
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/domain/StudyOutcomeMeasure.java
@@ -0,0 +1,96 @@
+package gov.nih.nci.pa.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.validator.NotNull;
+
+/**
+ * 
+ * @author Kalpana Guthikonda
+ * @since 10/29/2008
+ * copyright NCI 2008.  All rights reserved.
+ * This code may not be used without the express written permission of the
+ * copyright holder, NCI. 
+ */
+@Entity
+@Table(name =  "STUDY_OUTCOME_MEASURE")
+public class StudyOutcomeMeasure extends AbstractEntity {
+
+    private String name;
+    private String timeFrame;
+    private Boolean primaryIndicator;
+    private Boolean safetyIndicator;
+    private StudyProtocol studyProtocol;
+    
+    /**
+     * @return name
+     */
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return timeFrame
+     */
+    @Column(name = "TIMEFRAME")
+    public String getTimeFrame() {
+        return timeFrame;
+    }
+    /**
+     * @param timeFrame timeFrame
+     */
+    public void setTimeFrame(String timeFrame) {
+        this.timeFrame = timeFrame;
+    }
+    /**
+     * @return primaryIndicator
+     */
+    @Column(name = "PRIMARY_INDICATOR")
+    public Boolean getPrimaryIndicator() {
+        return primaryIndicator;
+    }
+    /**
+     * @param primaryIndicator primaryIndicator
+     */
+    public void setPrimaryIndicator(Boolean primaryIndicator) {
+        this.primaryIndicator = primaryIndicator;
+    }
+    /**
+     * @return safetyIndicator
+     */
+    @Column(name = "SAFETY_INDICATOR")
+    public Boolean getSafetyIndicator() {
+        return safetyIndicator;
+    }
+    /**
+     * @param safetyIndicator safetyIndicator
+     */
+    public void setSafetyIndicator(Boolean safetyIndicator) {
+        this.safetyIndicator = safetyIndicator;
+    }
+    /**
+     * @return protocol
+     */
+    @ManyToOne
+    @JoinColumn(name = "STUDY_PROTOCOL_ID", updatable = false)
+    @NotNull
+    public StudyProtocol getStudyProtocol() {
+        return studyProtocol;
+    }
+    /**
+     * @param studyProtocol studyProtocol
+     */
+    public void setStudyProtocol(StudyProtocol studyProtocol) {
+        this.studyProtocol = studyProtocol;
+    }
+}
