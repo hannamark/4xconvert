045caba91af25afe5e734a42fb88c1cc0d6bb836
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-02 20:55:56 +0000

    Add search().
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2723 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/InterventionServiceBean.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/InterventionServiceBean.java
@@ -5,16 +5,23 @@ package gov.nih.nci.pa.service;
 
 import gov.nih.nci.coppa.iso.Ii;
 import gov.nih.nci.pa.domain.Intervention;
+import gov.nih.nci.pa.enums.InterventionTypeCode;
 import gov.nih.nci.pa.iso.convert.InterventionConverter;
 import gov.nih.nci.pa.iso.dto.InterventionDTO;
+import gov.nih.nci.pa.iso.util.CdConverter;
 import gov.nih.nci.pa.iso.util.IiConverter;
+import gov.nih.nci.pa.iso.util.StConverter;
 import gov.nih.nci.pa.util.HibernateUtil;
 import gov.nih.nci.pa.util.PAUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.Stateless;
 
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -24,6 +31,7 @@ import org.hibernate.Session;
  *        holder, NCI.
  */
 @Stateless
+@SuppressWarnings("PMD.CyclomaticComplexity")
 public class InterventionServiceBean
         extends AbstractBasePaService<InterventionDTO>
         implements InterventionServiceRemote {
@@ -64,5 +72,61 @@ public class InterventionServiceBean
         }
         return resultDto;
     }
+
+    /**
+     * @param searchCriteria search string
+     * @return all interventions of given type with names or alternate names matching search string
+     * @throws PAException exception
+     */
+    public List<InterventionDTO> search(InterventionDTO searchCriteria) throws PAException {
+        if (searchCriteria == null) {
+            serviceError("Must pass in search criteria when calling search().");
+        }
+        if (searchCriteria.getTypeCode() == null) {
+            serviceError("Must pass in an intervention type when calling search().");
+        }
+        if (searchCriteria.getName() == null) {
+            serviceError("Must pass in a name when calling search().");
+        }
+        LOG.info("Entering search().  ");
+        List<Intervention> queryList = new ArrayList<Intervention>();
+        try {
+            Session session = HibernateUtil.getCurrentSession();
+            Query query = null;
+
+            // step 1: form the hql
+            String hql = "select distinct int "
+                       + "from Intervention int "
+                       + "left join int.interventionAlternateNames ian "
+                       + "where (upper(int.name) like upper(:name) "
+                       + "     or upper(ian.name) like upper(:name)) " 
+                       + "  and int.typeCode = :type "
+                       + "order by int.name ";
+            LOG.info("query Intervention = " + hql);
+
+            // step 2: construct query object
+            query = session.createQuery(hql);
+            query.setParameter("type", InterventionTypeCode.
+                    getByCode(CdConverter.convertCdToString(searchCriteria.getTypeCode())));
+            query.setParameter("name", "%" + StConverter.convertToString(searchCriteria.getName()) + "%");
+            
+            // step 3: query the result
+            queryList = query.list();
+        } catch (HibernateException hbe) {
+            serviceError("Hibernate exception in search.().  ", hbe);
+        }
+        ArrayList<InterventionDTO> resultList = new ArrayList<InterventionDTO>();
+        Long lastId = null;
+        for (Intervention bo : queryList) {
+            if (lastId != bo.getId()) {
+              resultList.add(InterventionConverter.convertFromDomainToDTO(bo));
+              lastId = bo.getId();
+            }
+        }
+
+        LOG.info("Leaving search(), returning " + resultList.size() + " object(s).");
+        return resultList;
+    }
+
 
 }
