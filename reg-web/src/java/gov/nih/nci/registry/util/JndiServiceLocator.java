2f99f6fb5d2d3cac0eaa5f1f70f1123cf1272a51
gaoh <gaoh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-06 15:18:02 +0000

    Added StudyIndldeServiceRemote
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2800 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- reg-web/src/java/gov/nih/nci/registry/util/JndiServiceLocator.java
+++ reg-web/src/java/gov/nih/nci/registry/util/JndiServiceLocator.java
@@ -3,6 +3,7 @@ package gov.nih.nci.registry.util;
 import gov.nih.nci.pa.service.DiseaseCondServiceRemote;
 import gov.nih.nci.pa.service.DocumentServiceRemote;
 import gov.nih.nci.pa.service.StudyContactServiceRemote;
+import gov.nih.nci.pa.service.StudyIndldeServiceRemote;
 import gov.nih.nci.pa.service.StudyOverallStatusServiceRemote;
 import gov.nih.nci.pa.service.StudyParticipationServiceRemote;
 import gov.nih.nci.pa.service.StudyProtocolServiceRemote;
@@ -140,8 +141,16 @@ public class JndiServiceLocator implements ServiceLocator {
      *
      * @return StudyContactService
      */
-    public StudyContactServiceRemote getStudyContactService() {
-        return (StudyContactServiceRemote) JNDIUtil.lookup("pa/StudyContactServiceBean/remote");
+    public StudyIndldeServiceRemote getStudyIndldeService() {
+        return (StudyIndldeServiceRemote) JNDIUtil.lookup("pa/StudyIndldeServiceBean/remote");
     }
+    
+    /**
+    *
+    * @return StudyContactService
+    */
+   public StudyContactServiceRemote getStudyContactService() {
+       return (StudyContactServiceRemote) JNDIUtil.lookup("pa/StudyContactServiceBean/remote");
+   }
 
 }
