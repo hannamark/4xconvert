f6b0667d785fa346081bd0e7c36daf91d0ac2b2f
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-09 22:45:36 +0000

    Action class for register user
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2902 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/action/RegisterUserAction.java
@@ -0,0 +1,294 @@
+/**
+ * 
+ */
+package gov.nih.nci.registry.action;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import gov.nih.nci.pa.domain.RegistryUser;
+import gov.nih.nci.pa.service.util.CSMUserService;
+import gov.nih.nci.pa.util.PAUtil;
+import gov.nih.nci.registry.util.Constants;
+import gov.nih.nci.registry.util.EncoderDecoder;
+import gov.nih.nci.registry.util.RegistryServiceLocator;
+import gov.nih.nci.registry.dto.RegistryUserWebDTO;
+import gov.nih.nci.registry.mail.MailManager;
+import gov.nih.nci.security.authorization.domainobjects.User;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+
+/**
+ * @author Bala Nair
+ *
+ */
+
+@SuppressWarnings({ "PMD" })
+@Validation
+public class RegisterUserAction extends ActionSupport {
+    
+    private static final Logger LOG  = Logger.getLogger(RegisterUserAction.class);
+    private RegistryUserWebDTO registryUserWebDTO = new RegistryUserWebDTO();
+
+    /**
+     * Send e-mail to the registering user.
+     * @return String
+     */
+    public String sendMail() {
+
+        try {
+            // record found, send him an email
+            final MailManager mailManager = new MailManager();
+            // first validate the form fields before sending e-mail
+            validateForm(false);
+            if (hasFieldErrors()) {
+                return Constants.REGISTER_USER_ERROR;
+            }
+            mailManager.sendConfirmationMail(
+                    registryUserWebDTO.getLoginName(), registryUserWebDTO.getPassword());
+            LOG.info(" sending email to " + registryUserWebDTO.getLoginName());
+            ServletActionContext.getRequest().
+                                setAttribute("emailAddress" , registryUserWebDTO.getLoginName());
+            
+        } catch (Exception ex) {
+            LOG.error("error while sending e-mail");
+            return Constants.APPLICATION_ERROR;
+
+        }
+        return Constants.CONFIRMATION;
+
+    }
+    
+    /**
+     * activate registry user.
+     * @return String
+     */
+    public String activate() {
+
+        String decodedLoginName = null;
+        String decodedPassword  = null;
+        try {
+            String loginName = ServletActionContext.getRequest().
+                                    getParameter("loginName");
+            String password = ServletActionContext.getRequest().
+                                    getParameter("password");
+            
+            // decode only if the value is not null, since user can select my account link
+            EncoderDecoder encoderDecoder = new gov.nih.nci.registry.util.EncoderDecoder();
+            if (loginName != null) {
+               decodedLoginName =  encoderDecoder.
+                                           decodeString(loginName);
+               ServletActionContext.getRequest().
+                                       setAttribute("loginName" , decodedLoginName);
+               
+            }
+            if (password != null) {
+               decodedPassword =  encoderDecoder.
+                                           decodeString(password);
+               ServletActionContext.getRequest().
+                                      setAttribute("password" , decodedPassword);
+    
+            }
+            
+            // check if user already exists
+            RegistryUser registryUser = RegistryServiceLocator.getRegistryUserService().
+                                            getUser(decodedLoginName);
+            
+            if (registryUser != null) {
+                registryUserWebDTO = new RegistryUserWebDTO(
+                                           registryUser, decodedLoginName, decodedPassword);
+              } else {
+                registryUserWebDTO.setLoginName(decodedLoginName);
+                registryUserWebDTO.setPassword(decodedPassword);
+                registryUserWebDTO.setRetypePassword(decodedPassword);
+                  ServletActionContext.getRequest().setAttribute("isNewUser", true);
+            }
+        } catch (Exception ex) {
+            LOG.error("error while activating user :" + decodedLoginName);
+            return Constants.APPLICATION_ERROR;
+            
+        }        
+        // show the My Account page
+        return Constants.MY_ACCOUNT;
+
+    }
+    
+    /**
+     * Show My Account Page.
+     * @return String
+     */
+    public String showMyAccount() {
+        String loginName = null;
+        try {
+            //String loginName = (String) ServletActionContext.getRequest().
+            //                                getSession().getAttribute("j_username");
+            loginName =  ServletActionContext.
+                                        getRequest().getRemoteUser();
+            String password = (String) ServletActionContext.getRequest().
+                                        getSession().getAttribute("j_password"); 
+            // retrieve user info
+            RegistryUser registryUser = RegistryServiceLocator.getRegistryUserService().
+                                            getUser(loginName);
+            
+            if (registryUser != null) {
+                registryUserWebDTO = new RegistryUserWebDTO(
+                                           registryUser, loginName, password);
+            }
+        } catch (Exception ex) {
+            LOG.error("error while displaying My Account page for user :" + loginName);
+            return Constants.APPLICATION_ERROR;
+            
+        }
+        
+        // show the My Account page
+        return Constants.MY_ACCOUNT;
+
+    }
+    
+    /**
+     * create/update registry user.
+     * @return String
+     */
+    public String updateAccount() {
+        try {
+            // first validate the form fields before creating user
+            validateForm(true);
+            if (hasFieldErrors()) {
+                return Constants.MY_ACCOUNT_ERROR;
+            }
+            
+            // convert RegistryUserWebDTO to RegistryUser
+            RegistryUser registryUser = new RegistryUser();
+            if (registryUserWebDTO.getId() != null 
+                    && registryUserWebDTO.getId().length() > 0) {
+                        registryUser.setId(new Long(
+                                registryUserWebDTO.getId()));
+            }
+            if (registryUserWebDTO.getCsmUserId() != null
+                    && registryUserWebDTO.getCsmUserId().length() > 0) {
+                        registryUser.setCsmUserId(new Long(
+                                registryUserWebDTO.getCsmUserId()));
+            } 
+            registryUser.setFirstName(registryUserWebDTO.getFirstName());
+            registryUser.setLastName(registryUserWebDTO.getLastName());
+            registryUser.setMiddleName(registryUserWebDTO.getMiddleName());
+            registryUser.setAddressLine(registryUserWebDTO.getAddressLine());
+            registryUser.setCity(registryUserWebDTO.getCity());
+            registryUser.setState(registryUserWebDTO.getState());
+            registryUser.setPostalCode(registryUserWebDTO.getPostalCode());
+            registryUser.setCountry(registryUserWebDTO.getCountry());
+            registryUser.setPhone(registryUserWebDTO.getPhone());
+            registryUser.setAffiliateOrg(registryUserWebDTO.getAffiliateOrg());       
+            
+            // check if it's  update action
+            if (registryUser.getId() != null) {
+                // first update the CSM user
+                User csmUser = CSMUserService.getInstance().
+                                        updateCSMUser(registryUser, 
+                                        registryUserWebDTO.getLoginName(), 
+                                        registryUserWebDTO.getPassword());
+                
+                //now update the RegistryUser
+                registryUser =  RegistryServiceLocator.getRegistryUserService().
+                                        updateUser(registryUser); 
+                
+            } else { //create user
+                // first create the CSM user
+                User csmUser = CSMUserService.getInstance().
+                                        createCSMUser(registryUser, 
+                                        registryUserWebDTO.getLoginName(), 
+                                        registryUserWebDTO.getPassword());
+                
+                if (csmUser != null) {
+                    registryUser.setCsmUserId(csmUser.getUserId());
+                }
+                
+                //now create the RegistryUser
+                registryUser =  RegistryServiceLocator.getRegistryUserService().
+                                        createUser(registryUser);           
+    
+            }
+        } catch (Exception ex) {
+            LOG.error("error while updating user info");
+            return Constants.APPLICATION_ERROR;
+            
+        }
+        // show the My Account page
+        return Constants.MY_ACCOUNT;
+
+    }
+    
+    /**
+     * @return the registryUserWebDTO
+     */
+    public RegistryUserWebDTO getRegistryUserWebDTO() {
+        return registryUserWebDTO;
+    }
+
+    /**
+     * @param registryUserWebDTO the registryUserWebDTO to set
+     */
+    public void setRegistryUserWebDTO(RegistryUserWebDTO registryUserWebDTO) {
+        this.registryUserWebDTO = registryUserWebDTO;
+    }
+    
+    
+    /**
+     * validate the  form elements.
+     */
+    private void validateForm(boolean isMyAccountPage)  {
+        if (PAUtil.isEmpty(registryUserWebDTO.getLoginName())) {
+            addFieldError("registryUserWebDTO.loginName",
+                    getText("error.register.emailAddress"));
+        }
+        if (PAUtil.isEmpty(registryUserWebDTO.getPassword())) {
+            addFieldError("registryUserWebDTO.password",
+                    getText("error.register.password"));
+        }
+        if (PAUtil.isEmpty(registryUserWebDTO.getRetypePassword())) {
+            addFieldError("registryUserWebDTO.retypePassword",
+                    getText("error.register.retypePassword"));
+        }
+        if (!PAUtil.isEmpty(registryUserWebDTO.getPassword()) 
+                && !PAUtil.isEmpty(registryUserWebDTO.getRetypePassword())) {            
+            if (!registryUserWebDTO.getPassword().equals(
+                        registryUserWebDTO.getRetypePassword())) {
+                addFieldError("registryUserWebDTO.retypePassword",
+                    getText("error.register.matchPassword"));
+            }
+        }
+        if (!PAUtil.isEmpty(registryUserWebDTO.getPassword())) {            
+            if (registryUserWebDTO.getPassword().length() 
+                            < Constants.MIN_PASSWORD_LENGTH) {
+                addFieldError("registryUserWebDTO.password",
+                    getText("error.register.passwordLength"));
+            }
+        }
+        // if it's My Account page validate required fields
+        if (isMyAccountPage) {
+            if (PAUtil.isEmpty(registryUserWebDTO.getFirstName())) {
+                addFieldError("registryUserWebDTO.firstName",
+                        getText("error.register.firstName"));
+            }
+            if (PAUtil.isEmpty(registryUserWebDTO.getLastName())) {
+                addFieldError("registryUserWebDTO.lastName",
+                        getText("error.register.lastName"));
+            }
+            if (PAUtil.isEmpty(registryUserWebDTO.getState())) {
+                addFieldError("registryUserWebDTO.state",
+                        getText("error.register.state"));
+            }
+            if (PAUtil.isEmpty(registryUserWebDTO.getCountry())) {
+                addFieldError("registryUserWebDTO.country",
+                        getText("error.register.country"));
+            }
+            if (PAUtil.isEmpty(registryUserWebDTO.getAffiliateOrg())) {
+                addFieldError("registryUserWebDTO.affiliateOrg",
+                        getText("error.register.affiliateOrg"));
+            }
+        }
+    }
+
+}
