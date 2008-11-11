518fcab5c51e15217929370997f94f4fbd9649c6
gaoh <gaoh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-11 16:24:30 +0000

    Masking DocumentWorkflowStatusCode
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2953 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- reg-web/src/java/gov/nih/nci/registry/decorator/RegistryDisplayTagDecorator.java
+++ reg-web/src/java/gov/nih/nci/registry/decorator/RegistryDisplayTagDecorator.java
@@ -3,8 +3,10 @@ package gov.nih.nci.registry.decorator;
 import java.util.Date;
 import org.displaytag.decorator.TableDecorator;
 import org.apache.commons.lang.time.FastDateFormat;
+import org.apache.struts2.ServletActionContext;
 
 import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+import gov.nih.nci.pa.enums.DocumentWorkflowStatusCode;
 
 /**
  * tag decorator registry.
@@ -12,6 +14,10 @@ import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
  *
  */
 public class RegistryDisplayTagDecorator extends TableDecorator {
+    
+    private DocumentWorkflowStatusCode documentWorkflowStatusCode;
+    private String loginUser;
+    private String userLastCreated;
 
     /**
      *
@@ -40,5 +46,29 @@ public class RegistryDisplayTagDecorator extends TableDecorator {
         }
 
     }
+    
+    /**
+    *
+    * @return masked processing status
+    */
+   public DocumentWorkflowStatusCode getDocumentWorkflowStatusCode() {
+       documentWorkflowStatusCode = ((StudyProtocolQueryDTO)
+               this.getCurrentRowObject()).getDocumentWorkflowStatusCode();
+       if (documentWorkflowStatusCode != null) {
+           loginUser =  ServletActionContext.getRequest().getRemoteUser();
+           if (((StudyProtocolQueryDTO) this.getCurrentRowObject()).getUserLastCreated() == null) {
+               return null;
+           } else {
+           userLastCreated = ((StudyProtocolQueryDTO) this.getCurrentRowObject()).getUserLastCreated();
+           }
+       if (loginUser.equalsIgnoreCase(userLastCreated)) {
+               return documentWorkflowStatusCode;
+           } else {
+               return null; 
+           }
+       } else {
+           return null;
+       }
+   }
 
 }
