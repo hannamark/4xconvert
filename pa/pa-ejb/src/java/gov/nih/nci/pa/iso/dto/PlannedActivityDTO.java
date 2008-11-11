a205bd7dc97bd07a9d4ccb1c66b26fbc3c2df480
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-11-11 20:10:26 +0000

    Remove unused attributes for PlannedActivityDTO.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@2966 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/iso/dto/PlannedActivityDTO.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/iso/dto/PlannedActivityDTO.java
@@ -3,7 +3,6 @@ package gov.nih.nci.pa.iso.dto;
 import gov.nih.nci.coppa.iso.Bl;
 import gov.nih.nci.coppa.iso.Cd;
 import gov.nih.nci.coppa.iso.Ii;
-import gov.nih.nci.coppa.iso.St;
 
 /**
  * @author Hugh Reinhart
@@ -12,40 +11,12 @@ import gov.nih.nci.coppa.iso.St;
  * copyright NCI 2008.  All rights reserved.
  * This code may not be used without the express written permission of the copyright holder, NCI.
  */
-public class PlannedActivityDTO extends BaseDTO {
+public class PlannedActivityDTO extends StudyDTO {
     private static final long serialVersionUID = 1238767890L;
-    private Ii studyProtocolIdentifier;
-    private St alternateName;
     private Cd categoryCode;
-    private St descriptionText;
     private Ii interventionIdentifier;
     private Bl leadProductIndicator;
-    private St name;
     private Cd subcategoryCode;
-    /**
-     * @return the studyProtocolIdentifier
-     */
-    public Ii getStudyProtocolIdentifier() {
-        return studyProtocolIdentifier;
-    }
-    /**
-     * @param studyProtocolIdentifier the studyProtocolIdentifier to set
-     */
-    public void setStudyProtocolIdentifier(Ii studyProtocolIdentifier) {
-        this.studyProtocolIdentifier = studyProtocolIdentifier;
-    }
-    /**
-     * @return the alternateName
-     */
-    public St getAlternateName() {
-        return alternateName;
-    }
-    /**
-     * @param alternateName the alternateName to set
-     */
-    public void setAlternateName(St alternateName) {
-        this.alternateName = alternateName;
-    }
     /**
      * @return the categoryCode
      */
@@ -57,18 +28,6 @@ public class PlannedActivityDTO extends BaseDTO {
      */
     public void setCategoryCode(Cd categoryCode) {
         this.categoryCode = categoryCode;
-    }
-    /**
-     * @return the descriptionText
-     */
-    public St getDescriptionText() {
-        return descriptionText;
-    }
-    /**
-     * @param descriptionText the descriptionText to set
-     */
-    public void setDescriptionText(St descriptionText) {
-        this.descriptionText = descriptionText;
     }
     /**
      * @return the interventionIdentifier
@@ -93,18 +52,6 @@ public class PlannedActivityDTO extends BaseDTO {
      */
     public void setLeadProductIndicator(Bl leadProductIndicator) {
         this.leadProductIndicator = leadProductIndicator;
-    }
-    /**
-     * @return the name
-     */
-    public St getName() {
-        return name;
-    }
-    /**
-     * @param name the name to set
-     */
-    public void setName(St name) {
-        this.name = name;
     }
     /**
      * @return the subcategoryCode
