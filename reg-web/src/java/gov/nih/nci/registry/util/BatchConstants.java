d28d04cd2094e7ee950d5fb23265b87f2807fc5b
niktev <niktev@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-12-09 23:04:13 +0000

    Registration Batch Upload
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@3643 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/util/BatchConstants.java
@@ -0,0 +1,212 @@
+package gov.nih.nci.registry.util;
+/**
+ * 
+ * @author Vrushali
+ *
+ */
+public class BatchConstants {
+    /** */
+    public static final int READ_SIZE = 8192;
+     /** */
+    public static final short DATE_FORMAT = 15;
+    /** */
+    public static final int UNIQUE_TRIAL_IDENTIFIER = 0;
+    /** */
+    public static final int LEAD_ORGANIZATION_TRIAL_IDENTIFIER = 1;
+    /** */
+    public static final int TITLE = 2;
+    /** */
+    public static final int TRIAL_TYPE = 3;
+    /** */
+    public static final int PRIMARY_PURPOSE = 4;
+    /** */
+    public static final int PRIMARY_PURPOSE_OTHER_VALUE_SP = 5;
+    /** */
+    public static final int PHASE = 6;
+    /** */
+    public static final int PHASE_OTHER_VALUE_SP = 7;
+    /** */
+    public static final int SPONSOR_ORG_NAME = 8;
+    /** */
+    public static final int SPONSOR_CETP_ORG_NO = 9;
+    /** */
+    public static final int SPONSOR_STREET_ADDRESS = 10;
+    /** */
+    public static final int SPONSOR_CITY = 11;
+    /** */
+    public static final int SPONSOR_STATE = 12;
+    /** */
+    public static final int SPONSOR_ZIP = 13;
+    /** */
+    public static final int SPONSOR_COUNTRY = 14;
+    /** */
+    public static final int SPONSOR_EMAIL = 15;
+    /** */
+    public static final int SPONSOR_PHONE = 16;
+    /** */
+    public static final int SPONSOR_TTY = 17;
+    /** */
+    public static final int SPONSOR_FAX = 18;
+    /** */
+    public static final int SPONSOR_URL = 19;
+    /** */
+    public static final int RESPONSIBLE_PARTY = 20;
+    /** */
+    public static final int SPONSOR_CONTACT_FIRST_NAME = 21;
+    /** */
+    public static final int SPONSOR_CONTACT_MIDDLE_NAME = 22;
+    /** */
+    public static final int SPONSOR_CONTACT_LAST_NAME = 23;
+    /** */
+    public static final int SPONSOR_CONTACT_CTEP_PERSON_NO = 24;
+    /** */
+    public static final int SPONSOR_CONTACT_STREET_ADDRESS = 25;
+    /** */
+    public static final int SPONSOR_CONTACT_CITY = 26;
+    /** */
+    public static final int SPONSOR_CONTACT_STATE = 27;
+    /** */
+    public static final int SPONSOR_CONTACT_ZIP = 28;
+    /** */
+    public static final int SPONSOR_CONTACT_COUNTRY = 29;
+    /** */
+    public static final int SPONSOR_CONTACT_EMAIL_ID = 30;
+    /** */
+    public static final int SPONSOR_CONTACT_PHONE = 31;
+    /** */
+    public static final int SPONSOR_CONTACT_TTY = 32;
+    /** */
+    public static final int SPONSOR_CONTACT_FAX = 33;
+    /** */
+    public static final int SPONSOR_CONTACT_URL = 34;
+    
+    /** */
+    public static final int LEAD_ORG_NAME = 35;
+    /** */
+    public static final int LEAD_ORG_CTEP_IDENTIFIER = 36;
+    /** */
+    public static final int LEAD_ORG_STREET_ADDRESS = 37;
+    /** */
+    public static final int LEAD_ORG_CITY = 38;
+    /** */
+    public static final int LEAD_ORG_STATE = 39;
+    /** */
+    public static final int LEAD_ORG_ZIP = 40;
+    /** */
+    public static final int LEAD_ORG_COUNTRY = 41;
+    /** */
+    public static final int LEAD_ORG_EMAIL = 42;
+    /** */
+    public static final int LEAD_ORG_PHONE = 43;
+    /** */
+    public static final int LEAD_ORG_TTY = 44;
+    /** */
+    public static final int LEAD_ORG_FAX = 45;
+    /** */
+    public static final int LEAD_ORG_URL = 46;
+    /** */
+    public static final int LEAD_ORG_TYPE = 47;
+    /** */
+    public static final int PI_FIRST_NAME = 48;
+    /** */
+    public static final int PI_MIDDLE_NAME = 49;
+    /** */
+    public static final int PI_LAST_NAME = 50;
+    /** */
+    public static final int PI_PERSON_CTEP_PERSON_NUMBER = 51;
+    /** */
+    public static final int PI_STREET_ADDRESS = 52;
+    /** */
+    public static final int PI_CITY = 53;
+    /** */
+    public static final int PI_STATE = 54;
+    /** */
+    public static final int PI_ZIP = 55;
+    /** */
+    public static final int PI_COUNTRY = 56;
+    /** */
+    public static final int PI_EMAIL = 57;
+    /** */
+    public static final int PI_PHONE = 58;
+    /** */
+    public static final int PI_TTY = 59;
+    /** */
+    public static final int PI_FAX = 60;
+    /** */
+    public static final int PI_URL = 61;
+    /** */
+    public static final int S4_FUND_CAT = 62;
+    /** */
+    public static final int S4_FUND_ORG_NAME = 63;
+    /** */
+    public static final int S4_FUND_ORG_CTEP_ORG_NO = 64;
+    /** */
+    public static final int S4_FUND_ORG_STREET_ADDRESS = 65;
+    /** */
+    public static final int S4_FUND_CITY = 66;
+    /** */
+    public static final int S4_FUND_STATE = 67;
+    /** */
+    public static final int S4_FUND_ZIP = 68;
+    /** */
+    public static final int S4_FUND_COUNTRY = 69;
+    /** */
+    public static final int S4_FUND_EMAIL = 70;
+    /** */
+    public static final int S4_FUND_PHONE = 71;
+    /** */
+    public static final int S4_FUND_TTY = 72;
+    /** */
+    public static final int S4_FUND_FAX = 73;
+    /** */
+    public static final int S4_FUND_URL = 74;
+    /** */
+    
+    public static final int NIH_GRANT_FUND_MC = 75;
+    /** */
+    public static final int NIH_GRANT_INSTITUTE_CODE = 76;
+    /** */
+    public static final int NIH_GRANT_SR_NO = 77;
+    /** */
+    public static final int NIH_GRANT_NCI_DIV_CODE = 78;
+    /** */
+    public static final int CURRENT_TRIAL_STATUS = 79;
+    /** */
+    public static final int CURRENT_TRIAL_STATUS_DATE = 80;
+    /** */
+    public static final int STUDY_START_DATE = 81;
+    /** */
+    public static final int STUDY_START_DATE_TYPE = 82;
+    /** */
+    public static final int PRIMARY_COMP_DATE = 83;
+    /** */
+    public static final int PRIMARY_COMP_DATE_TYPE = 84;
+    /** */
+    public static final int IND_TYPE = 85;
+    /** */
+    public static final int IND_NUMBER = 86;
+    /** */
+    public static final int IND_GRANTOR = 87;
+    /** */
+    public static final int IND_HOLDER_TYPE = 88;
+    /** */
+    public static final int IND_NIH_INSTITUTION = 89;
+    /** */
+    public static final int IND_NCI_DIV_CODE = 90;
+    /** */
+    public static final int IND_HAS_EXPANDED_ACCESS = 91;
+    /** */
+    public static final int IND_EXPANED_ACCESS_STATUS = 92;
+    /** */
+    public static final int PROTOCOL_DOC_FILE_NAME = 93;
+    /** */
+    public static final int IRB_APPROVAL_DOC_FILE_NAME = 94;
+    /** */
+    public static final int PARTICIPATIING_SITE_DOC_FILE_NAME = 95;
+    /** */
+    public static final int INFORMED_CONSENT_DOC_FILE_NAME = 96;
+    /** */
+    public static final int OTHER_TRIAL_DOC_FILE_NAME = 97;
+    
+    
+}
