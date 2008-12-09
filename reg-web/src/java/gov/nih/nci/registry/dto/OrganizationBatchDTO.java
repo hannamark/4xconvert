cb90e530cb21a72a7614adef683f997145e8845f
niktev <niktev@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-12-09 23:02:38 +0000

    Registration Batch Upload
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@3640 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/java/gov/nih/nci/registry/dto/OrganizationBatchDTO.java
@@ -0,0 +1,179 @@
+package gov.nih.nci.registry.dto;
+/**
+ * 
+ * @author Vrushali
+ *
+ */
+
+public class OrganizationBatchDTO {
+    private String name;
+    private String orgCTEPId;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+    private String email;
+    private String phone;
+    private String type;
+    private String tty;
+    private String fax;
+    private String url;
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return the orgCTEPId
+     */
+    public String getOrgCTEPId() {
+        return orgCTEPId;
+    }
+    /**
+     * @param orgCTEPId the orgCTEPId to set
+     */
+    public void setOrgCTEPId(String orgCTEPId) {
+        this.orgCTEPId = orgCTEPId;
+    }
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+    /**
+     * @return the streetAddress
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+    /**
+     * @param streetAddress the streetAddress to set
+     */
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    /**
+     * @return the zip
+     */
+    public String getZip() {
+        return zip;
+    }
+    /**
+     * @param zip the zip to set
+     */
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    /**
+     * @return the fax
+     */
+    public String getFax() {
+        return fax;
+    }
+    /**
+     * @param fax the fax to set
+     */
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+    /**
+     * @return the tty
+     */
+    public String getTty() {
+        return tty;
+    }
+    /**
+     * @param tty the tty to set
+     */
+    public void setTty(String tty) {
+        this.tty = tty;
+    }
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
