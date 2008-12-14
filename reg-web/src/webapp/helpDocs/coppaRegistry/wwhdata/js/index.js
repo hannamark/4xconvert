bbe3abdfcbb691d7090c7b4f12853d3d72c1f774
bnair <bnair@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-12-14 05:22:34 +0000

    updated online help
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@3748 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/webapp/helpDocs/coppaRegistry/wwhdata/js/index.js
@@ -0,0 +1,40 @@
+function  WWHBookData_AddIndexEntries(P)
+{
+var A=P.fA("A",null,null,"002");
+var B=A.fA("Application Support",new Array("37#1083557"));
+A=P.fA("B",null,null,"002");
+B=A.fA("Becoming a registered user",new Array("1"));
+A=P.fA("L",null,null,"002");
+B=A.fA("login",new Array("6#1081540"));
+var C=B.fA("editing",new Array("31#1064243"));
+A=P.fA("N",null,null,"002");
+B=A.fA("NCICB Application Support",new Array("37#1083557"));
+B=A.fA("new user");
+C=B.fA("register",new Array("5#1080273"));
+A=P.fA("O",null,null,"002");
+B=A.fA("Online help");
+C=B.fA("locating topics of interest",new Array("1#1086625"));
+C=B.fA("printing",new Array("1#1086653"));
+C=B.fA("using navigation tools",new Array("2"));
+B=A.fA("Online help, using",new Array("1"));
+A=P.fA("P",null,null,"002");
+B=A.fA("Principal Investigator",new Array("35#1133279"));
+B=A.fA("protocols");
+C=B.fA("searching",new Array("9#1101098"));
+C=B.fA("submitting",new Array("15#1064030"));
+A=P.fA("R",null,null,"002");
+B=A.fA("registration",new Array("5#1080273"));
+B=A.fA("Registration",new Array("1"));
+A=P.fA("T",null,null,"002");
+B=A.fA("trials");
+C=B.fA("viewing details",new Array("12#1103560"));
+A=P.fA("U",null,null,"002");
+B=A.fA("user");
+C=B.fA("editing",new Array("31#1064243"));
+B=A.fA("Using online help",new Array("1"));
+}
+
+function  WWHBookData_MaxIndexLevel()
+{
+  return 3;
+}
