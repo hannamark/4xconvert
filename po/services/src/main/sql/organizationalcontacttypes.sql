cbe9cda666e1c430789072940d158ea6ec39023e
smiller <smiller@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2008-12-18 02:02:11 +0000

    PO-782 - peer review bmason
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@3905 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/services/src/main/sql/organizationalcontacttypes.sql
+++ po/services/src/main/sql/organizationalcontacttypes.sql
@@ -1,2 +1,1 @@
-insert into organizationalcontacttype (id, code) values (1, 'For drug shipment');
-insert into organizationalcontacttype (id, code) values (2, 'For safety issues');
+insert into organizationalcontacttype (id, code) values (1, 'IRB');
