a4883b530889aa921f5b68e7f1815c90c8de3491
guthikondak <guthikondak@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-01-13 17:49:40 +0000

    fixed the defects 18615 and 18379;check for duplicate Trial INDIDE
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@4096 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/AbstractionCompletionServiceBean.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/AbstractionCompletionServiceBean.java
@@ -1,6 +1,61 @@
+/*
+ * Copyright Notice. Copyright 2008, ScenPro, Inc, (“caBIG™ Participant”). The Protocol Abstraction (PA) Application was
+ * created with NCI funding and is part of the caBIG™ initiative. The software subject to this notice and license
+ * includes both human readable source code form and machine readable, binary, object code form (the “caBIG™ Software”).
+ * This caBIG™ Software License (the “License”) is between caBIG™ Participant and You. “You (or “Your”) shall mean a
+ * person or an entity, and all other entities that control, are controlled by, or are under common control with the
+ * entity. “Control” for purposes of this definition means (i) the direct or indirect power to cause the direction or
+ * management of such entity, whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the
+ * outstanding shares, or (iii) beneficial ownership of such entity. License. Provided that You agree to the conditions
+ * described below, caBIG™ Participant grants You a non-exclusive, worldwide, perpetual, fully-paid-up, no-charge,
+ * irrevocable, transferable and royalty-free right and license in its rights in the caBIG™ Software, including any
+ * copyright or patent rights therein, to (i) use, install, disclose, access, operate, execute, reproduce, copy, modify,
+ * translate, market, publicly display, publicly perform, and prepare derivative works of the caBIG™ Software in any
+ * manner and for any purpose, and to have or permit others to do so; (ii) make, have made, use, practice, sell, and
+ * offer for sale, import, and/or otherwise dispose of caBIG™ Software (or portions thereof); (iii) distribute and have
+ * distributed to and by third parties the caBIG™ Software and any modifications and derivative works thereof; and (iv)
+ * sublicense the foregoing rights set out in (i), (ii) and (iii) to third parties, including the right to license such
+ * rights to further third parties. For sake of clarity, and not by way of limitation, caBIG™ Participant shall have no
+ * right of accounting or right of payment from You or Your sub licensees for the rights granted under this License.
+ * This License is granted at no charge to You. Your downloading, copying, modifying, displaying, distributing or use of
+ * caBIG™ Software constitutes acceptance of all of the terms and conditions of this Agreement. If You do not agree to
+ * such terms and conditions, You have no right to download, copy, modify, display, distribute or use the caBIG™
+ * Software. 1. Your redistributions of the source code for the caBIG™ Software must retain the above copyright notice,
+ * this list of conditions and the disclaimer and limitation of liability of Article 6 below. Your redistributions in
+ * object code form must reproduce the above copyright notice, this list of conditions and the disclaimer of Article 6
+ * in the documentation and/or other materials provided with the distribution, if any. 2. Your end-user documentation
+ * included with the redistribution, if any, must include the following acknowledgment: “This product includes software
+ * developed by ScenPro, Inc.” If You do not include such end-user documentation, You shall include this acknowledgment
+ * in the caBIG™ Software itself, wherever such third-party acknowledgments normally appear. 3. You may not use the
+ * names “ScenPro, Inc.”, “The National Cancer Institute”, “NCI”, “Cancer Bioinformatics Grid” or “caBIG™” to endorse or
+ * promote products derived from this caBIG™ Software. This License does not authorize You to use any trademarks,
+ * service marks, trade names, logos or product names of either caBIG™ Participant, NCI or caBIG™, except as required to
+ * comply with the terms of this License. 4. For sake of clarity, and not by way of limitation, You may incorporate this
+ * caBIG™ Software into Your proprietary programs and into any third party proprietary programs. However, if You
+ * incorporate the caBIG™ Software into third party proprietary programs, You agree that You are solely responsible for
+ * obtaining any permission from such third parties required to incorporate the caBIG™ Software into such third party
+ * proprietary programs and for informing Your sub licensees, including without limitation Your end-users, of their
+ * obligation to secure any required permissions from such third parties before incorporating the caBIG™ Software into
+ * such third party proprietary software programs. In the event that You fail to obtain such permissions, You agree to
+ * indemnify caBIG™ Participant for any claims against caBIG™ Participant by such third parties, except to the extent
+ * prohibited by law, resulting from Your failure to obtain such permissions. 5. For sake of clarity, and not by way of
+ * limitation, You may add Your own copyright statement to Your modifications and to the derivative works, and You may
+ * provide additional or different license terms and conditions in Your sublicenses of modifications of the caBIG™
+ * Software, or any derivative works of the caBIG™ Software as a whole, provided Your use, reproduction, and
+ * distribution of the Work otherwise complies with the conditions stated in this License. 6. THIS caBIG™ SOFTWARE IS
+ * PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * ScenPro, Inc. OR ITS AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS caBIG™ SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 package gov.nih.nci.pa.service.util;
 
 import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.pa.domain.Organization;
 import gov.nih.nci.pa.dto.AbstractionCompletionDTO;
 import gov.nih.nci.pa.enums.ActivityCategoryCode;
 import gov.nih.nci.pa.enums.ArmTypeCode;
@@ -14,6 +69,7 @@ import gov.nih.nci.pa.iso.dto.ObservationalStudyProtocolDTO;
 import gov.nih.nci.pa.iso.dto.PlannedActivityDTO;
 import gov.nih.nci.pa.iso.dto.PlannedEligibilityCriterionDTO;
 import gov.nih.nci.pa.iso.dto.StudyDiseaseDTO;
+import gov.nih.nci.pa.iso.dto.StudyIndldeDTO;
 import gov.nih.nci.pa.iso.dto.StudyOutcomeMeasureDTO;
 import gov.nih.nci.pa.iso.dto.StudyOverallStatusDTO;
 import gov.nih.nci.pa.iso.dto.StudyParticipationContactDTO;
@@ -67,10 +123,9 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
 
     enforceRegulatoryInfo(studyProtocolIi);
     
-    //enforceTrialINDIDE(studyProtocolIi);
+    enforceTrialINDIDE(studyProtocolIi);
     
-    enforceTrialStatus(studyProtocolIi, studyProtocolDTO);
-    
+    enforceTrialStatus(studyProtocolIi, studyProtocolDTO);    
     
     List<DocumentDTO> isoList = PoPaServiceBeanLookup.getDocumentService()
     .getDocumentsByStudyProtocol(studyProtocolIi);
@@ -101,7 +156,7 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
           webDTO.setErrorType("Error");
           webDTO.setComment("Select Arm from Interventional Trial Design under Scientific Data menu.");
           webDTO.setErrorDescription("Number of interventional trial arm records must be the same"
-              + "as Number of Arms assigned in ‘Interventional Trial Design’.");
+              + " as Number of Arms assigned in ‘Interventional Trial Design’.");
           abstractionList.add(webDTO);
         }
       }
@@ -231,18 +286,36 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
     }
   }
 
-
-  /*private void enforceTrialINDIDE(Ii studyProtocolIi) throws PAException {
-    List<StudyIndldeDTO> siList = PoPaServiceBeanLookup.getStudyIndldeService().getByStudyProtocol(studyProtocolIi);
-    if (!(siList.isEmpty())) { // NOPMD
-    } else {
-      AbstractionCompletionDTO webDTO = new AbstractionCompletionDTO();
-      webDTO.setErrorType("Error");
-      webDTO.setComment("Select Trial IND/IDE under Regulatory Information from Administrative Data menu.");
-      webDTO.setErrorDescription("No IND/IDE exists for the trial.");
-      abstractionList.add(webDTO);
+  @SuppressWarnings({"PMD" })
+  private void enforceTrialINDIDE(Ii studyProtocolIi) throws PAException {    
+    List<StudyIndldeDTO> siList = PoPaServiceBeanLookup.getStudyIndldeService().
+    getByStudyProtocol(studyProtocolIi);
+    if (!(siList.isEmpty())) { 
+      for (int i = 0; i < siList.size(); i++) {
+        int j = 0;
+        if (siList.size() > 1 && (!(i == 0))) {          
+          if (siList.get(j).getGrantorCode().getCode().toString().equalsIgnoreCase(
+                   siList.get(i).getGrantorCode().getCode().toString())
+                && siList.get(j).getHolderTypeCode().getCode().toString().equalsIgnoreCase(
+                    siList.get(i).getHolderTypeCode().getCode().toString())
+                && siList.get(j).getIndldeNumber().getValue().toString().equalsIgnoreCase(
+                        siList.get(i).getIndldeNumber().getValue().toString())
+                && siList.get(j).getIndldeTypeCode().getCode().toString().equalsIgnoreCase(
+                            siList.get(i).getIndldeTypeCode().getCode().toString())) {
+            AbstractionCompletionDTO webDTO = new AbstractionCompletionDTO();
+            webDTO.setErrorType("Error");
+            webDTO.setComment("Select Trial IND/IDE under Regulatory Information from Administrative Data menu.");
+            webDTO.setErrorDescription("Trial IND/IDE should not have Duplicate values.");
+            abstractionList.add(webDTO);
+            if (i == siList.size()) { //NOPMD              
+            } else {
+              j++;
+            }
+          }
+        }
+      }
     }
-  }*/
+  }
 
 
   private void enforceRegulatoryInfo(Ii studyProtocolIi) throws PAException {
@@ -361,14 +434,7 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
       webDTO.setComment("Select General Trial Details from Administrative Data menu.");
       webDTO.setErrorDescription("Brief Title must be Entered");
       abstractionList.add(webDTO);
-    }
-    if (studyProtocolDTO.getAcronym().getValue() == null) {
-      webDTO = new AbstractionCompletionDTO();
-      webDTO.setErrorType("Error");
-      webDTO.setComment("Select General Trial Details from Administrative Data menu.");
-      webDTO.setErrorDescription("Acronym must be Entered");
-      abstractionList.add(webDTO);
-    }
+    }    
     if (studyProtocolDTO.getPublicDescription().getValue() == null) {
       webDTO = new AbstractionCompletionDTO();
       webDTO.setErrorType("Error");
@@ -376,24 +442,11 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
       webDTO.setErrorDescription("Brief Summary must be Entered");
       abstractionList.add(webDTO);
     }
-//    if (studyProtocolDTO.getScientificDescription().getValue() == null) {
-//      webDTO = new AbstractionCompletionDTO();
-//      webDTO.setErrorType("Error");
-//      webDTO.setComment("Select General Trial Details from Administrative Data menu.");
-//      webDTO.setErrorDescription("Detailed Description must be Entered");
-//      abstractionList.add(webDTO);
-//    }
-//    if (studyProtocolDTO.getKeywordText().getValue() == null) {
-//      webDTO = new AbstractionCompletionDTO();
-//      webDTO.setErrorType("Error");
-//      webDTO.setComment("Select General Trial Details from Administrative Data menu.");
-//      webDTO.setErrorDescription("Keywords must be Entered");
-//      abstractionList.add(webDTO);
-//    }
   }
 
 
-  private void enforceNCISpecificInfo(StudyProtocolDTO studyProtocolDTO, StudyResourcingDTO studyResourcingDTO) {
+  private void enforceNCISpecificInfo(StudyProtocolDTO studyProtocolDTO, StudyResourcingDTO studyResourcingDTO)
+  throws PAException {
     AbstractionCompletionDTO webDTO = null;
     if (studyProtocolDTO.getAccrualReportingMethodCode().getCode() == null) {
       webDTO = new AbstractionCompletionDTO();
@@ -407,7 +460,24 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
       webDTO = new AbstractionCompletionDTO();
       webDTO.setErrorType("Error");
       webDTO.setComment("Select NCI Specific Information from Administrative Data menu.");
-      webDTO.setErrorDescription("Summary 4 funding category Method must be Entered");
+      webDTO.setErrorDescription("Summary 4 Funding Category must be Entered");
+      abstractionList.add(webDTO);
+    }
+    Organization o = new Organization();
+    Organization org = null;
+    if (studyResourcingDTO != null 
+        && studyResourcingDTO.getOrganizationIdentifier().getExtension() != null) {
+    o.setId(Long.valueOf(studyResourcingDTO.getOrganizationIdentifier().getExtension()));
+    org = PoPaServiceBeanLookup.getPAOrganizationService().getOrganizationByIndetifers(o);
+    }
+    if (studyResourcingDTO == null 
+        || studyResourcingDTO.getOrganizationIdentifier().getExtension() == null 
+        || org == null 
+        || org.getName() == null) {
+      webDTO = new AbstractionCompletionDTO();
+      webDTO.setErrorType("Error");
+      webDTO.setComment("Select NCI Specific Information from Administrative Data menu.");
+      webDTO.setErrorDescription("Summary 4 Funding Sponsor/Source must be Entered");
       abstractionList.add(webDTO);
     }
   }
@@ -554,15 +624,7 @@ public class AbstractionCompletionServiceBean implements AbstractionCompletionSe
           + "Interventional/Observational under Scientific Data menu.");
       webDTO.setErrorDescription("Allocation must be Entered");
       abstractionList.add(webDTO);
-    }
-    if (ispDTO.getStudyClassificationCode().getCode() == null) {
-      webDTO = new AbstractionCompletionDTO();
-      webDTO.setErrorType("Error");
-      webDTO.setComment("Select Design Details from specific " 
-          + "Interventional/Observational under Scientific Data menu.");
-      webDTO.setErrorDescription("StudyClassification must be Entered");
-      abstractionList.add(webDTO);
-    }
+    }   
     if (ispDTO.getMaximumTargetAccrualNumber().getValue() == null) {
       webDTO = new AbstractionCompletionDTO();
       webDTO.setErrorType("Error");
