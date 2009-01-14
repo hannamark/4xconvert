f99517f323f4a905ea4acc9b67e256a1f3313539
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-01-14 13:00:23 +0000

    Tracker 18480.  No duplicate org/functional role for a given study.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@4114 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/exception/PADuplicateException.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/exception/PADuplicateException.java
@@ -52,98 +52,45 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
  */
-package gov.nih.nci.pa.service;
+package gov.nih.nci.pa.service.exception;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
-import gov.nih.nci.coppa.iso.Ii;
-import gov.nih.nci.pa.domain.StudyParticipationContact;
-import gov.nih.nci.pa.enums.StatusCode;
-import gov.nih.nci.pa.enums.StudyContactRoleCode;
-import gov.nih.nci.pa.iso.convert.StudyParticipationContactConverter;
-import gov.nih.nci.pa.iso.dto.StudyParticipationContactDTO;
-import gov.nih.nci.pa.iso.util.AddressConverterUtil;
-import gov.nih.nci.pa.iso.util.BlConverter;
-import gov.nih.nci.pa.iso.util.CdConverter;
-import gov.nih.nci.pa.iso.util.IiConverter;
-import gov.nih.nci.pa.iso.util.TsConverter;
-import gov.nih.nci.pa.service.util.PAHealthCareProviderRemote;
-import gov.nih.nci.pa.service.util.PAHealthCareProviderServiceBean;
-import gov.nih.nci.pa.util.PAUtil;
-import gov.nih.nci.pa.util.TestSchema;
-
-import org.junit.Before;
-import org.junit.Test;
+import gov.nih.nci.pa.service.PAException;
 
 /**
- * Test service and converter.
- * @author hreinhart
+ * PA Exception for error handling.
+ * 
+ * @author Hugh Reinhart
+ * @since 01/13/2009 copyright NCI 2007. All rights reserved. This code may not
+ *        be used without the express written permission of the copyright
+ *        holder, NCI.
  */
-public class StudyParticipationContactServiceTest {
-    private StudyParticipationContactServiceRemote remoteEjb = new StudyParticipationContactServiceBean();
-    private PAHealthCareProviderRemote paRemote = new PAHealthCareProviderServiceBean();
-    Long protocolId;
-    Ii protocolIi;
-    Long participationId;
-    Ii participationIi;
-    Long contactId;
-    Ii contactIi;
-    Long healthCareProviderId;
-    Ii healthCareProviderIi;
+public class PADuplicateException extends PAException {
+    static final long serialVersionUID = 1234509090L;
 
-    @Before
-    public void setUp() throws Exception {
-        TestSchema.reset1();
-        TestSchema.primeData();
-        protocolId = TestSchema.studyProtocolIds.get(0);
-        protocolIi = IiConverter.convertToIi(protocolId);
-        participationId = TestSchema.studyParticipationIds.get(0);
-        participationIi = IiConverter.convertToIi(participationId);
-        contactId = TestSchema.studyParticipationContactIds.get(0);
-        contactIi = IiConverter.convertToIi(contactId);
-        healthCareProviderId = TestSchema.healthCareFacilityIds.get(0);
-        healthCareProviderIi = IiConverter.convertToIi(healthCareProviderId);
-    }
-    @Test
-    public void getPersons() throws Exception {
-        //List a = paRemote.getPersonsByStudyParticpationId(participationId);
-        assertEquals("", "");
-
-    }
-   
-    @Test
-    public void get() throws Exception {
-        StudyParticipationContactDTO spcDto = remoteEjb.get(contactIi);
-        StudyParticipationContact spcBo = StudyParticipationContactConverter.convertFromDtoToDomain(spcDto);
-        assertEquals(participationId, spcBo.getStudyParticipation().getId());
-        assertEquals(protocolId, spcBo.getStudyProtocol().getId());
-    }
-    
-    @Test
-    public void create() throws Exception {
-        StudyParticipationContactDTO spc = new StudyParticipationContactDTO();
-        spc.setIdentifier(IiConverter.convertToIi((Long) null));
-        spc.setPostalAddress(AddressConverterUtil.create("1", "2", "3", "4", "5", "ZZZ"));
-        spc.setPrimaryIndicator(BlConverter.convertToBl(true));
-        spc.setRoleCode(CdConverter.convertToCd(StudyContactRoleCode.COORDINATING_INVESTIGATOR));
-        spc.setStatusCode(CdConverter.convertToCd(StatusCode.ACTIVE));
-        spc.setStatusDateRangeLow(TsConverter.convertToTs(PAUtil.dateStringToTimestamp("1/1/2005")));
-        spc.setStudyParticipationIi(participationIi);
-        spc.setStudyProtocolIdentifier(protocolIi);
-        spc.setHealthCareProviderIi(healthCareProviderIi);
-        StudyParticipationContactDTO result = remoteEjb.create(spc);
-        assertFalse(PAUtil.isIiNull(result.getIdentifier()));
+    /** */
+    public PADuplicateException() {
+        super();
     }
 
-    @Test
-    public void delete() throws Exception {
-        remoteEjb.delete(contactIi);
-        try {
-            remoteEjb.get(contactIi);
-            fail("get() should have thrown an exception after delete().");
-        } catch(PAException e) {
-            // expected behavior
-        }
+    /**
+     * @param message message
+     * @param t exception
+     */
+    public PADuplicateException(String message, Throwable t) {
+        super(message, t);
+    }
+
+    /**
+     * @param message message
+     */
+    public PADuplicateException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param t exception
+     */
+    public PADuplicateException(Throwable t) {
+        super(t);
     }
 }
