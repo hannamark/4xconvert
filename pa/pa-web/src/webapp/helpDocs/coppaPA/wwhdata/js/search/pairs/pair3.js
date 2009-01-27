3c3c4ee5104d29865ae6f5426b047dd3ada55edf
guthikondak <guthikondak@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-01-27 19:29:03 +0000

    updated new HelpDocs files
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@4313 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/src/webapp/helpDocs/coppaPA/wwhdata/js/search/pairs/pair3.js
@@ -0,0 +1,24 @@
+function FileData_Pairs(x)
+{
+x.t("abstraction","instructions");
+x.t("abstraction","portal");
+x.t("instructions","forlogging");
+x.t("getting","started");
+x.t("overview","protocol");
+x.t("overview","nci");
+x.t("logging","nci");
+x.t("logging","protocol");
+x.t("provides","overview");
+x.t("started","getting");
+x.t("started","section");
+x.t("portal","logging");
+x.t("portal","topics");
+x.t("section","provides");
+x.t("section","include");
+x.t("include","overview");
+x.t("forlogging","nci");
+x.t("protocol","abstraction");
+x.t("nci","clinical");
+x.t("topics","section");
+x.t("clinical","protocol");
+}
