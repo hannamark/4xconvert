dfa87417eb1044acc41d78c1f0aee4d7e27b6ff9
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-02-03 22:00:59 +0000

    new license text changes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@4387 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/domain/InterventionalStudyProtocol.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/domain/InterventionalStudyProtocol.java
@@ -1,57 +1,81 @@
-/*
- * Copyright Notice. Copyright 2008, ScenPro, Inc, (“caBIG™ Participant”). The Protocol Abstraction (PA) Application was
- * created with NCI funding and is part of the caBIG™ initiative. The software subject to this notice and license
- * includes both human readable source code form and machine readable, binary, object code form (the “caBIG™ Software”).
- * This caBIG™ Software License (the “License”) is between caBIG™ Participant and You. “You (or “Your”) shall mean a
- * person or an entity, and all other entities that control, are controlled by, or are under common control with the
- * entity. “Control” for purposes of this definition means (i) the direct or indirect power to cause the direction or
- * management of such entity, whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the
- * outstanding shares, or (iii) beneficial ownership of such entity. License. Provided that You agree to the conditions
- * described below, caBIG™ Participant grants You a non-exclusive, worldwide, perpetual, fully-paid-up, no-charge,
- * irrevocable, transferable and royalty-free right and license in its rights in the caBIG™ Software, including any
- * copyright or patent rights therein, to (i) use, install, disclose, access, operate, execute, reproduce, copy, modify,
- * translate, market, publicly display, publicly perform, and prepare derivative works of the caBIG™ Software in any
- * manner and for any purpose, and to have or permit others to do so; (ii) make, have made, use, practice, sell, and
- * offer for sale, import, and/or otherwise dispose of caBIG™ Software (or portions thereof); (iii) distribute and have
- * distributed to and by third parties the caBIG™ Software and any modifications and derivative works thereof; and (iv)
- * sublicense the foregoing rights set out in (i), (ii) and (iii) to third parties, including the right to license such
- * rights to further third parties. For sake of clarity, and not by way of limitation, caBIG™ Participant shall have no
- * right of accounting or right of payment from You or Your sub licensees for the rights granted under this License.
- * This License is granted at no charge to You. Your downloading, copying, modifying, displaying, distributing or use of
- * caBIG™ Software constitutes acceptance of all of the terms and conditions of this Agreement. If You do not agree to
- * such terms and conditions, You have no right to download, copy, modify, display, distribute or use the caBIG™
- * Software. 1. Your redistributions of the source code for the caBIG™ Software must retain the above copyright notice,
- * this list of conditions and the disclaimer and limitation of liability of Article 6 below. Your redistributions in
- * object code form must reproduce the above copyright notice, this list of conditions and the disclaimer of Article 6
- * in the documentation and/or other materials provided with the distribution, if any. 2. Your end-user documentation
- * included with the redistribution, if any, must include the following acknowledgment: “This product includes software
- * developed by ScenPro, Inc.” If You do not include such end-user documentation, You shall include this acknowledgment
- * in the caBIG™ Software itself, wherever such third-party acknowledgments normally appear. 3. You may not use the
- * names “ScenPro, Inc.”, “The National Cancer Institute”, “NCI”, “Cancer Bioinformatics Grid” or “caBIG™” to endorse or
- * promote products derived from this caBIG™ Software. This License does not authorize You to use any trademarks,
- * service marks, trade names, logos or product names of either caBIG™ Participant, NCI or caBIG™, except as required to
- * comply with the terms of this License. 4. For sake of clarity, and not by way of limitation, You may incorporate this
- * caBIG™ Software into Your proprietary programs and into any third party proprietary programs. However, if You
- * incorporate the caBIG™ Software into third party proprietary programs, You agree that You are solely responsible for
- * obtaining any permission from such third parties required to incorporate the caBIG™ Software into such third party
- * proprietary programs and for informing Your sub licensees, including without limitation Your end-users, of their
- * obligation to secure any required permissions from such third parties before incorporating the caBIG™ Software into
- * such third party proprietary software programs. In the event that You fail to obtain such permissions, You agree to
- * indemnify caBIG™ Participant for any claims against caBIG™ Participant by such third parties, except to the extent
- * prohibited by law, resulting from Your failure to obtain such permissions. 5. For sake of clarity, and not by way of
- * limitation, You may add Your own copyright statement to Your modifications and to the derivative works, and You may
- * provide additional or different license terms and conditions in Your sublicenses of modifications of the caBIG™
- * Software, or any derivative works of the caBIG™ Software as a whole, provided Your use, reproduction, and
- * distribution of the Work otherwise complies with the conditions stated in this License. 6. THIS caBIG™ SOFTWARE IS
- * PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE
- * ScenPro, Inc. OR ITS AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS caBIG™ SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- */
+/**
+* caBIG Open Source Software License
+*
+* Copyright Notice.  Copyright 2008, ScenPro, Inc,  (caBIG Participant).   The Protocol  Abstraction (PA) Application
+* was created with NCI funding and is part of  the caBIG initiative. The  software subject to  this notice  and license
+* includes both  human readable source code form and machine readable, binary, object code form (the caBIG Software).
+*
+* This caBIG Software License (the License) is between caBIG  Participant  and  You.  You (or Your) shall  mean a
+* person or an entity, and all other entities that control, are  controlled by,  or  are under common  control  with the
+* entity.  Control for purposes of this definition means
+*
+* (i) the direct or indirect power to cause the direction or management of such entity,whether by contract
+* or otherwise,or
+*
+* (ii) ownership of fifty percent (50%) or more of the outstanding shares, or
+*
+* (iii) beneficial ownership of such entity.
+* License.  Provided that You agree to the conditions described below, caBIG Participant  grants  You a  non-exclusive,
+* worldwide, perpetual, fully-paid-up, no-charge, irrevocable,  transferable  and royalty-free  right and license in its
+* rights in the caBIG Software, including any copyright or patent rights therein, to
+*
+* (i) use,install, disclose, access, operate,  execute, reproduce,  copy, modify, translate,  market,  publicly display,
+* publicly perform, and prepare derivative works of the caBIG Software in any manner and for any  purpose,  and to have
+* or permit others to do so;
+*
+* (ii) make, have made, use, practice, sell, and offer  for sale,  import, and/or  otherwise  dispose of caBIG Software
+* (or portions thereof);
+*
+* (iii) distribute and have distributed  to  and by third   parties the   caBIG  Software  and any   modifications  and
+* derivative works thereof; and (iv) sublicense the  foregoing rights  set  out in (i), (ii) and (iii) to third parties,
+* including the right to license such rights to further third parties. For sake of clarity,and not by way of limitation,
+* caBIG Participant shall have no right of accounting or right of payment from You or Your sub licensees for the rights
+* granted under this License.   This  License  is  granted  at no  charge  to You. Your downloading, copying, modifying,
+* displaying, distributing or use of caBIG Software constitutes acceptance  of  all of the terms and conditions of this
+* Agreement.  If You do not agree to such terms and conditions,  You have no right to download,  copy,  modify, display,
+* distribute or use the caBIG Software.
+*
+* 1.  Your redistributions of the source code for the caBIG Software must retain the above copyright notice, this  list
+* of conditions and the disclaimer and limitation of liability of Article 6 below.   Your redistributions in object code
+* form must reproduce the above copyright notice,  this list of  conditions  and the  disclaimer  of  Article  6  in the
+* documentation and/or other materials provided with the distribution, if any.
+*
+* 2.  Your end-user documentation included with the redistribution, if any,  must include the  following acknowledgment:
+* This product includes software developed by ScenPro, Inc.   If  You  do not include such end-user documentation, You
+* shall include this acknowledgment in the caBIG Software itself, wherever such third-party acknowledgments normally
+* appear.
+*
+* 3.  You may not use the names ScenPro, Inc., The National Cancer Institute, NCI, Cancer Bioinformatics Grid or
+* caBIG to endorse or promote products derived from this caBIG Software.  This License does not authorize You to use
+* any trademarks, service marks, trade names, logos or product names of either caBIG Participant, NCI or caBIG, except
+* as required to comply with the terms of this License.
+*
+* 4.  For sake of clarity, and not by way of limitation, You  may incorporate this caBIG Software into Your proprietary
+* programs and into any third party proprietary programs.  However, if You incorporate the  caBIG Software  into  third
+* party proprietary programs,  You agree  that You are  solely responsible  for obtaining any permission from such third
+* parties required to incorporate the caBIG Software  into such third party proprietary programs and for informing Your
+* sub licensees, including without limitation Your end-users, of their obligation  to  secure  any  required permissions
+* from such third parties before incorporating the caBIG Software into such third party proprietary  software programs.
+* In the event that You fail to obtain such permissions,  You  agree  to  indemnify  caBIG  Participant  for any claims
+* against caBIG Participant by such third parties, except to the extent prohibited by law,  resulting from Your failure
+* to obtain such permissions.
+*
+* 5.  For sake of clarity, and not by way of limitation, You may add Your own copyright statement  to Your modifications
+* and to the derivative works, and You may provide  additional  or  different  license  terms  and  conditions  in  Your
+* sublicenses of modifications of the caBIG  Software,  or  any  derivative  works  of  the caBIG Software as a whole,
+* provided Your use, reproduction,  and  distribution  of the Work otherwise complies with the conditions stated in this
+* License.
+*
+* 6.  THIS caBIG SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES  ( INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED.  IN
+* NO EVENT SHALL THE ScenPro, Inc. OR ITS AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+* OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  LIMITED  TO,  PROCUREMENT OF SUBSTITUTE GOODS  OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS caBIG SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*
+*/
 package gov.nih.nci.pa.domain;
 
 import gov.nih.nci.pa.enums.AllocationCode;
@@ -69,11 +93,9 @@ import javax.persistence.Enumerated;
 
 /**
  * An action plan for InterventionalStudyProtocol.
- * 
+ *
  * @author Naveen Amiruddin
- * @since 09/07/2008 copyright NCI 2007. All rights reserved. This code may not
- *        be used without the express written permission of the copyright
- *        holder, NCI.
+ * @since 09/07/2008
  */
 
 @Entity
@@ -91,7 +113,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     private StudyClassificationCode studyClassificationCode;
 
     /**
-     * 
+     *
      * @return allocationCode
      */
     @Column(name = "ALLOCATION_CODE")
@@ -101,7 +123,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param allocationCode
      *            allocation Code
      */
@@ -110,7 +132,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return blindingRoleCodeSubject
      */
     @Column(name = "BLINDING_ROLE_CODE_SUBJECT")
@@ -120,7 +142,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param blindingRoleCodeSubject
      *            blindingRoleCodeSubject
      */
@@ -130,7 +152,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return blindingRoleCodeCaregiver
      */
     @Column(name = "BLINDING_ROLE_CODE_CAREGIVER")
@@ -140,7 +162,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param blindingRoleCodeCaregiver
      *            blindingRoleCodeCaregiver
      */
@@ -150,7 +172,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return blindingRoleCodeInvestigator
      */
     @Column(name = "BLINDING_ROLE_CODE_INVESTIGATOR")
@@ -160,7 +182,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param blindingRoleCodeInvestigator
      *            blindingRoleCodeInvestigator
      */
@@ -170,7 +192,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return blindingRoleCodeOutcome
      */
     @Column(name = "BLINDING_ROLE_CODE_OUTCOME")
@@ -180,7 +202,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param blindingRoleCodeOutcome
      *            blindingRoleCodeOutcome
      */
@@ -190,7 +212,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return blindingSchemaCode
      */
     @Column(name = "BLINDING_SCHEMA_CODE")
@@ -200,7 +222,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param blindingSchemaCode
      *            blindingSchemaCode
      */
@@ -209,7 +231,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return designConfigurationCode
      */
     @Column(name = "DESIGN_CONFIGURATION_CODE")
@@ -219,7 +241,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param designConfigurationCode
      *            designConfigurationCode
      */
@@ -245,7 +267,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @return studyClassificationCode
      */
     @Column(name = "STUDY_CLASSIFICATION_CODE")
@@ -255,7 +277,7 @@ public class InterventionalStudyProtocol extends StudyProtocol {
     }
 
     /**
-     * 
+     *
      * @param studyClassificationCode
      *            studyClassificationCode
      */
