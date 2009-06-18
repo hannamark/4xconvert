ff557466758eb04865219cfc39bab9f286be6e9a
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-06-18 23:06:06 +0000

    Check in updated registry help documents.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@5828 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/webapp/helpDocs/coppaRegistry/wwhdata/js/search/pairs/pair12.js
@@ -0,0 +1,27 @@
+function FileData_Pairs(x)
+{
+x.t("searching","trials");
+x.t("trials","searching");
+x.t("trials","working");
+x.t("trials","section");
+x.t("trials","ctrp");
+x.t("describes","search");
+x.t("working","search");
+x.t("includes","following");
+x.t("trial","details");
+x.t("trial","metadata");
+x.t("results","viewing");
+x.t("search","results");
+x.t("search","existing");
+x.t("metadata","searching");
+x.t("viewing","trial");
+x.t("section","describes");
+x.t("section","includes");
+x.t("registration","site");
+x.t("existing","trials");
+x.t("site","section");
+x.t("ctrp","registration");
+x.t("topics","clinical");
+x.t("following","topics");
+x.t("clinical","trial");
+}
---
+++ reg-web/src/webapp/helpDocs/coppaRegistry/wwhdata/js/search/pairs/pair48.js
@@ -0,0 +1,27 @@
+function FileData_Pairs(x)
+{
+x.t("trials","amending");
+x.t("instructions","completing");
+x.t("submissions","completing");
+x.t("ind/ide","details");
+x.t("ind/ide","serial");
+x.t("ind/ide","section");
+x.t("ind/ide","information");
+x.t("verified","trials");
+x.t("change","existing");
+x.t("original","amended");
+x.t("details","instructions");
+x.t("details","amending");
+x.t("completing","ind/ide");
+x.t("registered","trials");
+x.t("serial","number");
+x.t("cannot","change");
+x.t("section","original");
+x.t("section","cannot");
+x.t("amended","submissions");
+x.t("existing","ind/ide");
+x.t("information","section");
+x.t("amending","ind/ide");
+x.t("amending","verified");
+x.t("amending","registered");
+}
