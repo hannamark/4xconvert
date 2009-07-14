c744badf2572ac5ead9ea772bdc53400f09f8fea
chickerura <chickerura@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-07-14 15:56:07 +0000

    junit for studyRelationshipServiceBean
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@5962 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-ejb/test/java/gov/nih/nci/pa/service/StudyRelationshipServiceBeanTest.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.pa.service;
+
+
+import gov.nih.nci.coppa.services.LimitOffset;
+import gov.nih.nci.coppa.services.TooManyResultsException;
+import gov.nih.nci.pa.iso.dto.StudyRelationshipDTO;
+import gov.nih.nci.pa.iso.util.IiConverter;
+import gov.nih.nci.pa.util.TestSchema;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyRelationshipServiceBeanTest {
+
+    private StudyRelationshipServiceRemote remoteEjb = new StudyRelationshipServiceBean();;
+    private StudyRelationshipDTO srDto = new StudyRelationshipDTO();
+
+    @Before
+    public void setUp() throws Exception {
+      TestSchema.reset1();
+      TestSchema.primeData();
+      srDto.setSourceStudyProtocolIdentifier(IiConverter.convertToIi(TestSchema.studyProtocolIds.get(0)));
+    
+      }
+
+    @Test    
+    public void testSearchStudyRelationshipDTOLimitOffset() throws PAException,TooManyResultsException{
+        LimitOffset limitOffset = new LimitOffset(1, 1);
+        List<StudyRelationshipDTO> listDTO = remoteEjb.search(srDto, limitOffset);
+        assertEquals(0,listDTO.size());
+    }
+
+}
