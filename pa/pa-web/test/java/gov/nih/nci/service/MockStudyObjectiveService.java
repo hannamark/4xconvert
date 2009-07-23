02735c78f34c78f5ed198334400d2ee0bc3c1bd9
niktev <niktev@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-07-23 15:01:48 +0000

    Fixed-#-22399 Web CTRP - 19751 Support abstraction of Study Objectives;Target accrual accepts only positive number
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@6022 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/test/java/gov/nih/nci/service/MockStudyObjectiveService.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package gov.nih.nci.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.pa.iso.dto.StudyObjectiveDTO;
+import gov.nih.nci.pa.service.PAException;
+import gov.nih.nci.pa.service.StudyObjectiveServiceRemote;
+
+/**
+ * @author Vrushali
+ *
+ */
+public class MockStudyObjectiveService implements StudyObjectiveServiceRemote {
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.StudyPaService#copy(gov.nih.nci.coppa.iso.Ii, gov.nih.nci.coppa.iso.Ii)
+     */
+    public void copy(Ii fromStudyProtocolIi, Ii toStudyProtocolIi)
+            throws PAException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.StudyPaService#getByStudyProtocol(gov.nih.nci.coppa.iso.Ii)
+     */
+    public List<StudyObjectiveDTO> getByStudyProtocol(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return new ArrayList<StudyObjectiveDTO>();
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.StudyPaService#getCurrentByStudyProtocol(gov.nih.nci.coppa.iso.Ii)
+     */
+    public StudyObjectiveDTO getCurrentByStudyProtocol(Ii studyProtocolIi)
+            throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.BasePaService#create(java.lang.Object)
+     */
+    public StudyObjectiveDTO create(StudyObjectiveDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.BasePaService#delete(gov.nih.nci.coppa.iso.Ii)
+     */
+    public void delete(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.BasePaService#get(gov.nih.nci.coppa.iso.Ii)
+     */
+    public StudyObjectiveDTO get(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nih.nci.pa.service.BasePaService#update(java.lang.Object)
+     */
+    public StudyObjectiveDTO update(StudyObjectiveDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
