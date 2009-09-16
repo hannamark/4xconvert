0c9c1938f246d89454a08c5ca7872936e6decb04
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-09-16 16:08:43 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@6658 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pdq/scripts/manual/m_disease001.sql
+++ pa/pdq/scripts/manual/m_disease001.sql
@@ -1,2 +1,5 @@
 INSERT INTO disease (identifier,disease_code,nt_term_identifier,preferred_name,menu_display_name,status_code,status_date_range_low,date_last_created,user_last_created) 
 VALUES (1776,'CDR0000587622',null,'systemic scleroderma','systemic scleroderma','ACTIVE',now(),'09/16/2009','CDR587622.xml');
+
+UPDATE DISEASE SET STATUS_CODE = 'ACTIVE' WHERE IDENTIFIER = 1755 AND STATUS_CODE = 'INACTIVE';
+
