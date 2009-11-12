afce671eee2fcbfa331e6b98fc77de0a6a14068d
hreinhart <hreinhart@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-11-12 12:39:16 +0000

    Fix build.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@7501 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- accrual/accrual-web/test/java/gov/nih/nci/accrual/accweb/util/MockPaPlannedActivityServiceBean.java
+++ accrual/accrual-web/test/java/gov/nih/nci/accrual/accweb/util/MockPaPlannedActivityServiceBean.java
@@ -1,92 +1,196 @@
 package gov.nih.nci.accrual.accweb.util;
 
-import java.util.List;
-import java.util.Map;
-
 import gov.nih.nci.coppa.iso.Ii;
 import gov.nih.nci.pa.iso.dto.PlannedActivityDTO;
 import gov.nih.nci.pa.iso.dto.PlannedEligibilityCriterionDTO;
+import gov.nih.nci.pa.iso.dto.PlannedProcedureDTO;
+import gov.nih.nci.pa.iso.dto.PlannedSubstanceAdministrationDTO;
 import gov.nih.nci.pa.service.PAException;
 import gov.nih.nci.pa.service.PlannedActivityServiceRemote;
 
+import java.util.List;
+import java.util.Map;
+
 public class MockPaPlannedActivityServiceBean implements PlannedActivityServiceRemote{
 
-    public void copyPlannedEligibilityStudyCriterions(Ii arg0, Ii arg1)
-            throws PAException {
+    /**
+     * {@inheritDoc}
+     */
+    public void copyPlannedEligibilityStudyCriterions(Ii fromStudyProtocolIi, Ii toStudyProtocolIi) throws PAException {
         // TODO Auto-generated method stub
 
     }
 
-    public PlannedEligibilityCriterionDTO createPlannedEligibilityCriterion(
-            PlannedEligibilityCriterionDTO arg0) throws PAException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public void deletePlannedEligibilityCriterion(Ii arg0) throws PAException {
-        // TODO Auto-generated method stub
-
-    }
-
-    public List<PlannedActivityDTO> getByArm(Ii arg0) throws PAException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public PlannedEligibilityCriterionDTO getPlannedEligibilityCriterion(Ii arg0)
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedEligibilityCriterionDTO createPlannedEligibilityCriterion(PlannedEligibilityCriterionDTO dto)
             throws PAException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public List<PlannedEligibilityCriterionDTO> getPlannedEligibilityCriterionByStudyProtocol(
-            Ii arg0) throws PAException {
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedProcedureDTO createPlannedProcedure(PlannedProcedureDTO dto) throws PAException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public PlannedEligibilityCriterionDTO updatePlannedEligibilityCriterion(
-            PlannedEligibilityCriterionDTO arg0) throws PAException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Map<Ii, Ii> copy(Ii arg0, Ii arg1) throws PAException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public List<PlannedActivityDTO> getByStudyProtocol(Ii arg0)
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedSubstanceAdministrationDTO createPlannedSubstanceAdministration(PlannedSubstanceAdministrationDTO dto)
             throws PAException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public PlannedActivityDTO create(PlannedActivityDTO arg0)
+    /**
+     * {@inheritDoc}
+     */
+    public void deletePlannedEligibilityCriterion(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<PlannedActivityDTO> getByArm(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedEligibilityCriterionDTO getPlannedEligibilityCriterion(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<PlannedEligibilityCriterionDTO> getPlannedEligibilityCriterionByStudyProtocol(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedProcedureDTO getPlannedProcedure(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<PlannedProcedureDTO> getPlannedProcedureByStudyProtocol(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedSubstanceAdministrationDTO getPlannedSubstanceAdministration(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<PlannedSubstanceAdministrationDTO> getPlannedSubstanceAdministrationByStudyProtocol(Ii ii)
             throws PAException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public void delete(Ii arg0) throws PAException {
-        // TODO Auto-generated method stub
-
-    }
-
-    public PlannedActivityDTO get(Ii arg0) throws PAException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public PlannedActivityDTO update(PlannedActivityDTO arg0)
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedEligibilityCriterionDTO updatePlannedEligibilityCriterion(PlannedEligibilityCriterionDTO dto)
             throws PAException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public void validate(PlannedActivityDTO arg0) throws PAException {
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedProcedureDTO updatePlannedProcedure(PlannedProcedureDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedSubstanceAdministrationDTO updatePlannedSubstanceAdministration(PlannedSubstanceAdministrationDTO dto)
+            throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedActivityDTO create(PlannedActivityDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void delete(Ii ii) throws PAException {
         // TODO Auto-generated method stub
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedActivityDTO get(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlannedActivityDTO update(PlannedActivityDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void validate(PlannedActivityDTO dto) throws PAException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Map<Ii, Ii> copy(Ii fromStudyProtocolIi, Ii toStudyProtocolIi) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<PlannedActivityDTO> getByStudyProtocol(Ii ii) throws PAException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
