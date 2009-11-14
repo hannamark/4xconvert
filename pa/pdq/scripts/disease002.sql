a39ee78de23a80187dfdafcdd2b61ac0bb01682c
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-11-14 23:47:24 +0000

    renaming the pdq  files
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@7529 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pdq/scripts/disease002.sql
@@ -0,0 +1,3 @@
+INSERT INTO disease (identifier,disease_code,nt_term_identifier,preferred_name,menu_display_name,status_code,status_date_range_low,date_last_created,user_last_created) 
+
+VALUES (1755,'CDR0000590650','C3764','adenomatous polyp','adenomatous polyp','ACTIVE',now(),'07/17/2009','CDR590650.xml');
---
+++ pa/pdq/scripts/disease005.sql
@@ -0,0 +1,3 @@
+INSERT INTO disease (identifier,disease_code,nt_term_identifier,preferred_name,menu_display_name,status_code,status_date_range_low,date_last_created,user_last_created) 
+VALUES (1777,'CDR0000039126',null,'central nervous system involvement/metastases','central nervous system involvement/metastases','ACTIVE',now(),'10/01/2009','CDR39126.xml');
+
