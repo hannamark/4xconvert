045a19c4f850ee3b588de3668107e5ff90f20b3d
smatyas <smatyas@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-12-01 16:50:54 +0000

    PO-1430: replacing tabs with 4 spaces
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@7710 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/web/src/main/webapp/scripts/po.js
+++ po/web/src/main/webapp/scripts/po.js
@@ -87,85 +87,85 @@ function setFocusToFirstControl() {
 //       1. eventElement - If contextName is not blank or null, the topic will be appended.
 //       2. includeNav - If true, the left navigation pane appears. Else, the navigation pane is not displayed.
 function fireEvent(eventElement, firefoxEvent, ieEvent) {
-	if (document.createEvent) {
-		var e = document.createEvent('Events');
-		e.initEvent(firefoxEvent, true, true);
-	} else if (document.createEventObject) {
-		var e = document.createEventObject();
-	}
+    if (document.createEvent) {
+        var e = document.createEvent('Events');
+        e.initEvent(firefoxEvent, true, true);
+    } else if (document.createEventObject) {
+        var e = document.createEventObject();
+    }
 
-	if (eventElement.dispatchEvent) {
-		eventElement.dispatchEvent(e);
-	} else if (eventElement.fireEvent) {
-		eventElement.fireEvent(ieEvent, e);
-	}
+    if (eventElement.dispatchEvent) {
+        eventElement.dispatchEvent(e);
+    } else if (eventElement.fireEvent) {
+        eventElement.fireEvent(ieEvent, e);
+    }
 }
 
 
 function copyValueToTextField(value, textFieldId) {
-	if ($(textFieldId) && $(textFieldId).type == 'text') {
-		$(textFieldId).value = value;
-	}
+    if ($(textFieldId) && $(textFieldId).type == 'text') {
+        $(textFieldId).value = value;
+    }
 }
 
 function selectValueInSelectField(value, selectBoxId, firefoxEvent, ieEvent) {
-	var found = false;
-	if ($(selectBoxId) && $(selectBoxId).type == 'select-one') { 
-		for ( var i = 0; i <= $(selectBoxId).length - 1; i = i + 1) {
-			var selectedText = $(selectBoxId).options[i].value;
-			if (selectedText == value) {
-				var isFireEvent = false;
-				if ($(selectBoxId).selectedIndex != i) {
-					isFireEvent = true;
-				}
-				$(selectBoxId).selectedIndex = i;
-				found = true;
-				if (isFireEvent) {
-					if (firefoxEvent == null) {
-						firefoxEvent = 'change';
-					}
-					if (ieEvent == null) {
-						ieEvent = 'onchange';
-					}
-					fireEvent($(selectBoxId), firefoxEvent, ieEvent);
-				}
-				break;
-			}
-		}
-		if (!found) {
-			alert('' + value + ' not found!');
-		}
-	}
+    var found = false;
+    if ($(selectBoxId) && $(selectBoxId).type == 'select-one') { 
+        for ( var i = 0; i <= $(selectBoxId).length - 1; i = i + 1) {
+            var selectedText = $(selectBoxId).options[i].value;
+            if (selectedText == value) {
+                var isFireEvent = false;
+                if ($(selectBoxId).selectedIndex != i) {
+                    isFireEvent = true;
+                }
+                $(selectBoxId).selectedIndex = i;
+                found = true;
+                if (isFireEvent) {
+                    if (firefoxEvent == null) {
+                        firefoxEvent = 'change';
+                    }
+                    if (ieEvent == null) {
+                        ieEvent = 'onchange';
+                    }
+                    fireEvent($(selectBoxId), firefoxEvent, ieEvent);
+                }
+                break;
+            }
+        }
+        if (!found) {
+            alert('' + value + ' not found!');
+        }
+    }
 }
 
 function selectValuesInMultiSelectField(values, multiSelectBoxId, firefoxEvent, ieEvent) {
-	var found = false;
-	var value;
-	var isFireEvent = false;
-	for (var j = 0; j <= values.length -1; j = j + 1) {
-		value = values[j];
-		for ( var i = 0; i <= $(multiSelectBoxId).length - 1; i = i + 1) {
-			var option = $(multiSelectBoxId).options[i];
-			var selectBoxValue = option.value;
-			if (selectBoxValue == value) {
-				if (!option.selected) {
-					isFireEvent = true;
-				}
-				option.selected = true;
-				found = true;
-				break;
-			}
-		}
-	}
-	if (isFireEvent) {
-		if (firefoxEvent == null) {
-			firefoxEvent = 'change';
-		}
-		if (ieEvent == null) {
-			ieEvent = 'onchange';
-		}
-		fireEvent($(multiSelectBoxId), firefoxEvent, ieEvent);
-	}
+    var found = false;
+    var value;
+    var isFireEvent = false;
+    for (var j = 0; j <= values.length -1; j = j + 1) {
+        value = values[j];
+        for ( var i = 0; i <= $(multiSelectBoxId).length - 1; i = i + 1) {
+            var option = $(multiSelectBoxId).options[i];
+            var selectBoxValue = option.value;
+            if (selectBoxValue == value) {
+                if (!option.selected) {
+                    isFireEvent = true;
+                }
+                option.selected = true;
+                found = true;
+                break;
+            }
+        }
+    }
+    if (isFireEvent) {
+        if (firefoxEvent == null) {
+            firefoxEvent = 'change';
+        }
+        if (ieEvent == null) {
+            ieEvent = 'onchange';
+        }
+        fireEvent($(multiSelectBoxId), firefoxEvent, ieEvent);
+    }
 }
 
 var IdValue = Class.create();
@@ -177,33 +177,33 @@ IdValue.prototype = {
 };
 
 function showPopup(url, callbackFunc) {
-	showPopWin(url, calculateMaxPopupWidth(), calculateMaxPopupHeight(), callbackFunc);
+    showPopWin(url, calculateMaxPopupWidth(), calculateMaxPopupHeight(), callbackFunc);
 }
 
 function assembleAndSubmitPhoneNumber(type, url, divId) {
-    	
-	$(type + 'Entry_value').value = $(type + 'Entry_part1').value + '-' + $(type + 'Entry_part2').value + '-' + $(type + 'Entry_part3').value;
+        
+    $(type + 'Entry_value').value = $(type + 'Entry_part1').value + '-' + $(type + 'Entry_part2').value + '-' + $(type + 'Entry_part3').value;
     
-	if ($(type + 'Entry_value').value.length < 12) {
-		alert('The entire ' + type + ' number must be provided.');
-		return false;
-	}
-	
-	var myRe = new RegExp ("\\d{3}-\\d{3}-\\d{4}");
-	var OK = myRe.exec($(type + 'Entry_value').value); 
-	if (!OK) {
-		alert($(type + 'Entry_value').value + ' must match ###-###-####.');
-		return false;
-	}
-	
+    if ($(type + 'Entry_value').value.length < 12) {
+        alert('The entire ' + type + ' number must be provided.');
+        return false;
+    }
+    
+    var myRe = new RegExp ("\\d{3}-\\d{3}-\\d{4}");
+    var OK = myRe.exec($(type + 'Entry_value').value); 
+    if (!OK) {
+        alert($(type + 'Entry_value').value + ' must match ###-###-####.');
+        return false;
+    }
+    
     if ($(type + 'Entry_part4').value.length > 0) {
-    	myRe = new RegExp ("^\\d{0,}$");
-    	OK = myRe.exec($(type + 'Entry_part4').value); 
-    	if (!OK) {
-    		alert('Ext ' + $(type + 'Entry_part4').value + ' must match digits only.');
-    		return false;
-    	}
-    	$(type + 'Entry_value').value = $(type + 'Entry_value').value + 'x' + $(type + 'Entry_part4').value;
+        myRe = new RegExp ("^\\d{0,}$");
+        OK = myRe.exec($(type + 'Entry_part4').value); 
+        if (!OK) {
+            alert('Ext ' + $(type + 'Entry_part4').value + ' must match digits only.');
+            return false;
+        }
+        $(type + 'Entry_value').value = $(type + 'Entry_value').value + 'x' + $(type + 'Entry_part4').value;
     }
     
     
@@ -218,34 +218,34 @@ Auto tabbing script- By JavaScriptKit.com
 http://www.javascriptkit.com
 */ 
 function autotab(original,destination) {
-	if (original.getAttribute && original.value.length == original.getAttribute("maxlength")) {
-		destination.focus();
-	}
+    if (original.getAttribute && original.value.length == original.getAttribute("maxlength")) {
+        destination.focus();
+    }
 }
 
 function switchContactNumberFormats(country) {
-	
-	var countryField = $(country);
-	if (!countryField) {
-		return;
-	}
+    
+    var countryField = $(country);
+    if (!countryField) {
+        return;
+    }
     var countryName = countryField.options[countryField.selectedIndex].text;
    
     if ($('no_format_phone')) {
-	    if (countryName == 'United States' || countryName == 'Canada') {
-	        $('no_format_phone').hide();
-	        $('no_format_fax').hide();
-	        $('no_format_tty').hide();
-	        $('us_format_phone').show();
-	        $('us_format_fax').show();
-	        $('us_format_tty').show();
-	    } else {
-	        $('no_format_phone').show();
-	        $('no_format_fax').show();
-	        $('no_format_tty').show();
-	        $('us_format_phone').hide();
-	        $('us_format_fax').hide();
-	        $('us_format_tty').hide();
-	    }
+        if (countryName == 'United States' || countryName == 'Canada') {
+            $('no_format_phone').hide();
+            $('no_format_fax').hide();
+            $('no_format_tty').hide();
+            $('us_format_phone').show();
+            $('us_format_fax').show();
+            $('us_format_tty').show();
+        } else {
+            $('no_format_phone').show();
+            $('no_format_fax').show();
+            $('no_format_tty').show();
+            $('us_format_phone').hide();
+            $('us_format_fax').hide();
+            $('us_format_tty').hide();
+        }
     }
 }
