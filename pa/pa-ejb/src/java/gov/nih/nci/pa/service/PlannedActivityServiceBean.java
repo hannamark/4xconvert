440a8cfced3491b77ebc95c08baed3c3409f0d3e
amiruddinn <amiruddinn@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-12-02 20:59:17 +0000

    removed security on Planned Activity
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@7747 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/PlannedActivityServiceBean.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/PlannedActivityServiceBean.java
@@ -80,13 +80,13 @@ package gov.nih.nci.pa.service;
 
 import gov.nih.nci.pa.util.HibernateSessionInterceptor;
 
-import javax.annotation.security.RolesAllowed;
+//import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.interceptor.Interceptors;
 
-import org.jboss.annotation.security.SecurityDomain;
+//import org.jboss.annotation.security.SecurityDomain;
 
 /**
  * @author Hugh Reinhart
@@ -97,8 +97,8 @@ import org.jboss.annotation.security.SecurityDomain;
 @Stateless
 @Interceptors({ HibernateSessionInterceptor.class })
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
-@SecurityDomain("pa")
-@RolesAllowed({"gridClient", "client" , "Abstractor" , "Submitter" , "Outcomes" })
+//@SecurityDomain("pa")
+//@RolesAllowed({"gridClient", "client" , "Abstractor" , "Submitter" , "Outcomes" })
 public class PlannedActivityServiceBean extends PlannedActivityBeanLocal implements PlannedActivityServiceRemote {
     
    
