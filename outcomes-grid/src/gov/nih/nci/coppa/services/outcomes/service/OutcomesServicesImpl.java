9a71a09db22ef76f1ef4a326485a433add692ac9
lustbaders <lustbaders@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2009-12-09 01:52:21 +0000

    PO-1445: Added initial introduce skeleton for outcomes grid services
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@7855 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ outcomes-grid/src/gov/nih/nci/coppa/services/outcomes/service/OutcomesServicesImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.coppa.services.outcomes.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class OutcomesServicesImpl extends OutcomesServicesImplBase {
+
+	
+	public OutcomesServicesImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
---
+++ outcomes-grid/src/gov/nih/nci/coppa/services/outcomes/service/globus/resource/OutcomesServicesResource.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.coppa.services.outcomes.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this OutcomesServicesResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class OutcomesServicesResource extends OutcomesServicesResourceBase {
+
+}
