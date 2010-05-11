932e575db90e7c17127ff36ebc651a74e6af0e3e
evanselaj <evanselaj@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2010-05-11 14:30:46 +0000

    PO-1666: Addressing peer review comments from slustbader for grid account creation in registry.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@9296 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/other/migrateRegistryAccount.sql
+++ pa/dbscripts/other/migrateRegistryAccount.sql
@@ -1,4 +1,5 @@
 --The good news is it appears that the registry user's login name is only used to determine the user's full name
 --so as long as we properly update the login names we should be fine with exisiting registry accounts.
---The below sql is the basis for what will need to be run to manaully migrate those accounts
+--The below sql is the basis for what will need to be run to manaully migrate those accounts.
+--One thing to note, we will need to enable grid login in registry before we migrate over.
 UPDATE CSM_USER SET LOGIN_NAME = 'NEW_LOGIN_NAME', PASSWORD = '' where LOGIN_NAME = 'OLD_LOGIN_NAME';
