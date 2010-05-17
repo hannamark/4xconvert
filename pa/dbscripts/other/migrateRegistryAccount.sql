5660468f09af6314409e5f722beaf528e5c72e3b
evanselaj <evanselaj@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2010-05-17 21:08:51 +0000

    Updating migration script for existing registry accounts to go through and update all the necessary columns in the entire database. Peer reviewed by ludetc and slustbader.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@9386 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/other/migrateRegistryAccount.sql
+++ pa/dbscripts/other/migrateRegistryAccount.sql
@@ -1,5 +1,18 @@
---The good news is it appears that the registry user's login name is only used to determine the user's full name
---so as long as we properly update the login names we should be fine with exisiting registry accounts.
---The below sql is the basis for what will need to be run to manaully migrate those accounts.
---One thing to note, we will need to enable grid login in registry before we migrate over.
-UPDATE CSM_USER SET LOGIN_NAME = 'NEW_LOGIN_NAME', PASSWORD = '' where LOGIN_NAME = 'OLD_LOGIN_NAME';
+CREATE OR REPLACE FUNCTION migrate_registry_account(old_login_name varchar, new_login_name varchar) RETURNS void as $$
+DECLARE 
+        tr RECORD;
+BEGIN
+     update csm_user set login_name = new_login_name, password = '', update_date = now() where login_name = old_login_name;
+     FOR tr in SELECT * FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema') LOOP
+     
+     IF (SELECT count(*) FROM information_schema.columns WHERE table_name = tr.table_name and column_name = 'user_last_created') > 0 THEN
+        EXECUTE 'update ' || tr.table_name || ' set user_last_created=' || quote_literal(new_login_name) || ', date_last_updated = now() where user_last_created=' || quote_literal(old_login_name) || ';';
+        EXECUTE 'update ' || tr.table_name || ' set user_last_updated=' || quote_literal(new_login_name) || ', date_last_updated = now() where user_last_updated=' || quote_literal(old_login_name) || ';';
+     END IF;
+END LOOP;   
+END 
+$$ LANGUAGE 'plpgsql';
+
+--To migrate an account, peform the following call.
+--select migrate_registry_account('OLD_ACCOUNT_NAME', '/O=caBIG/OU=caGrid/OU=Training/OU=Dorian/CN=NEW_LOGIN_NAME');
+
