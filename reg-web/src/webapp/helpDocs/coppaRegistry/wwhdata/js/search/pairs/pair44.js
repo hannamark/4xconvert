58e1138388d91dc4266fdb3ea94f4e6f73668ed8
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2010-06-24 18:53:12 +0000

    adding new help content from doc team
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@9769 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/src/webapp/helpDocs/coppaRegistry/wwhdata/js/search/pairs/pair44.js
@@ -0,0 +1,20 @@
+function FileData_Pairs(x)
+{
+x.t("trials","instructions");
+x.t("trials","registering");
+x.t("trials","completing");
+x.t("instructions","completing");
+x.t("summary","information");
+x.t("non-proprietary","trials");
+x.t("new","trials");
+x.t("registering","new");
+x.t("registering","proprietary");
+x.t("thesummary","information");
+x.t("completing","summary");
+x.t("completing","thesummary");
+x.t("section","proprietary");
+x.t("proprietary","trials");
+x.t("proprietary","non-proprietary");
+x.t("information","section");
+x.t("information","proprietary");
+}
