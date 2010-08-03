5f1949c97dc254caaf6814ecae564f0f6e332451
niktevv <niktevv@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2010-08-03 15:17:11 +0000

    PO-2388-Remove duplicate EncoderDecoders and replace Sun's BASE64Encoder/Decoder with commons-codec's encoder/decoders in Viewer/Registry -- Peer review ludetc
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@10033 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- reg-web/src/java/gov/nih/nci/registry/util/EncoderDecoder.java
+++ reg-web/src/java/gov/nih/nci/registry/util/EncoderDecoder.java
@@ -1,35 +1,21 @@
 package gov.nih.nci.registry.util;
 
 
-import java.io.IOException;
-
-import org.apache.log4j.Logger;
-
-//CHECKSTYLE:OFF illegal import - PO-2388
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-//CHECKSTYLE:ON
-
+import org.apache.commons.codec.binary.Base64;
 /**
  *
  * @author Bala Nair
  */
-@SuppressWarnings({ "PMD" })
+
 public class EncoderDecoder {
-
-    private static final Logger LOG = Logger.getLogger(EncoderDecoder.class);
-
     /**
      *
      * @param data data
      * @return encoded String
      */
     public String encodeString(String data) {
-        BASE64Encoder base64 = new BASE64Encoder();
-        String encoded = base64.encode(data.getBytes());
-        return encoded;
+        return  new String(Base64.encodeBase64(data.getBytes()));
     }
-
 
     /**
      *
@@ -37,15 +23,6 @@ public class EncoderDecoder {
      * @return decoded String
      */
     public String decodeString(String data)  {
-        BASE64Decoder b2 = new BASE64Decoder();
-
-        String decodeString = null;
-        try {
-            byte[] decode = b2.decodeBuffer(data);
-            decodeString = new String(decode);
-        } catch (IOException e) {
-            LOG.error("Exception in decoding String: " + data, e);
-        }
-        return decodeString;
+        return new String(Base64.decodeBase64(data.getBytes()));
     }
 }
