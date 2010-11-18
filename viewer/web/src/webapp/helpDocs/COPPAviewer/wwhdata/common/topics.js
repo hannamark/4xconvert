2f86df6bd7dccf6fd6488eb5fb4616c6d671a0b1
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2010-11-18 16:18:45 +0000

    Adding viewer help from doc team
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@10834 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- viewer/web/src/webapp/helpDocs/COPPAviewer/wwhdata/common/topics.js
+++ viewer/web/src/webapp/helpDocs/COPPAviewer/wwhdata/common/topics.js
@@ -1,8 +1,9 @@
 function  WWHBookData_MatchTopic(P)
 {
 var C=null;
-if(P=="welcome")C="Welcome.1.1.html#1052621";
-if(P=="login")C="GettingStarted.2.4.html#1079060";
+if(P=="Welcome")C="Welcome.1.1.html#1078027";
+if(P=="register")C="GettingStartedViewer.2.5.html#1096048";
+if(P=="login")C="GettingStartedViewer.2.6.html#1079060";
 if(P=="ctro_report")C="report.3.2.html#1127590";
 if(P=="run_reports")C="report.3.3.html#1140753";
 if(P=="run_summary_submission")C="report.3.4.html#1142198";
@@ -11,5 +12,8 @@ if(P=="run_portfolio_average")C="report.3.6.html#1135131";
 if(P=="run_submitted_date")C="report.3.7.html#1133398";
 if(P=="run_submitted_institution")C="report.3.8.html#1133399";
 if(P=="run_current_milestone")C="report.3.9.html#1135604";
+if(P=="run_summ4_types")C="report.3.10.html#1133377";
+if(P=="reports:exporting;exporting_reports;report_data,_exporting_to_Excel")C="report.3.11.html#1145213";
+if(P=="my_account")C="userAccount.4.2.html#1089507";
 return C;
 }
