904479205673fe75c3627d686e04f3b906e0f73f
gannk <gannk@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2011-11-29 21:18:06 +0000

    Remove the checks for the popup since it no longer exists.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13307 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- tests/performance/ctro-performance/src/main/java/gov/nih/nci/qa/selenium/PageComponents/AdHocReport/DiseaseSection.java
+++ tests/performance/ctro-performance/src/main/java/gov/nih/nci/qa/selenium/PageComponents/AdHocReport/DiseaseSection.java
@@ -3,18 +3,13 @@ package gov.nih.nci.qa.selenium.PageComponents.AdHocReport;
 import gov.nih.nci.qa.selenium.PageComponents.AdHocPageDiseaseLookUp;
 import gov.nih.nci.qa.selenium.util.SplitUtil;
 
-import java.util.List;
-
 import org.javasimon.Split;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DiseaseSection {
 
@@ -64,44 +59,6 @@ public class DiseaseSection {
 		diseaseField.sendKeys(Keys.RETURN);
 		addAllButton.click();
 
-		split.stop();
-	}
-
-	private void waitForLoadingToFinish() {
-		Split split = SplitUtil.getPageElementSplit(webDriver,
-				"AdHocReportPage", "waitForLoadingToFinish");
-		WebElement diseaseAccordion = webDriver.findElement(By
-				.id("diseasesSection"));
-		// If it contains "Loading..." look again.
-		while (diseaseAccordion.getText().contains("Loading...")) {
-			diseaseAccordion = (new WebDriverWait(webDriver, 10))
-					.until(new ExpectedCondition<WebElement>() {
-						public WebElement apply(WebDriver d) {
-							return d.findElement(By.id("diseasesSection"));
-						}
-					});
-		}
-		split.stop();
-	}
-
-	private void waitForPopupToAppear() {
-		Split split = SplitUtil.getPageElementSplit(webDriver,
-				"AdHocReportPage", "waitForFrameToAppear");
-
-		(new WebDriverWait(webDriver, 15))
-				.until(new ExpectedCondition<Boolean>() {
-					public Boolean apply(WebDriver d) {
-						d.switchTo().defaultContent();
-						List<WebElement> iframes = d.findElements(By
-								.tagName("iframe"));
-						for (WebElement iframe : iframes) {
-							d.switchTo().frame(iframe);
-							d.findElement(By.tagName("body"));
-							d.switchTo().defaultContent();
-						}
-						return true;
-					}
-				});
 		split.stop();
 	}
 
