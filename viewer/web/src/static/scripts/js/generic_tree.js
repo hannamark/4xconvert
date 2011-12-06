b73230fb37e95bcc513ce4bf6baadb0f369e7f29
shrabsteinp <shrabsteinp@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2011-12-06 20:53:34 +0000

    PO-4386: Allow disease tree widget to have multiple roots.  Peer review by CR-PO-1348
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13375 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- viewer/web/src/static/scripts/js/generic_tree.js
+++ viewer/web/src/static/scripts/js/generic_tree.js
@@ -11,35 +11,36 @@ function NBNode( item ) {
 
 NBNode.prototype = {
     addChild: function( node ) {
-    	if( this.children == null )
-    		this.children = {};
-		this.children[node.id] = node;
+        if( this.children == null )
+            this.children = {};
+        this.children[node.id] = node;
     }
 }
-		
+        
 
 //******************
 //** Generic Tree **
 //******************
 function NBTree() {
-	this.root = null;
+    // Always create abstract node as a top-level root to mitigate both multi-root and single-root data sets
+    this.root = new NBNode({'id':-2, 'name':'Abstract Root Node', 'parentId':null}); 
 }
 
 NBTree.prototype = {
-	buildFromFlatData : function( flatData ) {
-		var nodes={};
-		for( var i in flatData ) {
-			if(!flatData.hasOwnProperty(i))
-				continue;
-			var item = flatData[i];
-			item['id'] = i;
-			nodes[i] = new NBNode(item);
-		}
-		
-		for( var i in nodes ) {
-			if(!nodes.hasOwnProperty(i))
-				continue;
-		    var node = nodes[i];
+    buildFromFlatData : function( flatData ) {
+        var nodes={};
+        for( var i in flatData ) {
+            if(!flatData.hasOwnProperty(i))
+                continue;
+            var item = flatData[i];
+            item['id'] = i;
+            nodes[i] = new NBNode(item);
+        }
+        
+        for( var i in nodes ) {
+            if(!nodes.hasOwnProperty(i))
+                continue;
+            var node = nodes[i];
             if ( node.parentId != null && node.parentId.length != undefined  && !(node.parentId.length==1 && node.parentId[0]==null) ) {
                 for (var i=0 ; i<node.parentId.length ; i++ ) {
                     var parentId = node.parentId[i];
@@ -48,44 +49,47 @@ NBTree.prototype = {
                     var parent =  nodes[parentId];
                     parent.addChild(node);
                 }
-		    } else {
-		    	if( this.root != null )
-		    		alert( 'Tree data is inconsistent: node with the id=' + node.id + ' and id=' + this.root.id + ' both have parents=null');
-				this.root = node;
-		    }
-		}
-	},
+            } else {
+                this.root.addChild(node);
+            }
+        }
+    },
 
-	generateJstreeJsonData : function() {
-		var go = function(node, compositeId) {
-			compositeId += '_'+node.id;
-			var jsNode = { 
-				'attr' : { 
-					'id' : 'ptid'+compositeId,
-					'title' : 'Click to add this item to your selections for the search'
-				},
-				'data' : {
-					'title' : node.name,
-					'icon' : 'folder'
-				}, 
-				'metadata' : { 'id' : compositeId },
-			};
-			if( node.children != null ) {
-				//jsNode['state'] = 'closed'; 	
-				jsNode['children'] = [];
-				jQuery.map(node.children, function(val,i) {
-					jsNode.children.push(go(val,compositeId));  
-				});
-			}
-			return jsNode;
-		};
-		
-		return { 
-			'data' : [go(this.root,'')],
-			'progressive_render' : false 
-		};
-	},
-	
+    generateJstreeJsonData : function() {
+        var go = function(node, compositeId) {
+            compositeId += '_'+node.id;
+            var jsNode = { 
+                'attr' : { 
+                    'id' : 'ptid'+compositeId,
+                    'title' : 'Click to add this item to your selections for the search'
+                },
+                'data' : {
+                    'title' : node.name,
+                    'icon' : 'folder'
+                }, 
+                'metadata' : { 'id' : compositeId },
+            };
+            if( node.children != null ) {
+                //jsNode['state'] = 'closed';   
+                jsNode['children'] = [];
+                jQuery.map( node.children, function(val,i) {
+                    jsNode.children.push(go(val,compositeId));  
+                });
+            }
+            return jsNode;
+        };
+        
+        var jsTreeData = [];
+        jQuery.map( this.root.children, function(val,i) {
+            jsTreeData.push(go(val,''));  
+        });
+ 
+        return { 
+            'data' : jsTreeData,
+            'progressive_render' : false 
+        };
+    },
+    
     add : function(id, name, parentId){
     },
 
