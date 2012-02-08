d6716f60e2a4c8e541f961720995b14284fa4342
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-02-08 16:28:53 +0000

    PO-4626 - DW Milestone
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13619 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/groovy/milestone.groovy
@@ -0,0 +1,87 @@
+import groovy.sql.Sql
+def sql = """SELECT
+				sm.milestone_date,
+				sm.milestone_code,
+				sm.date_last_created, 
+				sm.date_last_updated,
+                sm.identifier,
+                sm.comment_text,
+                nci_id.extension,
+                CASE WHEN NULLIF(ru_creator.first_name, '') is not null THEN ru_creator.first_name || ' ' || ru_creator.last_name
+                     WHEN NULLIF(split_part(creator.login_name, 'CN=', 2), '') is null THEN creator.login_name
+                     ELSE split_part(creator.login_name, 'CN=', 2)
+                END as creator,
+                CASE WHEN NULLIF(ru_updater.first_name, '') is not null THEN ru_updater.first_name || ' ' || ru_updater.last_name
+                     WHEN NULLIF(split_part(updater.login_name, 'CN=', 2), '') is null THEN updater.login_name
+                     ELSE split_part(updater.login_name, 'CN=', 2)
+                END as updater      
+                FROM STUDY_MILESTONE sm
+                inner join study_otheridentifiers as nci_id on nci_id.study_protocol_id = sm.study_protocol_identifier
+                    and nci_id.root = '2.16.840.1.113883.3.26.4.3'
+                left outer join csm_user as creator on sm.user_last_created_id = creator.user_id   
+                left outer join registry_user as ru_creator on ru_creator.csm_user_id = creator.user_id
+                left outer join csm_user as updater on sm.user_last_created_id = updater.user_id
+                left outer join registry_user as ru_updater on ru_updater.csm_user_id = updater.user_id"""
+
+def sourceConnection = Sql.newInstance(properties['datawarehouse.pa.source.jdbc.url'], properties['datawarehouse.pa.source.db.username'],
+    properties['datawarehouse.pa.source.db.password'], properties['datawarehouse.pa.source.jdbc.driver'])
+def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'],
+    properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
+def milestones = destinationConnection.dataSet("DW_STUDY_MILESTONE");
+
+sourceConnection.eachRow(sql) { row ->
+    milestones.add(
+    		comments: row.comment_text,
+    		name: row.milestone_code,
+    		date: row.milestone_date,
+    		date_created: row.date_last_created,
+    		date_last_updated: row.date_last_updated,
+            internal_system_id: row.identifier,
+            nci_id: row.extension, 
+            user_name_created: row.creator,
+            user_name_last_updated: row.updater)
+            }
+            
+            
+   
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Submission Received Date'    
+	where NAME='SUBMISSION_RECEIVED'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Submission Acceptance Date'    
+	where NAME='SUBMISSION_ACCEPTED'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Submission Rejection Date'    
+	where NAME='SUBMISSION_REJECTED'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Administrative Processing Start Date'    
+	where NAME='ADMINISTRATIVE_PROCESSING_START_DATE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Administrative Processing Completed Date'    
+	where NAME='ADMINISTRATIVE_PROCESSING_COMPLETED_DATE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Ready for Administrative QC Date'    
+	where NAME='ADMINISTRATIVE_READY_FOR_QC'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Administrative QC Start Date'    
+	where NAME='ADMINISTRATIVE_QC_START'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Administrative QC Completed Date'    
+	where NAME='ADMINISTRATIVE_QC_COMPLETE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Scientific Processing Start Date'    
+	where NAME='SCIENTIFIC_PROCESSING_START_DATE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Scientific Processing Completed Date'    
+	where NAME='SCIENTIFIC_PROCESSING_COMPLETED_DATE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Ready for Scientific QC Date'    
+	where NAME='SCIENTIFIC_READY_FOR_QC'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Scientific QC Start Date'    
+	where NAME='SCIENTIFIC_QC_START'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Scientific QC Completed Date'    
+	where NAME='SCIENTIFIC_QC_COMPLETE'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Ready for Trial Summary Report Date'    
+	where NAME='READY_FOR_TSR'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Trial Summary Report Sent Date'    
+	where NAME='TRIAL_SUMMARY_SENT'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Submitter Trial Summary Report Feedback Date'    
+	where NAME='TRIAL_SUMMARY_FEEDBACK'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Initial Abstraction Verified Date'    
+	where NAME='INITIAL_ABSTRACTION_VERIFY'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='On-going Abstraction Verified Date'    
+	where NAME='ONGOING_ABSTRACTION_VERIFICATION'""")
+destinationConnection.execute("""UPDATE DW_STUDY_MILESTONE SET NAME='Late Rejection Date'    
+	where NAME='LATE_REJECTION_DATE'""")
+	
+	
+	            
---
+++ pa/data-warehouse/sql/milestone.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS DW_STUDY_MILESTONE;
+
+CREATE TABLE DW_STUDY_MILESTONE (
+    COMMENTS character varying(200),
+    DATE date,
+    DATE_CREATED date,
+    DATE_LAST_UPDATED date,
+    INTERNAL_SYSTEM_ID INTEGER not null,
+	NAME character varying (50),
+    NCI_ID character varying(255),
+    USER_NAME_CREATED character varying(500),
+    USER_NAME_LAST_UPDATED character varying(500)
+);
+
+CREATE INDEX DW_STUDY_MILESTONE_COMMENTS_IDX on dw_study_milestone(comments);
+CREATE INDEX DW_STUDY_MILESTONE_DATE_IDX on dw_study_milestone(date);
+CREATE INDEX DW_STUDY_MILESTONE_DATE_CREATED_IDX on dw_study_milestone(date_created);
+CREATE INDEX DW_STUDY_MILESTONE_DATE_UPDATED_IDX on dw_study_milestone(date_last_updated);
+CREATE INDEX DW_STUDY_MILESTONE_ID_IDX on dw_study_milestone(internal_system_id);
+CREATE INDEX DW_STUDY_MILESTONE_NAME_IDX on dw_study_milestone(name);
+CREATE INDEX DW_STUDY_MILESTONE_NCI_ID_IDX on dw_study_milestone(nci_id);
+CREATE INDEX DW_STUDY_MILESTONE_USER_CREATED_IDX on dw_study_milestone(user_name_created);
+CREATE INDEX DW_STUDY_MILESTONE_USER_UPDATED_IDX on dw_study_milestone(user_name_last_updated);
