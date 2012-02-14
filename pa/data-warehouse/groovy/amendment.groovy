a297dc55b623378789ada2af943679e328cb2ce8
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-02-14 19:53:34 +0000

    PO-4670 - include all amendments in DW
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13683 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/amendment.groovy
+++ pa/data-warehouse/groovy/amendment.groovy
@@ -7,8 +7,11 @@ def sql =
             sp.amendment_reason_code,
             sp.identifier as system_id,
 			sp.submission_number,
-			submitter.first_name || ' ' || submitter.last_name as submitter
+			submitter.first_name || ' ' || submitter.last_name as submitter,
+			nci_id.extension		
             from STUDY_PROTOCOL sp
+            inner join study_otheridentifiers as nci_id on nci_id.study_protocol_id = sp.identifier
+            and nci_id.root = '2.16.840.1.113883.3.26.4.3'
             left outer join registry_user as submitter on submitter.csm_user_id = sp.user_last_created_id
             where sp.amendment_date is not null"""
 
@@ -26,19 +29,11 @@ sourceConnection.eachRow(sql) { row ->
             number: row.amendment_number, 
             internal_system_id: row.system_id,
             submission_number: row.submission_number, 
+            nci_id: row.extension,
             submitter_name: row.submitter)
         } catch (Exception e) {
             println "Error adding row : " + row + " - " + e.getMessage(); 
         }
     }
 }
-
-sourceConnection.eachRow("""select oid.extension as nci_id, oid.study_protocol_id from study_otheridentifiers as oid
-    inner join study_protocol as sp on sp.identifier = oid.study_protocol_id and sp.status_code = 'ACTIVE'
-    where oid.root = '2.16.840.1.113883.3.26.4.3' and oid.extension is not null""") { row ->
-        id = row.study_protocol_id
-        nci_id = row.nci_id
-        destinationConnection.execute("UPDATE DW_STUDY_AMENDMENT SET NCI_ID = ? where internal_system_id = ?", [nci_id, id])
-    }
-destinationConnection.execute("DELETE FROM DW_STUDY_AMENDMENT where NCI_ID is null")
 
