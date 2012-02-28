765d58d64a0c46bfe82a73425b08db2963d150e5
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-02-28 21:03:45 +0000

    PO-4690 - Add person audit to DW
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13757 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/groovy/person_audit.groovy
@@ -0,0 +1,31 @@
+import groovy.sql.Sql
+
+def sql = """select 
+			 per.firstname,
+			 per.lastname,	
+			 per.id,
+			 audit.username,
+			 audit.createddate,
+			 ctepid.assigned_identifier_extension as ctep_id
+			 from Auditlogrecord audit
+			 join person per on per.id = audit.entityid
+			 left outer join identifiedperson ctepid on ctepid.player_id = per.id and ctepid.assigned_identifier_root = 'Cancer Therapy Evaluation Program Person Identifier'
+			 where audit.entityname = 'Person'
+			"""
+
+def sourceConnection = Sql.newInstance(properties['datawarehouse.po.jdbc.url'], properties['datawarehouse.po.db.username'], 
+    properties['datawarehouse.po.db.password'], properties['datawarehouse.po.jdbc.driver'])
+def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'], 
+    properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
+
+def audits = destinationConnection.dataSet("DW_PERSON_AUDIT")
+
+sourceConnection.eachRow(sql) { row ->
+    audits.add(
+    	ctep_id: row.ctep_id,
+    	name: row.firstname + " " + row.lastname,
+    	po_id: row.id,
+    	date: row.createddate,
+    	username: row.username
+	)
+}
---
+++ pa/data-warehouse/sql/person_audit.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS DW_PERSON_AUDIT;
+
+CREATE TABLE DW_PERSON_AUDIT ( 
+	CTEP_ID character varying (50),
+	NAME character varying (255),
+	PO_ID integer,
+	DATE date,
+	USERNAME character varying (255)
+	)
+	;
