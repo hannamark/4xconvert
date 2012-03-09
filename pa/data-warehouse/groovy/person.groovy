bd2557b4f559533651df23427c93e75709862486
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-03-09 15:05:07 +0000

    PO-4677 - Add Person to DW
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13802 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/groovy/person.groovy
@@ -0,0 +1,72 @@
+import groovy.sql.Sql
+
+def sql = """select 
+			 add.streetaddressline,
+			 add.deliveryaddressline,
+			 add.postalcode,
+			 per.birthdate,
+			 add.cityormunicipality,
+			 country.name as country_name,
+			 ctepid.assigned_identifier_extension as ctep_id,
+			 per.comments,
+			 per.firstname,
+			 per.lastname,	
+			 per.id,
+			 per.prefix,
+			 per.status,
+			 per.statusdate,
+			 per.sex,
+			 per.suffix,
+			 add.stateorprovince,
+			 e.value as email,
+			 fax.value as faxnumber,	 
+			 phone.value as phone,
+			 tty.value as tty,
+			 url.value as url
+			 from Person per
+			 join address add on add.id = per.postal_address_id
+			 left outer join identifiedperson ctepid on ctepid.player_id = per.id and ctepid.assigned_identifier_root = 'Cancer Therapy Evaluation Program Person Identifier'
+			 left outer join country on country.id = add.country_id
+			 left outer join person_email p_e on p_e.person_id = per.id
+			 left outer join email e on e.id = p_e.email_id
+			 left outer join person_fax p_f on p_f.person_id = per.id
+			 left outer join phonenumber fax on fax.id = p_f.fax_id
+			 left outer join person_phone p_ph on p_ph.person_id = per.id
+			 left outer join phonenumber phone on phone.id = p_ph.phone_id			 
+			 left outer join person_tty p_tty on p_tty.person_id = per.id
+			 left outer join phonenumber tty on tty.id = p_tty.tty_id
+			 left outer join person_url p_url on p_url.person_id = per.id
+			 left outer join url url on url.id = p_url.url_id
+			"""
+
+def sourceConnection = Sql.newInstance(properties['datawarehouse.po.jdbc.url'], properties['datawarehouse.po.db.username'], 
+    properties['datawarehouse.po.db.password'], properties['datawarehouse.po.jdbc.driver'])
+def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'], 
+    properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
+
+def persons = destinationConnection.dataSet("DW_PERSON")
+
+sourceConnection.eachRow(sql) { row ->
+    persons.add(
+    	address_line_1: row.streetaddressline,
+    	address_line_2: row.deliveryaddressline,
+    	postal_code: row.postalcode,
+    	birthdate: row.birthdate,
+    	city: row.cityormunicipality,
+    	country: row.country_name,
+    	ctep_id: row.ctep_id,
+    	curator_comment: row.comments,
+    	name: row.firstname + " " + row.lastname,
+ 		po_id: row.id,
+ 		prefix: row.prefix,
+ 		status: row.status,
+ 		status_date: row.statusdate,
+ 		sex_code: row.sex,
+ 		state_or_province: row.stateorprovince,
+ 		suffix: row.suffix,
+ 		email: row.email,
+ 	 	fax: row.faxnumber,
+ 	 	phone: row.phone,
+ 	 	tty: row.tty
+	)
+}
---
+++ pa/data-warehouse/sql/person.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS DW_PERSON;
+
+CREATE TABLE DW_PERSON ( 
+	ADDRESS_LINE_1 character varying (254),
+	ADDRESS_LINE_2 character varying (254),
+	POSTAL_CODE character varying (30),
+	BIRTHDATE date,
+	CITY character varying (50),
+	COUNTRY character varying (50),
+	CTEP_ID character varying (50),
+	CURATOR_COMMENT text,
+	NAME character varying (255),
+	PO_ID integer,
+	PREFIX character varying (10),
+	STATUS character varying (20),
+	STATUS_DATE date,
+	SEX_CODE character varying (20),
+	STATE_OR_PROVINCE character varying (255),
+	SUFFIX character varying (10),
+	EMAIL character varying (256),
+	FAX character varying (256),
+	PHONE character varying (256),
+	TTY character varying (50)
+	)
+	;
