3a4528ad5e42e36a1d507b093bbe01028fcf060a
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-03-09 21:01:11 +0000

    PO-4714 - DW Organization
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13807 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/groovy/person_role.groovy
@@ -0,0 +1,41 @@
+import groovy.sql.Sql
+
+
+def poSourceConnection = Sql.newInstance(properties['datawarehouse.po.jdbc.url'], properties['datawarehouse.po.db.username'], 
+    properties['datawarehouse.po.db.password'], properties['datawarehouse.po.jdbc.driver'])
+def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'], 
+    properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
+def paSourceConnection = Sql.newInstance(properties['datawarehouse.pa.source.jdbc.url'], properties['datawarehouse.pa.source.db.username'],
+    properties['datawarehouse.pa.source.db.password'], properties['datawarehouse.pa.source.jdbc.driver'])    
+    
+def roles = destinationConnection.dataSet("DW_PERSON_ROLE")
+
+def hcpSql = """select person_id from HealthcareProvider where status in ('ACTIVE', 'PENDING')"""
+def hcps = [];
+
+poSourceConnection.eachRow(hcpSql) { row ->
+        hcps << row.person_id;
+}
+
+
+hcps.each {row ->
+	roles.add(paSourceConnection.firstRow("select * from dw_person where po_id=?", [row]))
+}
+
+destinationConnection.execute("UPDATE DW_PERSON_ROLE set ROLE_NAME = 'Healthcare Provider' where Role_name is null")
+
+    
+def crsSql = """select person_id from ClinicalResearchStaff where status in ('ACTIVE', 'PENDING')"""
+def crss = [];
+
+poSourceConnection.eachRow(crsSql) { row ->
+        crss << row.person_id;
+}
+
+crss.each {row ->
+	roles.add(paSourceConnection.firstRow("select * from dw_person where po_id=?", [row]))
+}
+
+destinationConnection.execute("UPDATE DW_PERSON_ROLE set ROLE_NAME = 'Clinical Research Staff' where Role_name is null")
+
+    
---
+++ pa/data-warehouse/sql/person_role.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS DW_PERSON_ROLE;
+
+CREATE TABLE DW_PERSON_ROLE (
+	ROLE_NAME character varying (20),
+	ADDRESS_LINE_1 character varying (254),
+	ADDRESS_LINE_2 character varying (254),
+	POSTAL_CODE character varying (30),
+	BIRTHDATE date,
+	CITY character varying (50),
+	COUNTRY character varying (50),
+	CTEP_ID character varying (50),
+	CURATOR_COMMENT text,
+	NAME character varying (255),
+	PO_ID integer,
+	PREFIX character varying (10),
+	STATUS character varying (20),
+	STATUS_DATE date,
+	SEX_CODE character varying (20),
+	STATE_OR_PROVINCE character varying (255),
+	SUFFIX character varying (10),
+	EMAIL character varying (256),
+	FAX character varying (256),
+	PHONE character varying (256),
+	TTY character varying (50)
+	)
+	;
