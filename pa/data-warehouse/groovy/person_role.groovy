450e66da7d29f3e26e7b76ba1b1fc6806cf3b531
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-03-14 16:50:23 +0000

    DW - Test dw_person_role fix
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13834 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/person_role.groovy
+++ pa/data-warehouse/groovy/person_role.groovy
@@ -2,11 +2,10 @@ import groovy.sql.Sql
 
 def poSourceConnection = Sql.newInstance(properties['datawarehouse.po.jdbc.url'], properties['datawarehouse.po.db.username'], 
     properties['datawarehouse.po.db.password'], properties['datawarehouse.po.jdbc.driver'])
+    
 def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'], 
     properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
-def paSourceConnection =    Sql.newInstance(properties['datawarehouse.pa.source.jdbc.url'], properties['datawarehouse.pa.source.db.username'],
-    properties['datawarehouse.pa.source.db.password'], properties['datawarehouse.pa.source.jdbc.driver'])    
-    
+ 
 def roles = destinationConnection.dataSet("STG_DW_PERSON_ROLE")
 
 def hcpSql = """select person_id from HealthcareProvider where status in ('ACTIVE', 'PENDING')"""
@@ -31,7 +30,7 @@ poSourceConnection.eachRow(crsSql) { row ->
 }
 
 crss.each {row ->
-	roles.add(paSourceConnection.firstRow("select * from STG_DW_PERSON where po_id=?", [row]))
+	roles.add(destinationConnection.firstRow("select * from STG_DW_PERSON where po_id=?", [row]))
 }
 
 destinationConnection.execute("UPDATE STG_DW_PERSON_ROLE set ROLE_NAME = 'Clinical Research Staff' where Role_name is null")
