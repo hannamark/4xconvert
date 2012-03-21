52d0e08718f6136455a57019594f6eb847a88721
ludetc <ludetc@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-03-21 14:07:16 +0000

    PO-4678 - Add Org to DW
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13880 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/groovy/organization.groovy
@@ -0,0 +1,77 @@
+import groovy.sql.Sql
+
+def sql = """select 
+			 add.streetaddressline,
+			 add.deliveryaddressline,
+			 add.postalcode,
+			 add.cityormunicipality,
+			 country.name as country_name,
+			 ctepid.assigned_identifier_extension as ctep_id,
+			 org.comments,
+			 org.id,
+			 org.status,
+			 org.statusdate,
+			 add.stateorprovince,
+			 e.value as email,
+			 fax.value as faxnumber,	 
+			 phone.value as phone,
+			 tty.value as tty,
+			 url.value as url,
+			 fam.name as fam_name,
+			 f_o_r.functionaltype,
+			 crcount
+			 from Organization org
+			 left outer join address add on add.id = org.postal_address_id
+			 left outer join identifiedorganization ctepid on ctepid.player_id = org.id and ctepid.assigned_identifier_root = 'Cancer Therapy Evaluation Program Person Identifier'
+			 left outer join country on country.id = add.country_id
+			 left outer join organization_email o_e on o_e.organization_id = org.id
+			 left outer join email e on e.id = o_e.email_id
+			 left outer join organization_fax o_f on o_f.organization_id = org.id
+			 left outer join phonenumber fax on fax.id = o_f.fax_id
+			 left outer join organization_phone o_ph on o_ph.organization_id = org.id
+			 left outer join phonenumber phone on phone.id = o_ph.phone_id			 
+			 left outer join organization_tty o_tty on o_tty.organization_id = org.id
+			 left outer join phonenumber tty on tty.id = o_tty.tty_id
+			 left outer join organization_url o_url on o_url.organization_id = org.id
+			 left outer join url url on url.id = o_url.url_id
+			 left outer join familyorganizationrelationship f_o_r on f_o_r.organization_id = org.id
+			 left outer join family fam on fam.id = f_o_r.family_id
+			 left outer join (select count(*) as crcount, target from organizationcr group by target) AS orgcr
+				on orgcr.target = org.id
+			"""
+
+def sourceConnection = Sql.newInstance(properties['datawarehouse.po.jdbc.url'], properties['datawarehouse.po.db.username'], 
+    properties['datawarehouse.po.db.password'], properties['datawarehouse.po.jdbc.driver'])
+def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jdbc.url'], properties['datawarehouse.pa.dest.db.username'], 
+    properties['datawarehouse.pa.dest.db.password'], properties['datawarehouse.pa.dest.jdbc.driver'])
+
+def orgs = destinationConnection.dataSet("STG_DW_ORGANIZATION")
+
+sourceConnection.eachRow(sql) { row ->
+    orgs.add(
+    	address_line_1: row.streetaddressline,
+    	address_line_2: row.deliveryaddressline,
+    	postal_code: row.postalcode,
+    	city: row.cityormunicipality,
+    	country: row.country_name,
+    	ctep_id: row.ctep_id,
+    	curator_comment: row.comments,
+ 		po_id: row.id,
+ 		status: row.status,
+ 		status_date: row.statusdate,
+ 		state_or_province: row.stateorprovince,
+ 		email: row.email,
+ 	 	fax: row.faxnumber,
+ 	 	phone: row.phone,
+ 	 	tty: row.tty,
+ 	 	internal_id: row.id,
+ 	 	FAMILY: row.fam_name,
+ 	 	ORG_TO_FAMILY_RELATIONSHIP: row.functionaltype,
+ 	 	CHANGE_REQUEST_COUNT: row.crcount
+	)
+}
+
+destinationConnection.execute("UPDATE STG_DW_ORGANIZATION set CHANGE_REQUEST_COUNT = 0 where CHANGE_REQUEST_COUNT is null")
+
+
+
---
+++ pa/data-warehouse/sql/organization.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS STG_DW_ORGANIZATION;
+
+CREATE TABLE STG_DW_ORGANIZATION ( 
+	ADDRESS_LINE_1 character varying (254),
+	ADDRESS_LINE_2 character varying (254),
+	POSTAL_CODE character varying (30),
+	CITY character varying (50),
+	STATE_OR_PROVINCE character varying (255),
+	COUNTRY character varying (50),
+	CTEP_ID character varying (50),
+	CURATOR_COMMENT text,
+	NAME character varying (255),
+	PO_ID integer,
+	STATUS character varying (20),
+	STATUS_DATE date,
+	SUFFIX character varying (10),
+	EMAIL character varying (256),
+	FAX character varying (256),
+	PHONE character varying (256),
+	TTY character varying (50),
+	INTERNAL_ID integer,
+	CHANGE_REQUEST_COUNT integer,
+	FAMILY character varying (400),
+	ORG_TO_FAMILY_RELATIONSHIP character varying (30)
+	)
+;
