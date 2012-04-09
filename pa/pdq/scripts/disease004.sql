facc93b7914c4d6096259b0eedcf668240c7007d
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-04-09 12:53:11 +0000

    Remove unneeded scripts.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@13980 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pdq/scripts/disease004.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO pdq_disease (identifier,disease_code,nt_term_identifier,preferred_name,menu_display_name,status_code,status_date_range_low,date_last_created) 
-VALUES (1776,'CDR0000587622',null,'systemic scleroderma','systemic scleroderma','ACTIVE',now(),'09/16/2009');
-
-UPDATE PDQ_DISEASE SET STATUS_CODE = 'ACTIVE' WHERE IDENTIFIER = 1755 AND STATUS_CODE = 'INACTIVE';
-
-
