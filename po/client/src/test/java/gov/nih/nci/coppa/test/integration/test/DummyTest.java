7236c70f6932e710af9e3f7c9ef475a0bae95a3a
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-04-27 16:56:04 +0000

    Debug Selenium.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14096 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/DummyTest.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.coppa.test.integration.test;
+
+public class DummyTest {
+
+    public void test01() throws Exception {
+        System.out.println("running dummy test");
+    }
+
+}
