b3632a9ead4b7d6256ff58d823b30d481285c520
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-04-27 18:34:28 +0000

    Debug
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14100 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/DummyTest.java
+++ po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/DummyTest.java
@@ -1,7 +1,10 @@
 package gov.nih.nci.coppa.test.integration.test;
 
+import org.junit.Test;
+
 public class DummyTest {
 
+    @Test
     public void test01() throws Exception {
         System.out.println("running dummy test");
     }
