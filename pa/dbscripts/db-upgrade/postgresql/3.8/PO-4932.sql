134c1c3d0c59aa80a91d93dae33dee5ab51569aa
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-04-30 13:12:52 +0000

    PO-4932 CLONE - NCI-2011-00962: Onhold trial: fixed.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14104 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/db-upgrade/postgresql/3.8/PO-4932.sql
+++ pa/dbscripts/db-upgrade/postgresql/3.8/PO-4932.sql
@@ -5,6 +5,6 @@ update study_onhold set offhold_date=current_date where study_onhold.identifier 
 
 select so.identifier from study_onhold so inner join study_protocol sp on so.study_protocol_identifier=sp.identifier inner join document_workflow_status dws on sp.identifier=dws.study_protocol_identifier
    where so.offhold_date is null and (select count(identifier) from study_onhold where study_onhold.study_protocol_identifier=sp.identifier and study_onhold.offhold_date is null)>0
-   and dws.status_code!='ON_HOLD'
+   and dws.status_code!='ON_HOLD' and dws.identifier=(select max(identifier) from document_workflow_status where document_workflow_status.study_protocol_identifier=sp.identifier)
 
 );
