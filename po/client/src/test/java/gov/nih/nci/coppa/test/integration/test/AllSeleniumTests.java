9c3bd222a8c9292ac9c4fec7b7b972a1f116185c
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-05-21 17:30:42 +0000

    Debug
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14237 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/AllSeleniumTests.java
+++ po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/AllSeleniumTests.java
@@ -87,8 +87,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses(value = { DummyTest.class,
-        LoginTest.class })
+@SuiteClasses(value = { DummyTest.class })
+// LoginTest.class })
 // ContactFormattingTest.class,
 // CreateOrganizationTest.class,
 // CreatePersonTest.class,
