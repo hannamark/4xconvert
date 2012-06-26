472c706e84d6813cd1d377bbb67ac8341d46238f
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-06-26 15:44:08 +0000

    PO-5185 Data Warehouse - DW_STUDY_COLLABORATOR contains duplicate records for some trials
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14378 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/collaborator.groovy
+++ pa/data-warehouse/groovy/collaborator.groovy
@@ -7,11 +7,13 @@ def sql = """
     	org.name,
     	nci_id.extension
     from study_site ss
+        inner join study_protocol as sp on ss.study_protocol_identifier = sp.identifier
         inner join study_otheridentifiers as nci_id on nci_id.study_protocol_id = ss.study_protocol_identifier
         	and nci_id.root = '2.16.840.1.113883.3.26.4.3'
         left outer join Research_organization as ro on RO.identifier = ss.research_organization_identifier
         left outer join organization as org on org.identifier = RO.organization_identifier
     where ss.functional_code in ('AGENT_SOURCE', 'FUNDING_SOURCE', 'LABORATORY')
+      and sp.status_code = 'ACTIVE'
     """
 
 def sourceConnection = Sql.newInstance(properties['datawarehouse.pa.source.jdbc.url'], properties['datawarehouse.pa.source.db.username'],
