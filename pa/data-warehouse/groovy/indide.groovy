8fbfabf8dfbffcd1901f7123a309f073fae82f2f
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-09-10 14:39:46 +0000

    PO-5463 Data Warehouse - Please include field EXEMPT_INDICATOR in DW_STUDY_IND_IDE
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@14762 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/indide.groovy
+++ pa/data-warehouse/groovy/indide.groovy
@@ -7,6 +7,7 @@ def sql = """
             indide.expanded_access_status_code, indide.grantor_code, indide.holder_type_code, indide.indlde_number,
             indide.indlde_type_code, indide.identifier, indide.nci_div_prog_holder_code, nci_id.extension,
             indide.nih_inst_holder_code,
+            indide.exempt_indicator,
             CASE WHEN NULLIF(ru_creator.first_name, '') is not null THEN ru_creator.first_name || ' ' || ru_creator.last_name
                  WHEN NULLIF(split_part(creator.login_name, 'CN=', 2), '') is null THEN creator.login_name
                  ELSE split_part(creator.login_name, 'CN=', 2)
@@ -29,6 +30,7 @@ def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jd
 def indides = destinationConnection.dataSet("STG_DW_STUDY_IND_IDE")
 sourceConnection.eachRow(sql) { row ->
     indides.add(date_last_created: row.date_last_created, date_last_updated: row.date_last_updated, expanded_access_indicator: row.access_indicator,
+            exempt_indicator: row.exempt_indicator,
             expanded_access_status_code: row.expanded_access_status_code, grantor_code: row.grantor_code, holder_type_code: row.holder_type_code,
             ind_ide_number: row.indlde_number, ind_ide_type_code: row.indlde_type_code, internal_system_id: row.identifier,
             nci_div_prog_holder_code: row.nci_div_prog_holder_code, nci_id: row.extension, nih_instholder_code: row.nih_inst_holder_code,
