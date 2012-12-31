03098becde6531099856682e2f2b6fd6b91ad7a6
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2012-12-31 16:10:00 +0000

    PO-5715 Accruals - Support ICD-0-3 disease code terminology
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@15096 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/db-upgrade/postgresql/3.9/PO-5715.sql
+++ pa/dbscripts/db-upgrade/postgresql/3.9/PO-5715.sql
@@ -17,14 +17,14 @@ INSERT INTO accrual_disease (identifier, code_system, disease_code, preferred_na
   SELECT sdc_disease.identifier, 'SDC' AS code_system, sdc_disease.disease_code, sdc_disease.ctep_term AS preferred_name, sdc_disease.ctep_short_name AS display_name
     FROM sdc_disease
   UNION 
-  SELECT icd9_disease.identifier, 'ICD9' AS code_system, icd9_disease.disease_code, ((icd9_disease.name::text || ' ('::text) || icd9_disease.disease_code::text) || ')'::text AS preferred_name, ((icd9_disease.name::text || ' ('::text) || icd9_disease.disease_code::text) || ')'::text AS display_name
+  SELECT icd9_disease.identifier + 10000, 'ICD9' AS code_system, icd9_disease.disease_code, ((icd9_disease.name::text || ' ('::text) || icd9_disease.disease_code::text) || ')'::text AS preferred_name, ((icd9_disease.name::text || ' ('::text) || icd9_disease.disease_code::text) || ')'::text AS display_name
     FROM icd9_disease;
 
 ALTER TABLE study_subject DROP CONSTRAINT fk_study_subject_icd9disease;
 ALTER TABLE study_subject DROP CONSTRAINT fk_study_subject_disease;
 
 UPDATE study_subject
-  SET disease_identifier = icd9disease_identifier
+  SET disease_identifier = icd9disease_identifier + 10000
   WHERE disease_identifier IS NULL;
 
 ALTER TABLE study_subject ADD CONSTRAINT fk_study_subject_disease 
