7799be1f6ddad76399a3cd8ce290df99f18d61a2
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2013-03-04 13:21:05 +0000

    PO-2660 Support caMOD Use Case - test coverage
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@15282 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/test/java/gov/nih/nci/registry/rest/StudyProtocolRsrcTest.java
@@ -0,0 +1,71 @@
+package gov.nih.nci.registry.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import gov.nih.nci.pa.dto.StudyProtocolQueryDTO;
+import gov.nih.nci.pa.enums.DocumentWorkflowStatusCode;
+import gov.nih.nci.pa.enums.StudyStatusCode;
+import gov.nih.nci.pa.service.util.ProtocolQueryServiceLocal;
+import gov.nih.nci.pa.util.PaRegistry;
+import gov.nih.nci.pa.util.ServiceLocator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyProtocolRsrcTest {
+    ProtocolQueryServiceLocal pcs;
+    StudyProtocolQueryDTO resultDto;
+
+    /**
+     * Set up services.
+     */
+    @Before
+    public void setUpServices() throws Exception {
+        resultDto = new StudyProtocolQueryDTO();
+        resultDto.setNciIdentifier("nciIdentifier");
+        resultDto.setOfficialTitle("officialTitls");
+        resultDto.setPhaseName("phaseName");
+        resultDto.setLeadOrganizationName("leadOrganizationName");
+        resultDto.setPiFullName("piFullName");
+        resultDto.setStudyStatusCode(StudyStatusCode.TEMPORARILY_CLOSED_TO_ACCRUAL_AND_INTERVENTION);
+        resultDto.setNctIdentifier("nctIdentifier");
+        resultDto.setDocumentWorkflowStatusCode(DocumentWorkflowStatusCode.ABSTRACTION_VERIFIED_RESPONSE);
+        List<StudyProtocolQueryDTO> result = new ArrayList<StudyProtocolQueryDTO>();
+        result.add(resultDto);
+
+        pcs = mock(ProtocolQueryServiceLocal.class);
+        when(pcs.getStudyProtocolByAgentNsc(anyString())).thenReturn(result);
+        ServiceLocator sl = mock(ServiceLocator.class);
+        when(sl.getProtocolQueryService()).thenReturn(pcs);
+        PaRegistry.getInstance().setServiceLocator(sl);
+    }
+
+    @Test
+    public void getStudiesXMLFound() throws Exception {
+        StudyProtocolsRsrc rsrc = new StudyProtocolsRsrc();
+        Response resp = rsrc.getStudiesXML("123456");
+        assertEquals(200, resp.getStatus());
+    }
+
+    @Test
+    public void getStudiesXMLNullTest() {
+        StudyProtocolsRsrc rsrc = new StudyProtocolsRsrc();
+        Response resp = rsrc.getStudiesXML(null);
+        assertEquals(400, resp.getStatus());
+    }
+
+    @Test
+    public void getStudiesXMLInternalServerErroTest() throws Exception {
+        when(pcs.getStudyProtocolByAgentNsc(anyString())).thenThrow(new RuntimeException());
+        StudyProtocolsRsrc rsrc = new StudyProtocolsRsrc();
+        Response resp = rsrc.getStudiesXML("123456");
+        assertEquals(500, resp.getStatus());
+    }
+}
