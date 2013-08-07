60c384828f4ccd953e4699981a700e9f00b900a2
guthikondak <guthikondak@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2013-08-07 15:26:52 +0000

    3.9.1 biomarker changes.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@15592 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/sql/biomarker.sql
+++ pa/data-warehouse/sql/biomarker.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS STG_DW_STUDY_BIOMARKER;
 CREATE TABLE STG_DW_STUDY_BIOMARKER (
     ASSAY_PURPOSE character varying(200),
     ASSAY_PURPOSE_DESCRIPTION character varying(200),
-    ASSAY_TYPE_CODE character varying(200),
+    ASSAY_TYPE_CODE character varying(400),
     ASSAY_TYPE_DESCRIPTION character varying(200),
     ASSAY_USE character varying(200),
     INTERNAL_SYSTEM_ID INTEGER not null,
@@ -11,7 +11,7 @@ CREATE TABLE STG_DW_STUDY_BIOMARKER (
     NCI_ID character varying(255),
     STATUS_CODE character varying(200),
     TISSUE_COLLECTION_METHOD_CODE character varying(200),
-    TISSUE_SPECIMEN_TYPE_CODE character varying(200),
+    TISSUE_SPECIMEN_TYPE_CODE character varying(400),
     HUGO_BIOMARKER_CODE character varying(200),
     EVALUATION_TYPE_CODE character varying(400),
     EVALUATION_TYPE_OTHER_TEXT character varying(200),
