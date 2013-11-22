b27cea3cd3b4dd5f0b9746c18ef4fb06295d6f6a
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2013-11-22 18:11:23 +0000

    PO-6671 PA/DW- Report Request for CTRO-All CTRP Trials counts by different category report.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@15892 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/data-warehouse/sql/trial_type.sql
@@ -0,0 +1,59 @@
+﻿DROP TABLE IF EXISTS dw_study_type;
+DROP TABLE IF EXISTS dw_study_types;
+CREATE TABLE dw_study_type AS
+  SELECT 1 AS id, 'NCI Sponsored'::character varying(16) AS group, 'CTEP'::character varying(16) AS type, nci_id
+    FROM dw_study
+    WHERE sponsor = 'National Cancer Institute'
+      AND ctep_id IS NOT NULL
+      AND dcp_id IS NULL
+  UNION
+  SELECT 2, 'NCI Sponsored', 'DCP', nci_id
+    FROM dw_study
+    WHERE sponsor = 'National Cancer Institute'
+      AND dcp_id IS NOT NULL
+  UNION
+  SELECT 3, 'NCI Managed', 'CTEP', nci_id
+    FROM dw_study
+    WHERE ctep_id IS NOT NULL
+      AND dcp_id IS NULL
+  UNION
+  SELECT 4, 'NCI Managed', 'DCP', nci_id
+    FROM dw_study
+    WHERE dcp_id IS NOT NULL
+  UNION
+  SELECT 5, 'CCR', NULL, nci_id
+    FROM dw_study
+    WHERE lead_org = 'NCI - Center for Cancer Research'
+      AND dcp_id IS NULL
+      AND ctep_id IS NULL
+  UNION
+  SELECT 6, 'Center Trials', 'Center Registered Complete', nci_id
+    FROM dw_study
+    WHERE  submitter_name NOT IN ('CTRO Staff National Cancer Institute','DCP PIO', 'Sulekha Avasthi')
+       AND category = 'Complete'
+  UNION
+  SELECT 7, 'Center Trials', 'Center Registered Abbreviated', nci_id
+    FROM dw_study
+    WHERE  submitter_name NOT IN ('CTRO Staff National Cancer Institute','DCP PIO', 'Sulekha Avasthi')
+       AND category = 'Abbreviated'
+  UNION
+  SELECT 8, 'Import from Clinicaltrials.gov', NULL, nci_id
+    FROM dw_study
+    WHERE SUBSTRING(created_by FROM 1 FOR 13) = 'CT.Gov Import'
+  UNION
+  SELECT 9, 'Consortia/Other', NULL, nci_id
+    FROM dw_study
+    WHERE consortia_trial_category IN ('EXTERNALLY_PEER_REVIEWED', 'NATIONAL')
+  UNION
+  SELECT 10, NULL, NULL, nci_id FROM dw_study;
+
+ALTER TABLE dw_study_type
+  ADD CONSTRAINT dw_study_type_pk PRIMARY KEY (id, nci_id);
+
+CREATE INDEX dw_study_type_nci_id_idx
+  ON dw_study_type USING btree  (nci_id);
+
+CREATE TABLE dw_study_types AS SELECT DISTINCT id, "group", type FROM dw_study_type;
+
+ALTER TABLE dw_study_types
+  ADD CONSTRAINT dw_study_types_pk PRIMARY KEY (id);
