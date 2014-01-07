157f106064b0d632e385669488a1804dadbaec34
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-01-07 13:11:30 +0000

    PO-6850 Add summary 4 funding category into dw_study.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@15941 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/summary_4_funding.groovy
+++ pa/data-warehouse/groovy/summary_4_funding.groovy
@@ -5,7 +5,7 @@ def sql = """SELECT sr.identifier, soi.extension, sr.type_code, org.name, CAST(o
              FROM study_resourcing sr
              JOIN study_protocol sp ON (sp.identifier = sr.study_protocol_identifier)
              JOIN study_otheridentifiers soi ON (soi.study_protocol_id = sp.identifier)
-             JOIN organization org ON (CAST(sr.organization_identifier AS bigint) = org.identifier)
+             LEFT JOIN organization org ON (CAST(sr.organization_identifier AS bigint) = org.identifier)
              WHERE sr.summ_4_rept_indicator = TRUE
                AND sp.status_code = 'ACTIVE'
                AND soi.root = '2.16.840.1.113883.3.26.4.3'
@@ -18,12 +18,14 @@ def destinationConnection = Sql.newInstance(properties['datawarehouse.pa.dest.jd
 def sponsors = destinationConnection.dataSet("stg_dw_summary_4_funding")
 
 sourceConnection.eachRow(sql) { row ->
-    sponsors.add(
+    if (row.name != null) {
+      sponsors.add(
         internal_system_id: row.identifier,
         nci_id: row.extension,
         sponsor_id: row.assigned_identifier,
         sponsor: row.name
-    );
+      );
+	}
 	destinationConnection.executeUpdate("""UPDATE stg_dw_study study
                                            SET summary_4_funding_category = ?
                                            WHERE internal_system_id = ?
