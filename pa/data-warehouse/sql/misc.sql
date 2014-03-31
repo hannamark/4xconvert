28af93ff4ab134099ea657dd1a7397846549bba0
reinharh <reinharh@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-03-31 11:32:57 +0000

    Adjust order so that db connection drop at the wrong time will not cause disease tree to be missing.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@16304 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/sql/misc.sql
+++ pa/data-warehouse/sql/misc.sql
@@ -1,32 +1,12 @@
---Listing of all orgs used in study/participating sites
-CREATE OR REPLACE VIEW dw_summary_4_report as
-select org, 
-        (select count(responsible_party_name) from dw_study where unique_org.org = responsible_party_name) as responsible, 
-        (select count(lead_org) from dw_study where unique_org.org = lead_org) as lead, 
-        (select count(sponsor) from dw_study where unique_org.org = sponsor) as sponsor, 
-        (select count(submitter_organization) from dw_study where unique_org.org = submitter_organization) as submitter,
-        (select count(sponsor) from dw_summary_4_funding where unique_org.org = sponsor) as sum_4_sponsor, 
-        (select count(org_name) from dw_study_participating_site where unique_org.org = org_name) as participant 
-from 
-( select distinct org from
-        (select distinct responsible_party_name as org from dw_study
-                union
-        select distinct lead_org as org from dw_study
-                union
-        select distinct sponsor as org from dw_study
-                union
-        select distinct sponsor as org from dw_summary_4_funding
-                union
-        select distinct org_name as org from dw_study_participating_site
-        ) as orgs order by org asc) as unique_org;
-        
 --Put here instead of user.groovy because organization table not yet populated when user.groovy run
-UPDATE DW_USER USR 
+UPDATE STG_DW_USER USR 
   SET AFFILIATED_ORGANIZATION = (SELECT DISTINCT NAME 
-                                 FROM DW_ORGANIZATION ORG 
+                                 FROM STG_DW_ORGANIZATION ORG 
                                  WHERE ORG.INTERNAL_ID = USR.AFFILIATED_ORGANIZATION_ID);
 
 --Put here instead of disease.groovy because of java heap issue
+CREATE INDEX sdtmp_index ON stg_dw_study_disease(nci_id);
+DROP TABLE IF EXISTS stg_dw_study_disease_tree;
 CREATE TABLE stg_dw_study_disease_tree AS
     SELECT DISTINCT
            CAST('NO' AS character varying(3)) AS ct_gov_xml_indicator,
@@ -43,11 +23,12 @@ CREATE TABLE stg_dw_study_disease_tree AS
            dp.nt_term_identifier AS nci_thesaurus_concept_id, 
            sd.user_last_created, 
            sd.user_last_updated
-      FROM dw_study_disease sd
+      FROM stg_dw_study_disease sd
       JOIN stg_dw_disease_parents dp ON (sd.internal_system_id2 = dp.disease_identifier);
 
 ALTER TABLE stg_dw_study_disease_tree ADD PRIMARY KEY (internal_system_id, internal_system_id2);
 
+DROP TABLE IF EXISTS stg_dw_study_disease_tree_keep;
 CREATE TABLE stg_dw_study_disease_tree_keep AS
     SELECT nci_id, MIN(internal_system_id) AS internal_system_id, internal_system_id2
     FROM stg_dw_study_disease_tree
@@ -55,15 +36,16 @@ CREATE TABLE stg_dw_study_disease_tree_keep AS
 
 ALTER TABLE stg_dw_study_disease_tree_keep ADD PRIMARY KEY (internal_system_id, internal_system_id2);
 
-INSERT INTO dw_study_disease 
+INSERT INTO stg_dw_study_disease 
     SELECT * 
     FROM stg_dw_study_disease_tree 
     WHERE (internal_system_id, internal_system_id2) 
           IN (SELECT internal_system_id, internal_system_id2 FROM stg_dw_study_disease_tree_keep);
 
-DELETE FROM dw_study_disease WHERE INCLUSION_INDICATOR = 'TREE'
-  AND (nci_id, internal_system_id2) IN (SELECT nci_id, internal_system_id2 FROM dw_study_disease WHERE INCLUSION_INDICATOR = 'TRIAL');
+DELETE FROM stg_dw_study_disease WHERE INCLUSION_INDICATOR = 'TREE'
+  AND (nci_id, internal_system_id2) IN (SELECT nci_id, internal_system_id2 FROM stg_dw_study_disease WHERE INCLUSION_INDICATOR = 'TRIAL');
 
 DROP TABLE stg_dw_study_disease_tree_keep;
 DROP TABLE stg_dw_study_disease_tree;
 DROP TABLE stg_dw_disease_parents;
+DROP INDEX IF EXISTS sdtmp_index;
