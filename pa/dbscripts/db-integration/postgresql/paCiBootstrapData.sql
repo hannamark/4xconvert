ecab966d5db82681f9421d5d384db16b2d9bb820
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-05-02 14:50:48 +0000

    Corrected paCiBootstrapData.sql.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@16518 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/db-integration/postgresql/paCiBootstrapData.sql
+++ pa/dbscripts/db-integration/postgresql/paCiBootstrapData.sql
@@ -2,17 +2,17 @@
 insert into csm_user(login_name, first_name, last_name) values ('/O=caBIG/OU=caGrid/OU=Training/OU=Dorian/CN=abstractor-ci', 'Abstractor', 'Selenium');
 insert into registry_user(identifier, first_name, last_name, address_line, city, state, postal_code, country, phone , affiliate_org, csm_user_id, affiliated_org_id, affiliated_org_user_type, email_address)
     values(nextval('hibernate_sequence'), 'Abstractor', 'User', '2115 E. Jefferson St.', 'North Bethesda', 'Maryland', '20852', 'USA', '123-456-7890', 
-        'ClinicalTrials.gov', (select user_id from csm_user where login_name like '%CN=abstractor-ci'), 3, 'ADMIN', 'abstractor-ci@example.com');
+        'National Cancer Institute Division of Cancer Prevention', (select user_id from csm_user where login_name like '%CN=abstractor-ci'), 3, 'ADMIN', 'abstractor-ci@example.com');
         
 insert into csm_user(login_name, first_name, last_name) values ('/O=caBIG/OU=caGrid/OU=Training/OU=Dorian/CN=submitter-ci', 'Submitter', 'CI');
 insert into registry_user(identifier, first_name, last_name, address_line, city, state, postal_code, country, phone , affiliate_org, csm_user_id, affiliated_org_id, affiliated_org_user_type, email_address)
     values(nextval('hibernate_sequence'), 'Submitter', 'CI', '2115 E. Jefferson St.', 'North Bethesda', 'Maryland', '20852', 'USA', '123-456-7890', 
-        'ClinicalTrials.gov', (select user_id from csm_user where login_name like '%CN=submitter-ci'), 3, 'MEMBER', 'submitter-ci@example.com');
+        'National Cancer Institute Division of Cancer Prevention', (select user_id from csm_user where login_name like '%CN=submitter-ci'), 3, 'MEMBER', 'submitter-ci@example.com');
         
 insert into csm_user(login_name, first_name, last_name) values ('/O=caBIG/OU=caGrid/OU=Training/OU=Dorian/CN=ctrpsubstractor', 'ctrpsubstractor', 'CI');
 insert into registry_user(identifier, first_name, last_name, address_line, city, state, postal_code, country, phone , affiliate_org, csm_user_id, affiliated_org_id, affiliated_org_user_type, email_address)
     values(nextval('hibernate_sequence'), 'ctrpsubstractor', 'CI', '2115 E. Jefferson St.', 'North Bethesda', 'Maryland', '20852', 'USA', '123-456-7890', 
-        'ClinicalTrials.gov', (select user_id from csm_user where login_name like '%CN=ctrpsubstractor'), 3, 'MEMBER', 'ctrpsubstractor-ci@example.com');        
+        'National Cancer Institute Division of Cancer Prevention', (select user_id from csm_user where login_name like '%CN=ctrpsubstractor'), 3, 'MEMBER', 'ctrpsubstractor-ci@example.com');        
         
 update csm_remote_group set grid_grouper_url='https://grouper.training.cagrid.org:8443/wsrf/services/cagrid/GridGrouper' where grid_grouper_url='@gridgrouper.url@';
 update csm_remote_group set grid_grouper_group_name='COPPA:PA:ScientificAbstractor' where grid_grouper_group_name='@gridgrouper.abstractor.group.stem@:ScientificAbstractor';
