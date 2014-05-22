7fde0f252f5499ec43ab141da3b9b9be0de46e32
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-05-22 16:41:56 +0000

    PO-7524 Hitting PO Grid services on the new tech stack produces EJBCLIENT000025 error intermittently without apparent cause: fixed.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@16670 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ po-grid-3.0/src/gov/nih/nci/coppa/po/grid/remote/InterceptingFilter.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package gov.nih.nci.coppa.po.grid.remote;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * @author Denis G. Krylov
+ * 
+ */
+// CHECKSTYLE:OFF
+public final class InterceptingFilter implements Filter {
+
+    private static final Logger LOG = LogManager
+            .getLogger(InterceptingFilter.class);
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+     * javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    public void doFilter(ServletRequest req, ServletResponse resp,
+            FilterChain fc) throws IOException, ServletException {
+        try {
+            fc.doFilter(req, resp);
+        } finally {
+            List<InitialContext> list = JNDIServiceLocator.CTX_HOLDER
+                    .get();
+            JNDIServiceLocator.CTX_HOLDER.remove();
+            if (list != null) {
+                for (InitialContext ic : list) {
+                    try {
+                        ic.close();
+                    } catch (Exception e) {
+                        LOG.warn("Unable to close InitialContext due to "
+                                + e.getMessage());
+                    }
+                }
+                list.clear();
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    public void init(FilterConfig arg0) throws ServletException {
+
+    }
+
+}
