6b53049442226f21b28c2d3801975415a3f1fbe4
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-05-28 20:44:48 +0000

    Fixed PO-5319.sql
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@16727 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/db-upgrade/postgresql/3.8.1/PO-5319.sql
+++ pa/dbscripts/db-upgrade/postgresql/3.8.1/PO-5319.sql
@@ -5,7 +5,7 @@ NCI Trial Identifier: ${nciTrialIdentifier}
 
 Dear ${SubmitterName},
 
-Thank you for submitting your accruals to the National Cancer Institute (NCI) Clinical Trials Reporting Program (CTRP). Change Code was set to \'2\' indicating that the file did not need to be processed. The batch file has been stored for future reference. The following file was received and saved successfully:
+Thank you for submitting your accruals to the National Cancer Institute (NCI) Clinical Trials Reporting Program (CTRP). Change Code was set to ''2'' indicating that the file did not need to be processed. The batch file has been stored for future reference. The following file was received and saved successfully:
 
 File Name: ${fileName}
 
