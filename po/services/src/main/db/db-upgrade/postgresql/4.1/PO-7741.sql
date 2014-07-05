b8dd1b6e1701253525c06790364a6c96d1c44bbe
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-07-05 17:47:37 +0000

    trying to fix PO-7741.sql in dev.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@16920 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/services/src/main/db/db-upgrade/postgresql/4.1/PO-7741.sql
+++ po/services/src/main/db/db-upgrade/postgresql/4.1/PO-7741.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION populate_createdby2() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION populate_createdby3() RETURNS VOID AS $$
     DECLARE    
         created_by_user_id INTEGER;             
         org_id INTEGER; 
@@ -14,8 +14,8 @@ CREATE OR REPLACE FUNCTION populate_createdby2() RETURNS VOID AS $$
                    
     BEGIN               
      
-	 -- Step 1: Create a temp table 'auditlogrecord_temp'
-     CREATE TABLE auditlogrecord_temp
+	 -- Step 1: Create a temp table 'auditlogrecord_temp2'
+     CREATE TABLE auditlogrecord_temp2
         (
         id bigint NOT NULL,
         username character varying(100) NOT NULL,
@@ -26,16 +26,16 @@ CREATE OR REPLACE FUNCTION populate_createdby2() RETURNS VOID AS $$
         type character varying(255) NOT NULL
         );
 
-    CREATE INDEX auditlog_temp_index ON auditlogrecord_temp USING btree (entityid);
+    CREATE INDEX auditlog_temp_index ON auditlogrecord_temp2 USING btree (entityid);
 
-    -- Step 2: Load data into 'auditlogrecord_temp' from 'auditlogrecord' table
-    insert into auditlogrecord_temp
-    select * from auditlogrecord where type ='INSERT' 
-    and entityname in ('Organization','HealthCareFacility','ResearchOrganization','OversightCommittee','IdentifiedOrganization','Person','ClinicalResearchStaff','HealthCareProvider','OrganizationalContact','IdentifiedPerson');
+    -- Step 2: Load data into 'auditlogrecord_temp2' from 'auditlogrecord' table
+    insert into auditlogrecord_temp2 (id,type,username,entityname,entityid,createddate,transactionid)
+	select id,type,username,entityname,entityid,createddate,transactionid from auditlogrecord where type ='INSERT' 
+	and entityname in ('Organization','HealthCareFacility','ResearchOrganization','OversightCommittee','IdentifiedOrganization','Person','ClinicalResearchStaff','HealthCareProvider','OrganizationalContact','IdentifiedPerson');
 
     if exists(select * from information_schema.tables where table_name = 'auditlogrecord_022713') then
-        insert into auditlogrecord_temp
-        select * from auditlogrecord_022713 where type ='INSERT' 
+        insert into auditlogrecord_temp2 (id,type,username,entityname,entityid,createddate,transactionid)
+        select id,type,username,entityname,entityid,createddate,transactionid from auditlogrecord_022713 where type ='INSERT' 
         and entityname in ('Organization','HealthCareFacility','ResearchOrganization','OversightCommittee','IdentifiedOrganization','Person','ClinicalResearchStaff','HealthCareProvider','OrganizationalContact','IdentifiedPerson');
     end if;
 
@@ -44,72 +44,72 @@ CREATE OR REPLACE FUNCTION populate_createdby2() RETURNS VOID AS $$
         
         -- update 'Organization'
         FOR org_id IN SELECT id FROM organization WHERE created_by_id is null LOOP              
-            SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = org_id );
+            SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = org_id );
             UPDATE organization SET created_by_id = created_by_user_id WHERE id = org_id;   
         END LOOP; 
 
         -- update 'HealthCareFacility'
         FOR hcf_id IN SELECT id FROM healthcarefacility WHERE created_by_id is null LOOP                
-            SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = hcf_id );
+            SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = hcf_id );
             UPDATE healthcarefacility SET created_by_id = created_by_user_id WHERE id = hcf_id; 
         END LOOP;   
 
         -- update 'ResearchOrganization'
         FOR ro_id IN SELECT id FROM researchorganization WHERE created_by_id is null LOOP               
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = ro_id );
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = ro_id );
            UPDATE researchorganization SET created_by_id = created_by_user_id WHERE id = ro_id;    
         END LOOP;   
 
         -- update 'OversightCommittee'
         FOR over_comm_id IN SELECT id FROM oversightcommittee WHERE created_by_id is null LOOP              
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = over_comm_id );
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = over_comm_id );
            UPDATE oversightcommittee SET created_by_id = created_by_user_id WHERE id = over_comm_id;   
         END LOOP;
 
         -- update 'IdentifiedOrganization'
         FOR io_id IN SELECT id FROM identifiedorganization WHERE created_by_id is null LOOP             
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = io_id );
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = io_id );
            UPDATE identifiedorganization SET created_by_id = created_by_user_id WHERE id = io_id;  
         END LOOP; 
 
         -- update 'Person'
         FOR per_id IN SELECT id FROM person WHERE created_by_id is null LOOP                
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = per_id);
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = per_id);
            UPDATE person SET created_by_id = created_by_user_id WHERE id = per_id; 
         END LOOP;
 
         -- update 'ClinicalResearchStaff'
         FOR crs_id IN SELECT id FROM clinicalresearchstaff WHERE created_by_id is null LOOP             
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = crs_id );
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = crs_id );
            UPDATE clinicalresearchstaff SET created_by_id = created_by_user_id WHERE id = crs_id;  
         END LOOP;
 
         -- update 'HealthCareProvider'
         FOR hcp_id IN SELECT id FROM healthcareprovider WHERE created_by_id is null LOOP                
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = hcp_id);
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = hcp_id);
            UPDATE healthcareprovider SET created_by_id = created_by_user_id WHERE id = hcp_id; 
         END LOOP;
 
         -- update 'OrganizationalContact'
         FOR org_cont_id IN SELECT id FROM organizationalcontact WHERE created_by_id is null LOOP                
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = org_cont_id);
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = org_cont_id);
            UPDATE organizationalcontact SET created_by_id = created_by_user_id WHERE id = org_cont_id; 
         END LOOP;
 
         -- update 'IdentifiedPerson'
         FOR ip_id IN SELECT id FROM identifiedperson WHERE created_by_id is null LOOP               
-           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp WHERE entityid = ip_id);
+           SELECT INTO created_by_user_id user_id FROM CSM_USER WHERE LOWER(LOGIN_NAME) = (SELECT LOWER(username) FROM auditlogrecord_temp2 WHERE entityid = ip_id);
            UPDATE identifiedperson SET created_by_id = created_by_user_id WHERE id = ip_id;    
         END LOOP;
 
         
     -- Step4: Drop the temp table
-        drop table auditlogrecord_temp;
+        drop table auditlogrecord_temp2;
            
     END;
 $$ LANGUAGE plpgsql;
 
-select populate_createdby2();
+select populate_createdby3();
 
 -- drop this functions as this is meant for temporary used only
-DROP FUNCTION populate_createdby2();
+DROP FUNCTION populate_createdby3();
