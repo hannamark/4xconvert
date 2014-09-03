d7ced581196ba4dcf4804eb0b4bb084783df5209
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-09-03 14:38:42 +0000

    Updated ParticipatingSiteMgmtServiceTest.java integration test.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@17161 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-webservices/test-integration/java/gov/nih/nci/pa/webservices/test/integration/ParticipatingSiteMgmtServiceTest.java
+++ pa/pa-webservices/test-integration/java/gov/nih/nci/pa/webservices/test/integration/ParticipatingSiteMgmtServiceTest.java
@@ -3,7 +3,6 @@
  */
 package gov.nih.nci.pa.webservices.test.integration;
 
-import gov.nih.nci.pa.webservices.types.CompleteTrialUpdate;
 import gov.nih.nci.pa.webservices.types.ObjectFactory;
 import gov.nih.nci.pa.webservices.types.ParticipatingSite;
 import gov.nih.nci.pa.webservices.types.TrialRegistrationConfirmation;
@@ -20,6 +19,8 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayHandler;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
@@ -54,10 +55,62 @@ public final class ParticipatingSiteMgmtServiceTest extends
 
     @Test
     public void testAddSite() throws Exception {
+        final String xmlFile = "/integration_ps_add.xml";
+        addSite(xmlFile);
+    }
+
+    @Test
+    public void testAddSiteWithSubInvestigator() throws Exception {
+        final String xmlFile = "/integration_ps_sub_investigator_add.xml";
+        addSite(xmlFile);
+    }
+
+    @Test
+    public void testAddSiteWithNoPrimaryContact() throws Exception {
+        final String xmlFile = "/integration_ps_without_primary_contact_add.xml";
+        addSite(xmlFile);
+    }
+
+    @Test
+    public void testAddSiteSchemaViolation() throws Exception {
+        final String xmlFile = "/integration_ps_schema_violation_add.xml";
+        
         TrialRegistrationConfirmation rConf = register("/integration_register_complete_minimal_dataset.xml");
         makeAbbreviated(rConf);
+        ParticipatingSite upd = readParticipatingSiteFromFile(xmlFile);
+        HttpResponse response = addSite("pa", rConf.getPaTrialID() + "", upd);
+        assertEquals(400, getReponseCode(response));
+    }
+    
+    @Test
+    public void testAddSiteBizValidation() throws Exception {
+        final String xmlFile = "/integration_ps_biz_validation_add.xml";
+        
+        TrialRegistrationConfirmation rConf = register("/integration_register_complete_minimal_dataset.xml");
+        makeAbbreviated(rConf);
+        ParticipatingSite upd = readParticipatingSiteFromFile(xmlFile);
+        HttpResponse response = addSite("pa", rConf.getPaTrialID() + "", upd);
+        assertEquals(500, getReponseCode(response));
+        assertTrue(EntityUtils.toString(response.getEntity(),
+                "utf-8").contains("Date Opened for Accrual must be null for In Review"));
+    }
 
-        ParticipatingSite upd = readParticipatingSiteFromFile("/integration_ps_add.xml");
+    /**
+     * @param xmlFile
+     * @throws JAXBException
+     * @throws SAXException
+     * @throws SQLException
+     * @throws ClientProtocolException
+     * @throws ParseException
+     * @throws IOException
+     * @throws NumberFormatException
+     */
+    private void addSite(final String xmlFile) throws JAXBException,
+            SAXException, SQLException, ClientProtocolException,
+            ParseException, IOException, NumberFormatException {
+        TrialRegistrationConfirmation rConf = register("/integration_register_complete_minimal_dataset.xml");
+        makeAbbreviated(rConf);
+        ParticipatingSite upd = readParticipatingSiteFromFile(xmlFile);
         HttpResponse response = addSite("pa", rConf.getPaTrialID() + "", upd);
         assertEquals(200, getReponseCode(response));
         assertEquals(TEXT_PLAIN, getResponseContentType(response));
@@ -68,7 +121,9 @@ public final class ParticipatingSiteMgmtServiceTest extends
     }
 
     private void verifySite(TrialRegistrationConfirmation rConf, long siteID,
-            ParticipatingSite ps) {
+            ParticipatingSite ps) throws SQLException {
+
+        assertEquals(getLastParticipatingSiteId().toString(), siteID + "");
         clickAndWait("link=Participating Sites");
         clickAndWait("xpath=//table[@id='row']/tbody/tr[1]//td[7]//img");
 
@@ -108,13 +163,25 @@ public final class ParticipatingSiteMgmtServiceTest extends
         assertEquals(
                 ps.getInvestigator().get(0).getRole(),
                 selenium.getText("xpath=//div[@id='saveAndShowContacts']//table[@id='row']//tr[1]//td[4]"));
+
+        selenium.click("link=Contact");
         if (ps.getInvestigator().get(0).isPrimaryContact()) {
-            selenium.click("link=Contact");
             assertEquals(ps.getInvestigator().get(0).getPerson()
                     .getExistingPerson().getPoID().toString(),
                     selenium.getValue("id=personContactWebDTO.selectedPersId"));
+        } else {
+            assertEquals("",
+                    selenium.getValue("id=personContactWebDTO.selectedPersId"));
         }
 
+    }
+
+    private Number getLastParticipatingSiteId() throws SQLException {
+        QueryRunner runner = new QueryRunner();
+        return (Number) runner
+                .query(connection,
+                        "select identifier from study_site order by identifier desc limit 1",
+                        new ArrayHandler())[0];
     }
 
     @SuppressWarnings("unchecked")
