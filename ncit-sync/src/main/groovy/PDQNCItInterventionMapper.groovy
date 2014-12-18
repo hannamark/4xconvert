9bd6c058768298dba2fa7920ee9615bc592d6253
khandalkara <khandalkara@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2014-12-18 15:37:33 +0000

    Added new ncit-sync project
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@17697 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ ncit-sync/src/main/groovy/PDQNCItInterventionMapper.groovy
@@ -0,0 +1,150 @@
+import groovy.sql.Sql;
+import groovyx.net.http.RESTClient
+
+
+public class PDQNCItInterventionMapper{
+
+  def restClient = new RESTClient()
+  def lexEVSRestClient = new RESTClient()
+  
+  StringBuffer fileContents = new StringBuffer("BEGIN;");
+
+  def getInvSynonyms(String ncitCode , url) {
+      String inverVentionSyncUrl = url+"?query=Entity[@_entityCode=${ncitCode}]";
+      def response =null
+      try{
+      response = lexEVSRestClient.get(uri: inverVentionSyncUrl)
+      }
+      catch(Exception e) {
+          return
+      } 
+    if (! response.success || response.status != 200) {
+      
+      throw new RuntimeException("Failure from LexEVS: " + response.data.text)
+    }
+
+    def doc = response.data;
+    def entity = doc.queryResponse.'class'.find{ it.@name.text() == "org.LexGrid.concepts.Entity" };
+
+    // get name
+    String prefName;
+    def synonyms = ["Chemical structure name":[],"Lexical variant":[],"IND code":[],"Subtype":[],"Broader":[],"Code name":[],"Foreign brand name":[],"Common usage":[],"Spanish":[],"Obsolete name":[],"Abbreviation":[],"US brand name":[],"Acronym":[],"NSC number":[],"Synonym" :[],"CAS Registry name":[]]
+
+    def presentations = entity.field.find{ it.@name.text() == "_presentationList" }.'class';
+
+    for (def presentation : presentations) {
+      boolean isPreferred = presentation.field.find{ it.@name.text() == "_isPreferred" }.text() == "true";
+      def name =  presentation.field.find{ it.@name.text() == "_value" }.'class'.field.find{ it.@name.text() == "_content" && it.text() }.text();
+      if (isPreferred){
+        prefName = name
+      }else {
+        def repForm = presentation.field.find{ it.@name.text() == "_representationalForm" }.text()
+        
+        switch (repForm){
+           
+          case 'SY' :   synonyms["Synonym"] += name; break;
+          case 'AB' :   synonyms["Abbreviation"] += name; break;
+          case 'BR' :   synonyms["US brand name"] += name; break;
+          case 'FB' :   synonyms["Foreign brand name"] += name; break;
+          case 'NY' :   synonyms["Chemical structure name"] += name; break;
+          case 'CN' :   synonyms["Code name"] += name; break;
+        }
+      }
+    }
+
+    def properties = entity.field.find{ it.@name.text() == "_propertyList" }.'class';
+
+    for (def property : properties) {
+      def value =  property.field.find{ it.@name.text() == "_value" }.'class'.field.find{ it.@name.text() == "_content" && it.text() }.text();
+      def propName = property.field.find{ it.@name.text() == "_propertyName" }.text()
+      switch (propName) {
+        case 'NSC_Code' : synonyms["NSC number"] += value; break
+        case 'CAS_Registry' : synonyms["CAS Registry name"] += value; break
+      }
+    }
+    return [prefName, synonyms]
+  }
+
+  def getPreferredName(def ncitCode,String url){
+      
+    String preferredNameUrl = url +"/${ncitCode}?format=xml"
+    def response = null
+    try{
+        response = restClient.get(uri: preferredNameUrl)
+    } catch(Exception e) {
+          return
+      }    
+
+    if (! response.success || response.status != 200) {
+      //            println "fetchChildren - failed to fetch children for ${ncitCode} from ${url} with error ${response.data.text}"
+      throw new RuntimeException("Failure from LexEVS: " + response.data.text)
+    }
+    def doc = response.data;
+    def prefName =  doc.EntityDescription.namedEntity.designation.find{ it.@designationRole == 'PREFERRED' }?.text()
+    return prefName;
+  }
+
+  def generateInvUpdateSQL( def ncitCode, def prefName){
+   fileContents.append(" update intervention  set name='${prefName.replaceAll('\'','\'\'')}' where nt_term_identifier='${ncitCode}';");
+  }
+
+  def generateInvSynUpdateSQL(def ncitCode, def synonyms){
+    synonyms.each (){ code, vals ->
+       
+      if(vals){
+        fileContents.append(" delete from intervention_alternate_name where intervention_identifier = (select min(identifier) from intervention where nt_term_identifier='${ncitCode}') and name_type_code='${code}';");
+        vals.each { val -> fileContents.append(" insert into intervention_alternate_name (intervention_identifier, name,status_code, status_date_range_low, ");
+          fileContents.append(" date_last_created, name_type_code) values ((select min(identifier) from intervention where nt_term_identifier='${ncitCode}'), ");
+          fileContents.append(" '${val.replaceAll('\'','\'\'')}','ACTIVE',now(),now(),'${code}' );")
+          }
+      }
+    }
+  }
+
+  def generateInvRemoveDupSQL(def remove, def replace){
+    println "update planned_activity set intervention_identifier = ${replace} where intervention_identifier=${remove};"
+    println "update performed_activity set intervention_identifier = ${replace} where intervention_identifier=${remove};"
+    println "delete from intervention_alternate_name where intervention_identifier=${remove};"
+    println "delete from intervention where identifier=${remove};"
+  }
+
+  public void syncIntervention(String outputDir, String preferredNameUrl, String interventionUrl ,
+         String paJdbcUrl,String dbuser,String dbpassword){
+         
+         String ncitCode =null;
+         def  sql = Sql.newInstance(paJdbcUrl, dbuser, dbpassword, "org.postgresql.Driver")
+         def ctrpTerms = sql.rows("select distinct(nt_term_identifier) from intervention");
+         
+        ctrpTerms.each (){
+             
+         ncitCode= it.nt_term_identifier;
+         
+         if(ncitCode!=null) {
+            println "Syncing intervention term "+ncitCode
+            String  prefName = getPreferredName(ncitCode,preferredNameUrl)
+            if(prefName!=null) {
+                generateInvUpdateSQL(ncitCode,prefName)
+            }
+           
+            // Get Intervention synonyms
+            def syns
+            syns  = getInvSynonyms(ncitCode ,interventionUrl)
+            generateInvSynUpdateSQL(ncitCode,syns[1])
+             }
+         }
+         def outputFile = new File(outputDir)
+         File sqlFile = new File(outputFile, "interventionQueries.sql")
+         if(sqlFile.exists()) {
+             sqlFile.delete();
+         }
+         def out = new FileOutputStream(sqlFile)
+         def writer = new OutputStreamWriter( out , "UTF-8")
+         fileContents.append("COMMIT;")
+         writer.write fileContents.toString()
+         writer.flush();
+         writer.close();
+         
+         sql.executeUpdate(fileContents.toString());
+    
+  }
+}
---
+++ ncit-sync/src/main/groovy/SyncDiseasesFromNCIt.groovy
@@ -0,0 +1,242 @@
+import groovy.sql.Sql;
+import groovyx.net.http.RESTClient
+
+public class SyncDiseasesFromNCIt{
+
+  def restClient = new RESTClient()
+  
+  StringBuffer fileContents = new StringBuffer("BEGIN;");
+
+  TreeSet<String> parentsExtracted = new TreeSet<String>();
+  TreeSet<String> childrenExtracted = new TreeSet<String>();
+
+  def sql;
+
+
+ 
+  def getTermNames(def ncitCode, url){
+    def lexEVSRESTDetailsUrl = url+"/${ncitCode}?format=xml"
+   def response = null;
+   try{
+        response = restClient.get(uri: lexEVSRESTDetailsUrl)
+   }
+   catch(Exception e){
+        return null;
+   } 
+   
+    
+    if (! response.success || response.status != 200) {
+      println "-- fetchChildren - failed to fetch children for ${ncitCode} from ${url} with error ${response.data.text}"
+      throw new RuntimeException("Failure from LexEVS: " + response.data.text)
+    }
+
+    def synonyms = []
+    def doc = response.data;
+    def prefName =  doc.EntityDescription.namedEntity.designation.find{ it.@designationRole == 'PREFERRED' }?.text().replaceAll("'","''")
+    def altDesignations = doc.EntityDescription.namedEntity.designation.findAll{ it.@designationRole == 'ALTERNATIVE' }
+    def synName
+    for (def altDesignation : altDesignations) {
+      synName = altDesignation.text().replaceAll("'","''")
+      if(!synonyms.contains(synName)){
+        synonyms += synName
+      }
+    }
+    return [prefName, synonyms]
+  }
+
+
+  def generateDiseaseSynUpdateSQL(def ncitCode, def synonyms){
+       String idQuery ="select identifier from pdq_disease where nt_term_identifier='${ncitCode}' limit 1"
+       
+  
+    fileContents.append(" DELETE FROM pdq_disease_altername where disease_identifier =("+idQuery+");")
+    synonyms.each{ syn ->
+        fileContents.append(" INSERT INTO pdq_disease_altername ( ");
+         fileContents.append("disease_identifier, alternate_name,status_code, status_date_range_low, date_last_created)");
+          fileContents.append(" values (("+idQuery+"),'${syn}','ACTIVE',now(),now() );")  }
+    
+  }
+
+  /**
+   * Extract and persist the parent tree for a term
+   */
+  def extractParentTree(def ncitCode , url) {
+   
+    def lexEVSRESTParentsUrl = url +"/${ncitCode}?format=xml"
+    def parents = []
+    def response = null
+    try{
+        response = restClient.get(uri: lexEVSRESTParentsUrl)
+    } catch(Exception e){
+        return null;
+   }  
+
+    if (! response.success || response.status != 200) {
+      println "-- fetchChildren - failed to fetch parents for ${ncitCode} from ${url} with error ${response.data.text}"
+      throw new RuntimeException("Failure from LexEVS: " + response.data.text)
+    }
+    
+    //delete existing parent
+    StringBuffer  deleteExistingParentQuery =new StringBuffer();
+    deleteExistingParentQuery.append (" delete from pdq_disease_parent where identifier in ( ");
+    deleteExistingParentQuery.append (" select identifier from pdq_disease_parent where disease_identifier = ");
+    deleteExistingParentQuery.append (" (select identifier from pdq_disease where nt_term_identifier='"+ncitCode+"' limit 1));");
+    fileContents.append(deleteExistingParentQuery.toString());
+    def doc = response.data;
+    def parentCodes = doc.EntityDescription.namedEntity.parent.name.findAll{ it.text().startsWith('C') }
+    for (def parentCode : parentCodes) {
+      if(!parents.contains(parentCode.text())){
+        parents += parentCode.text()
+      }
+    }
+
+    parentsExtracted.add(ncitCode)
+    println "-- Parents of ${ncitCode}: ${parents}"
+    parents.each { p ->
+      insertOrUpdateTerm(p , url)
+      insertParentChild(p,ncitCode)
+      if(! parentsExtracted.contains(p)) {
+        extractParentTree(p , url)
+      }
+    }
+
+  }
+
+  def insertParentChild( parent,  child) {
+    
+     String query = "select identifier from  pdq_disease where nt_term_identifier = '"+child+"' limit 1";
+     String parentQuery ="select  identifier from  pdq_disease where nt_term_identifier = '"+parent+"' limit 1";
+     String pdqParentquery = "select identifier from pdq_disease_parent where "+
+     "disease_identifier=("+query+") and parent_disease_identifier=("+parentQuery+")";
+  
+    
+      def insertParentSql = "INSERT INTO pdq_disease_parent(disease_identifier, parent_disease_identifier, parent_disease_code, status_code, status_date_range_low)"+
+          " VALUES (("+query+"),("+parentQuery+"), 'ISA', 'ACTIVE',now());"
+      fileContents.append(insertParentSql+";");
+  
+  }
+
+
+  /**
+   * Extract and persist a term
+   */
+  def insertOrUpdateTerm (def term, String url) {
+    println "-- Extracting ${term}"
+    def termNames = getTermNames(term, url)
+    if(termNames!=null) {
+        def prefName =   termNames[0]
+        def sqlInsertUpdate
+        if (sql.rows("select identifier from pdq_disease where nt_term_identifier = '"+term+"'").size() == 0) { // If term does not exist, insert it, else update
+         sqlInsertUpdate = "INSERT INTO pdq_disease(nt_term_identifier, preferred_name, menu_display_name, status_code, status_date_range_low)"+
+          " VALUES ('"+term+"','"+prefName+"','"+ prefName+"', 'ACTIVE', now());"
+        }else {
+        sqlInsertUpdate = " UPDATE pdq_disease set preferred_name='"+prefName+"',menu_display_name='"+prefName+"', date_last_updated=now() where nt_term_identifier='"+term+"';"
+       }
+         fileContents.append(sqlInsertUpdate);
+         // Update synonyms
+        generateDiseaseSynUpdateSQL(term, termNames[1])
+    }
+    
+   
+  }
+
+  /**
+   * Extract and persist the child tree for the term
+   */
+  def extractChildTree(def ncitCode, url) {
+    def lexEVSRESTChildrenUrl = url +"/${ncitCode}/children?maxtoreturn=1000"
+    def response = null
+    try{
+        response = restClient.get(uri: lexEVSRESTChildrenUrl)
+    }
+    catch(Exception e) {
+        return
+    }  
+
+    if (! response.success || response.status != 200) {
+      println "-- fetchChildren - failed to fetch children for ${ncitCode} from ${url} with error ${response.data.text}"
+      throw new RuntimeException("Failure from LexEVS: " + response.data.text)
+    }
+    
+    StringBuffer deleteExistingChildsQuery  = new StringBuffer();
+    deleteExistingChildsQuery.append (" delete from pdq_disease_parent where identifier in ( ");
+    deleteExistingChildsQuery.append (" select identifier from pdq_disease_parent where parent_disease_identifier = ");
+    deleteExistingChildsQuery.append (" (select identifier from pdq_disease where nt_term_identifier='"+ncitCode+"' limit 1));");
+    fileContents.append(deleteExistingChildsQuery.toString());
+
+    def children = []
+    def doc = response.data;
+    def childName
+    def childCodes = doc.entry.name.name.findAll{ it.text().startsWith('C')  }
+    for (def childCode : childCodes) {
+      if(!children.contains(childCode.text())){
+        children += childCode.text()
+      }
+    }
+
+    
+    
+    childrenExtracted.add(ncitCode)
+    println "-- Children of ${ncitCode}: ${children}"
+    children.each { c ->
+       
+      insertOrUpdateTerm(c , url)
+      insertParentChild(ncitCode, c)
+      if(! childrenExtracted.contains(c)) {
+        extractChildTree(c , url)
+      }
+    }
+  }
+
+  /**
+   * Retrieve parent tree and child tree for all CTRP disease terms indexed on trials
+   */
+  def syncDiseaseTerms( String paJdbcUrl ,String outputDir, String url, dbuser, dbpassword) {
+      def resolvedProperties = [:]
+      
+      sql = Sql.newInstance(paJdbcUrl, dbuser, dbpassword, "org.postgresql.Driver")
+     
+      
+    def ctrpTerms = sql.rows("select distinct nt_term_identifier from pdq_disease, study_disease where study_disease.disease_identifier = pdq_disease.identifier and nt_term_identifier is not null order by nt_term_identifier")
+    println "-- Syncing ${ctrpTerms.size()} CTRP Disease terms from NCIt..."
+  
+    String ncitTerm =null;
+   
+   
+    ctrpTerms.each (){
+        
+        
+    ncitTerm= it.nt_term_identifier;
+    //ncitTerm="C26871"
+    
+     println "-- SYNCING disease term "+ncitTerm
+     insertOrUpdateTerm(ncitTerm, url)
+     println "-- Syncing parents of CTRP term "+ncitTerm
+      // Retrieve parent and children tree for the term
+      extractParentTree(ncitTerm , url)
+     println "-- Syncing children of CTRP term "+ncitTerm
+      extractChildTree(ncitTerm , url)
+      
+    
+      
+    }
+    
+
+    
+    def outputFile = new File(outputDir)
+    File sqlFile = new File(outputFile, "queries.sql")
+    if(sqlFile.exists()) {
+        sqlFile.delete();
+    }
+    def out = new FileOutputStream(sqlFile)
+    def writer = new OutputStreamWriter( out , "UTF-8")
+    fileContents.append(";COMMIT;")
+    writer.write fileContents.toString()
+    writer.flush();
+    writer.close();
+   sql.executeUpdate(fileContents.toString());
+    
+  }
+
+ 
+}
