47f2294b69018830897363422452c66527b65ed9
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-03-24 21:51:07 +0000

    AllPerformanceTests.java
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@18216 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ reg-web/test-integration/java/gov/nih/nci/registry/test/performance/AllPerformanceTests.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.registry.test.performance;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * 
+ * Class to control the order that selenium tests are run in.
+ * 
+ * @author Abraham J. Evans-EL <aevansel@5amsolutions.com>
+ */
+@RunWith(Suite.class)
+@SuiteClasses(value = {})
+public class AllPerformanceTests {
+
+}
