7df6ad2e0ac2d9ee9545933178f6b5721197f12e
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-04-28 21:42:26 +0000

    PO-8263 Review trial search performance: added additional indexes to study_milestone.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@18354 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/dbscripts/db-upgrade/postgresql/4.3/PO-8732.sql
+++ pa/dbscripts/db-upgrade/postgresql/4.3/PO-8732.sql
@@ -9,6 +9,105 @@ create index sm_milestone_code on study_milestone (milestone_code);
 
 DROP INDEX IF EXISTS sm_milestone_date;
 create index sm_milestone_date on study_milestone (milestone_date);
+   
+DROP INDEX IF EXISTS sm_rv_admin_milestone01;
+create index sm_rv_admin_milestone01 on study_milestone (study_protocol_identifier, milestone_code) WHERE (
+               (
+                  (
+                     milestone_code
+                  )
+                  ::text = ANY
+                  (
+                     ARRAY[('ADMINISTRATIVE_PROCESSING_START_DATE'::character varying)::text,
+                     ('ADMINISTRATIVE_PROCESSING_COMPLETED_DATE'::character varying)::text,
+                     ('ADMINISTRATIVE_READY_FOR_QC'::character varying)::text,
+                     ('ADMINISTRATIVE_QC_START'::character varying)::text,
+                     ('ADMINISTRATIVE_QC_COMPLETE'::character varying)::text]
+                  )
+               )
+               OR
+               (
+                  (
+                     milestone_code
+                  )
+                  ::text = ANY
+                  (
+                     ARRAY[('READY_FOR_TSR'::character varying)::text,
+                     ('TRIAL_SUMMARY_REPORT'::character varying)::text,
+                     ('INITIAL_ABSTRACTION_VERIFY'::character varying)::text,
+                     ('TRIAL_SUMMARY_FEEDBACK'::character varying)::text,
+                     ('ONGOING_ABSTRACTION_VERIFICATION'::character varying)::text,
+                     ('LATE_REJECTION_DATE'::character varying)::text]
+                  )
+               )
+            );
+            
+DROP INDEX IF EXISTS sm_rv_admin_milestone02;
+create index sm_rv_admin_milestone02 on study_milestone (milestone_code) WHERE (
+      (
+         milestone_code
+      )
+      ::text = ANY
+      (
+         ARRAY[('ADMINISTRATIVE_PROCESSING_START_DATE'::character varying)::text,
+         ('ADMINISTRATIVE_PROCESSING_COMPLETED_DATE'::character varying)::text,
+         ('ADMINISTRATIVE_READY_FOR_QC'::character varying)::text,
+         ('ADMINISTRATIVE_QC_START'::character varying)::text,
+         ('ADMINISTRATIVE_QC_COMPLETE'::character varying)::text]
+      )
+   );
+   
+
+     
+DROP INDEX IF EXISTS sm_rv_scientific_milestone01;
+create index sm_rv_scientific_milestone01 on study_milestone (study_protocol_identifier,milestone_code) WHERE (
+               (
+                  (
+                     milestone_code
+                  )
+                  ::text = ANY
+                  (
+                     ARRAY[('SCIENTIFIC_PROCESSING_START_DATE'::character varying)::text,
+                     ('SCIENTIFIC_PROCESSING_COMPLETED_DATE'::character varying)::text,
+                     ('SCIENTIFIC_READY_FOR_QC'::character varying)::text,
+                     ('SCIENTIFIC_QC_START'::character varying)::text,
+                     ('SCIENTIFIC_QC_COMPLETE'::character varying)::text]
+                  )
+               )
+               OR
+               (
+                  (
+                     milestone_code
+                  )
+                  ::text = ANY
+                  (
+                     ARRAY[('READY_FOR_TSR'::character varying)::text,
+                     ('TRIAL_SUMMARY_REPORT'::character varying)::text,
+                     ('INITIAL_ABSTRACTION_VERIFY'::character varying)::text,
+                     ('TRIAL_SUMMARY_FEEDBACK'::character varying)::text,
+                     ('ONGOING_ABSTRACTION_VERIFICATION'::character varying)::text,
+                     ('LATE_REJECTION_DATE'::character varying)::text]
+                  )
+               )
+            );
+            
+DROP INDEX IF EXISTS sm_rv_scientific_milestone02;
+create index sm_rv_scientific_milestone02 on study_milestone (milestone_code) WHERE (
+      (
+         milestone_code
+      )
+      ::text = ANY
+      (
+         ARRAY[('SCIENTIFIC_PROCESSING_START_DATE'::character varying)::text,
+         ('SCIENTIFIC_PROCESSING_COMPLETED_DATE'::character varying)::text,
+         ('SCIENTIFIC_READY_FOR_QC'::character varying)::text,
+         ('SCIENTIFIC_QC_START'::character varying)::text,
+         ('SCIENTIFIC_QC_COMPLETE'::character varying)::text]
+      )
+   ); 
+
+
+vacuum analyze study_milestone;
 
 
 
