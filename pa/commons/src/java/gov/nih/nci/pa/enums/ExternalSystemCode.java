3fbed3079625f0c063e5a960fd5ef0195d9c1596
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-06-30 13:05:33 +0000

    PO-9045 Design and implement an approach to store passwords in the PA database securely: done.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@18620 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/commons/src/java/gov/nih/nci/pa/enums/ExternalSystemCode.java
@@ -0,0 +1,55 @@
+package gov.nih.nci.pa.enums;
+
+import static gov.nih.nci.pa.enums.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.pa.enums.CodedEnumHelper.register;
+import static gov.nih.nci.pa.enums.EnumHelper.sentenceCasedName;
+
+/**
+ * @author dkrylov
+ */
+public enum ExternalSystemCode implements CodedEnum<String> {
+
+    /**
+     * TWITTER
+     */
+    TWITTER("Twitter"),
+    /**
+     * Mailbox
+     */
+    MAILBOX("Mailbox");
+
+    private final String code;
+
+    private ExternalSystemCode(String code) {
+        this.code = code;
+        register(this);
+    }
+
+    /**
+     * @param code
+     *            code
+     * @return CheckOutType with the given code
+     */
+    public static ExternalSystemCode getByCode(String code) {
+        return getByClassAndCode(ExternalSystemCode.class, code);
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return sentenceCasedName(this);
+    }
+
+    /**
+     * Gets the name of the enum.
+     * 
+     * @return the name of the enum
+     */
+    public String getName() {
+        return name();
+    }
+}
