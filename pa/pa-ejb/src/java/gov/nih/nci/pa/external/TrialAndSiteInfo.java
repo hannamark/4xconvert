9984d471f67c1a88b073a779020771d2ada2f080
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-07-20 14:07:46 +0000

    Disabling PMD/Checkstyle checks for "external" package.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@18685 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-ejb/src/java/gov/nih/nci/pa/external/TrialAndSiteInfo.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/external/TrialAndSiteInfo.java
@@ -12,7 +12,7 @@ import java.util.Set;
  * @author vinodh
  *
  */
-public class TrialAndSiteInfo {
+public class TrialAndSiteInfo { // NOPMD
     
     
     /**
@@ -24,26 +24,26 @@ public class TrialAndSiteInfo {
      */
     private static final String YES = "Yes";
     
-    public static String NCI_ID = "nci_id";
-    public static String NCT_ID = "nct_id";
-    public static String IS_RSS_TRIAL = "is_rss_trial";
-    public static String TRIAL_TYPE = "trial_type";
-    public static String LEAD_ORG_FAM = "lead_org_family";
-    public static String LEAD_ORG = "lead_org";
-    public static String SUB_ORG = "submitting_org";
-    public static String SUB_NM = "submitter_name";
-    public static String SUM_4_FUND_CAT = "summary_4_funding_category";
-    public static String CTEP_ID = "ctep_id";
-    public static String DCP_ID = "dcp_id";
-    public static String SP_ID = "study_protocol_identifier";
-    public static String PROC_STS = "study_processing_status";
-    public static String PROC_STS_DT = "study_processing_status_date";
+    public static String NCI_ID = "nci_id"; // NOPMD
+    public static String NCT_ID = "nct_id"; // NOPMD
+    public static String IS_RSS_TRIAL = "is_rss_trial"; // NOPMD
+    public static String TRIAL_TYPE = "trial_type"; // NOPMD
+    public static String LEAD_ORG_FAM = "lead_org_family"; // NOPMD
+    public static String LEAD_ORG = "lead_org"; // NOPMD
+    public static String SUB_ORG = "submitting_org"; // NOPMD
+    public static String SUB_NM = "submitter_name"; // NOPMD
+    public static String SUM_4_FUND_CAT = "summary_4_funding_category"; // NOPMD
+    public static String CTEP_ID = "ctep_id";// NOPMD
+    public static String DCP_ID = "dcp_id";// NOPMD
+    public static String SP_ID = "study_protocol_identifier";// NOPMD
+    public static String PROC_STS = "study_processing_status";// NOPMD
+    public static String PROC_STS_DT = "study_processing_status_date";// NOPMD
     
-    public static String STUDY_STS = "most_recent_study_status";
-    public static String STUDY_STS_DT = "most_recent_study_status_date";
-    public static String PS_ID = "participating_site_identifier";
-    public static String PS_ORG_FAM = "participating_site_org_family";
-    public static String PS_ORG_NM = "participating_site_org_name";
+    public static String STUDY_STS = "most_recent_study_status";// NOPMD
+    public static String STUDY_STS_DT = "most_recent_study_status_date";// NOPMD
+    public static String PS_ID = "participating_site_identifier";// NOPMD
+    public static String PS_ORG_FAM = "participating_site_org_family";// NOPMD
+    public static String PS_ORG_NM = "participating_site_org_name";// NOPMD
     
     public Map<String, Object> infoMap;
     
@@ -81,7 +81,7 @@ public class TrialAndSiteInfo {
 
     public void map(ResultSet rs) throws Exception {
         if (rs == null) {
-            throw new Exception("Error encountered: ResultSet is Null");
+            throw new Exception("Error encountered: ResultSet is Null"); // NOPMD
         }
         Set<String> cols = this.infoMap.keySet();
         for (String col : cols) {
