a53b5a8fb412249816d05edd8133aaffbcca8487
kogantir <kogantir@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-07-24 14:39:26 +0000

    COPPA-1679  PO-8331 Provide the ability to classify an 'Industrial/Other' trial as 'Institutional'
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@18705 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/test-integration/java/gov/nih/nci/pa/test/integration/AllSeleniumTests.java
+++ pa/pa-web/test-integration/java/gov/nih/nci/pa/test/integration/AllSeleniumTests.java
@@ -103,7 +103,7 @@ import org.junit.runners.Suite.SuiteClasses;
         TrialSearchTest.class, ManageTermsTest.class, PlannedMarkerTest.class,
         ManageFlaggedTrialsTest.class, WebAppSecurityTest.class,TrialOnHoldTest.class , ResultsReportingDocumentTest.class,
         UnRejectTrialTest.class , ResultsReportingCoversheetTest.class,
-        ResultsDashboardTest.class ,TrialViewTest.class
+        ResultsDashboardTest.class ,TrialViewTest.class, NCISpecificInformationTest.class
 // DuplicateTrialEditTest.class,
 // LookupWithApostropheTest.class,
 // DiseaseTest.class,
---
+++ pa/pa-web/test-integration/java/gov/nih/nci/pa/test/integration/NCISpecificInformationTest.java
@@ -0,0 +1,44 @@
+package gov.nih.nci.pa.test.integration;
+
+import gov.nih.nci.pa.test.integration.AbstractPaSeleniumTest.TrialInfo;
+
+import java.sql.SQLException;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayHandler;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Reshma Koganti
+ *
+ */
+public class NCISpecificInformationTest extends AbstractPaSeleniumTest {
+     @Test
+     public void testConsortiaTrialCategoryCodeValues() throws SQLException {
+         loginAsSuperAbstractor();
+          TrialInfo info = createAcceptedTrial(true);
+          logoutUser();
+          selectTrialInPA(info);
+          assertTrue(selenium.isElementPresent("link=NCI Specific Information"));
+          clickAndWait("link=NCI Specific Information");
+          assertTrue(selenium.isElementPresent("id=nciSpecificInformationWebDTO.consortiaTrialCategoryCode"));
+          selenium.select("id=nciSpecificInformationWebDTO.consortiaTrialCategoryCode","label=No - Institutional");
+          clickAndWait("link=Save");
+          assertTrue(selenium.isTextPresent("Message. Record Updated."));
+          String valueCode = getConsortiaTrialCategory(info);
+          assertTrue(StringUtils.equalsIgnoreCase(valueCode, "INSTITUTIONAL"));
+     }
+     
+     private String getConsortiaTrialCategory(TrialInfo trial)
+             throws SQLException {
+         QueryRunner runner = new QueryRunner();
+         return (String) runner
+                 .query(connection,
+                         "select consortia_trial_category from study_protocol "
+                 + "where identifier="
+                 + (trial.id != null ? trial.id : getTrialIdByNciId(trial.nciID)),
+                         new ArrayHandler())[0];
+     }
+}
