497c9b7ec7597e6b869ee0f45ca7dc5fe21b060e
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-10-06 14:26:35 +0000

    Fixing Selenium.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@19114 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-web/test-integration/java/gov/nih/nci/pa/test/performance/DashboardPerformanceTest.java
+++ pa/pa-web/test-integration/java/gov/nih/nci/pa/test/performance/DashboardPerformanceTest.java
@@ -36,13 +36,13 @@ public class DashboardPerformanceTest extends AbstractPaSeleniumTest {
     @Test
     public void testAdmin() {
         loginAsAdminAbstractor();
-        checkDashboardPerformance("Admin Dashboard", 10);
+        checkDashboardPerformance("Admin Dashboard", 11);
     }
 
     @Test
     public void testScientific() {
         loginAsScientificAbstractor();
-        checkDashboardPerformance("Scientific Dashboard", 10);
+        checkDashboardPerformance("Scientific Dashboard", 11);
     }
 
     private void checkDashboardPerformance(String descr, int timeoutSecond) {
