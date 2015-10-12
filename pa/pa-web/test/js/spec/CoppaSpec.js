69ddca2839c4e2d82ef6336f5a27077ef222c428
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2015-10-12 18:34:40 +0000

    PO-9357 Incorporate a Javascript unit testing framework, such as Jasmine, into CTRP build.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@19146 553898ca-32d5-487a-af8b-6cf4ca579ab4

---
+++ pa/pa-web/test/js/spec/CoppaSpec.js
@@ -0,0 +1,89 @@
+describe(
+		"coppa.js",
+		function() {
+
+			beforeEach(function() {
+
+			});
+
+			describe(
+					"Wait indicators",
+					function() {
+						it(
+								"should display wait panel",
+								function() {
+									displayWaitPanel();
+									expect(
+											document
+													.getElementById('progress_indicator_panel').style.display)
+											.toEqual("");
+								});
+						it(
+								"should hide wait panel",
+								function() {
+									hideWaitPanel();
+									expect(
+											document
+													.getElementById('progress_indicator_panel').style.display)
+											.toEqual("none");
+								});
+					});
+
+			describe("Date validations", function() {
+				it("should detect valid dates", function() {
+					expect(isValidDate("01/01/1990")).toBe(true);
+					expect(isValidDate("01/31/2015")).toBe(true);
+					expect(isValidDate("02/29/2016")).toBe(true);
+				});
+				it("should detect invalid dates", function() {
+					expect(isValidDate("01/32/2015")).not.toBe(true);
+					expect(isValidDate("02/30/2015")).not.toBe(true);
+					expect(isValidDate("02/29/2015")).not.toBe(true);
+					expect(isValidDate("28/02/2015")).not.toBe(true);
+				});
+			});
+
+			describe(
+					"Phone numbers",
+					function() {
+						it(
+								"should extract extension properly",
+								function() {
+									expect(
+											extractPhoneNumberExt("555-555-5555ext784"))
+											.toEqual("784");
+									expect(
+											extractPhoneNumberExt("555-555-5555extn184"))
+											.toEqual("184");
+									expect(
+											extractPhoneNumberExt("555-5555555x184"))
+											.toEqual("184");
+									expect(
+											extractPhoneNumberExt("5555555555X184"))
+											.toEqual("184");
+									expect(
+											extractPhoneNumberExt("555-555-5555"))
+											.toEqual("");
+								});
+						it(
+								"should remove extension from a phone number properly",
+								function() {
+									expect(
+											extractPhoneNumberNoExt("555-555-5555ext784"))
+											.toEqual("555-555-5555");
+									expect(
+											extractPhoneNumberNoExt("555-555-5555extn184"))
+											.toEqual("555-555-5555");
+									expect(
+											extractPhoneNumberNoExt("555-5555555x184"))
+											.toEqual("555-5555555");
+									expect(
+											extractPhoneNumberNoExt("5555555555X184"))
+											.toEqual("5555555555");
+									expect(
+											extractPhoneNumberNoExt("555-555-5555"))
+											.toEqual("555-555-5555");
+								});
+
+					});
+		});
