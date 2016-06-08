9a66418ce320b1b1d91f64f2a7b3c55504886963
krylovd <krylovd@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2016-06-08 18:20:21 +0000

    Removed gov.nih.nci.pa.webservices.test.integration.RegisterCompleteTrialTest.testRegisterRotateEnumValues() as it takes a long time to execute with questionable usefulness. It will need to be probably moved to unit tests.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@19808 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/pa-webservices/test-integration/java/gov/nih/nci/pa/webservices/test/integration/RegisterCompleteTrialTest.java
+++ pa/pa-webservices/test-integration/java/gov/nih/nci/pa/webservices/test/integration/RegisterCompleteTrialTest.java
@@ -1,31 +1,16 @@
 package gov.nih.nci.pa.webservices.test.integration;
 
-import gov.nih.nci.pa.enums.StudyStatusCode;
-import gov.nih.nci.pa.webservices.types.AccrualDiseaseTerminology;
 import gov.nih.nci.pa.webservices.types.BaseTrialInformation;
 import gov.nih.nci.pa.webservices.types.CompleteTrialRegistration;
-import gov.nih.nci.pa.webservices.types.ExpandedAccessType;
-import gov.nih.nci.pa.webservices.types.GrantorCode;
-import gov.nih.nci.pa.webservices.types.HolderType;
-import gov.nih.nci.pa.webservices.types.NciDivisionProgramCode;
-import gov.nih.nci.pa.webservices.types.NihInstitutionCode;
-import gov.nih.nci.pa.webservices.types.PrimaryPurpose;
-import gov.nih.nci.pa.webservices.types.SecondaryPurpose;
-import gov.nih.nci.pa.webservices.types.StudyModelCode;
-import gov.nih.nci.pa.webservices.types.TimePerspectiveCode;
-import gov.nih.nci.pa.webservices.types.TrialCategory;
 import gov.nih.nci.pa.webservices.types.TrialRegistrationConfirmation;
-import gov.nih.nci.pa.webservices.types.TrialStatusCode;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.GregorianCalendar;
 import java.util.Iterator;
 
 import javax.xml.bind.JAXBException;
-import javax.xml.datatype.DatatypeFactory;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ArrayHandler;
@@ -237,162 +222,6 @@ public class RegisterCompleteTrialTest extends AbstractRestServiceTest {
         super.verifyTrialStatus(reg, conf); // To change body of overridden
                                             // methods use File | Settings |
                                             // File Templates.
-    }
-
-    @Test
-    public void testRegisterRotateEnumValues() throws Exception {
-
-        for (TrialCategory v : TrialCategory.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.setCategory(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyNCISpecificInformation(reg, conf);
-        }
-
-        for (PrimaryPurpose v : PrimaryPurpose.values()) {
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            reg.setPrimaryPurpose(v);
-            reg.setPrimaryPurposeOtherDescription(v != PrimaryPurpose.OTHER ? null
-                    : "PrimaryPurpose.OTHER");
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyTrialDesign(reg, conf);
-        }
-
-        for (SecondaryPurpose v : SecondaryPurpose.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getInterventionalDesign().setSecondaryPurpose(v);
-            reg.getInterventionalDesign().setSecondaryPurposeOtherDescription(
-                    v != SecondaryPurpose.OTHER ? null
-                            : "SecondaryPurpose.OTHER");
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyTrialDesign(reg, conf);
-        }
-
-        for (AccrualDiseaseTerminology v : AccrualDiseaseTerminology.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.setAccrualDiseaseTerminology(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyDiseaseTerm(reg, conf);
-        }
-
-        for (StudyModelCode v : StudyModelCode.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_nonInt_success.xml");
-            reg.getNonInterventionalDesign().setStudyModelCode(v);
-            reg.getNonInterventionalDesign().setStudyModelCodeOtherDescription(
-                    v != StudyModelCode.OTHER ? null : "StudyModelCode.OTHER");
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyTrialDesign(reg, conf);
-        }
-
-        for (TimePerspectiveCode v : TimePerspectiveCode.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_nonInt_success.xml");
-            reg.getNonInterventionalDesign().setTimePerspectiveCode(v);
-            reg.getNonInterventionalDesign()
-                    .setTimePerspectiveCodeOtherDescription(
-                            v != TimePerspectiveCode.OTHER ? null
-                                    : "TimePerspectiveCode.OTHER");
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyTrialDesign(reg, conf);
-        }
-
-        for (TrialStatusCode v : TrialStatusCode.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.setTrialStatus(v);
-            if (StudyStatusCode.getByCode(v.value()).requiresReasonText()) {
-                reg.setWhyStopped("WhyStopped");
-            }
-            if (v.value().contains("Complete")) {
-                reg.getPrimaryCompletionDate()
-                        .getValue()
-                        .setValue(
-                                DatatypeFactory.newInstance()
-                                        .newXMLGregorianCalendar(
-                                                new GregorianCalendar()));
-                reg.getPrimaryCompletionDate().getValue().setType("Actual");
-                reg.getCompletionDate().setValue(
-                        DatatypeFactory.newInstance().newXMLGregorianCalendar(
-                                new GregorianCalendar()));
-                reg.getCompletionDate().setType("Actual");
-            }
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyTrialStatus(reg, conf);
-        }
-
-        for (HolderType v : HolderType.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            if (v == HolderType.NIH || v == HolderType.NCI) {
-                continue;
-            }
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getInd().get(0).setHolderType(v);
-            reg.getIde().get(0).setHolderType(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyIndIde(reg, conf);
-        }
-
-        for (GrantorCode v : new GrantorCode[] { GrantorCode.CBER,
-                GrantorCode.CDER }) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getInd().get(0).setGrantor(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyIndIde(reg, conf);
-        }
-
-        for (NihInstitutionCode v : NihInstitutionCode.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getInd().get(0).setNihInstitution(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyIndIde(reg, conf);
-        }
-
-        for (NciDivisionProgramCode v : NciDivisionProgramCode.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getIde().get(0).setNciDivisionProgramCode(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyIndIde(reg, conf);
-        }
-
-        for (ExpandedAccessType v : ExpandedAccessType.values()) {
-            LOG.info("Rotating " + v.getClass() + ": " + v.name());
-            CompleteTrialRegistration reg = readCompleteTrialRegistrationFromFile("/integration_register_complete_success.xml");
-            reg.getInd().get(0).setExpandedAccessType(v);
-            deactivateTrialByLeadOrgId(reg.getLeadOrgTrialID());
-            TrialRegistrationConfirmation conf = registerTrialFromJAXBElement(reg);
-            logInFindAndAcceptTrial(conf);
-            verifyIndIde(reg, conf);
-        }
     }
 
     @Test
