431a9d32f7c2634e83d53172e37e1f1804a914fd
josephb2 <josephb2@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2016-06-24 15:35:36 +0000

    PO-9858 - fixing tests
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@19846 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/AllSeleniumTests.java
+++ po/client/src/test/java/gov/nih/nci/coppa/test/integration/test/AllSeleniumTests.java
@@ -87,22 +87,21 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-//@SuiteClasses(value = { LoginTest.class, CreateOrganizationTest.class,
-//        CreatePersonTest.class, CreateResearchOrganizationTest.class,
-//        GenericOrganizationalContactTest.class, CurateOrganizationTest.class,
-//        CuratePersonTest.class, ManageClinicalResearchStaffWithCRTest.class,
-//        ManageHealthCareFacilityWithCRTest.class,
-//        ManageHealthCareProviderWithCRTest.class,
-//        ManageIdentifiedOrganizationWithCRTest.class,
-//        ManageIdentifiedPersonWithCRTest.class,
-//        ManageOrganizationalContactWithCRTest.class,
-//        ManageOversightCommitteeWithCRTest.class,
-//        ManageResearchOrganizationWithCRTest.class,
-//        OrganizationFamilyTest.class, OrganizationRelationshipTest.class,
-//        SearchOrganizationTest.class, SearchPersonTest.class,
-//        SelectOrganizationTest.class, UsePersonsAddressButtonTest.class,
-//        ManageUserGroupsTest.class })
-@SuiteClasses(value = { OrganizationRelationshipTest.class })
+@SuiteClasses(value = { LoginTest.class, CreateOrganizationTest.class,
+        CreatePersonTest.class, CreateResearchOrganizationTest.class,
+        GenericOrganizationalContactTest.class, CurateOrganizationTest.class,
+        CuratePersonTest.class, ManageClinicalResearchStaffWithCRTest.class,
+        ManageHealthCareFacilityWithCRTest.class,
+        ManageHealthCareProviderWithCRTest.class,
+        ManageIdentifiedOrganizationWithCRTest.class,
+        ManageIdentifiedPersonWithCRTest.class,
+        ManageOrganizationalContactWithCRTest.class,
+        ManageOversightCommitteeWithCRTest.class,
+        ManageResearchOrganizationWithCRTest.class,
+        OrganizationFamilyTest.class, OrganizationRelationshipTest.class,
+        SearchOrganizationTest.class, SearchPersonTest.class,
+        SelectOrganizationTest.class, UsePersonsAddressButtonTest.class,
+        ManageUserGroupsTest.class })
 public class AllSeleniumTests {
 
 }
