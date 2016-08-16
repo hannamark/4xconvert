32c98c4abe3a49acffd0770a2c43a76e3eaed3da
khandalkara <khandalkara@553898ca-32d5-487a-af8b-6cf4ca579ab4>
2016-08-16 19:07:17 +0000

    Adding back missing table required for DT4 table
    git-svn-id: https://ncisvn.nci.nih.gov/svn/coppa/trunk/code@19884 553898ca-32d5-487a-af8b-6cf4ca579ab4

--- pa/data-warehouse/groovy/family_trial_data.groovy
+++ pa/data-warehouse/groovy/family_trial_data.groovy
@@ -9,26 +9,8 @@ destinationConnection.execute("""INSERT INTO stg_dw_family_trial_data
                                  SELECT DISTINCT org_org_family, nci_id, NULL
                                  FROM stg_dw_study_participating_site
                                  WHERE org_org_family IS NOT NULL
-                                   AND COALESCE(program_code, '') != ''""");
+                                   """);
 
-destinationConnection.eachRow("""SELECT DISTINCT org_org_family, nci_id, program_code
-                                 FROM stg_dw_study_participating_site
-                                 WHERE org_org_family IS NOT NULL
-                                   AND COALESCE(program_code, '') != ''
-                                   ORDER BY program_code""") { row ->
-    destinationConnection.executeUpdate("""UPDATE stg_dw_family_trial_data
-                                     SET program_codes = program_codes || chr(13) || ?
-                                     WHERE family_name = ?
-                                       AND nci_id = ?
-                                       AND program_codes IS NOT NULL
-                                  """, [row.program_code, row.org_org_family, row.nci_id]);
-    destinationConnection.executeUpdate("""UPDATE stg_dw_family_trial_data
-                                     SET program_codes = ?
-                                     WHERE family_name = ?
-                                       AND nci_id = ?
-                                       AND program_codes IS NULL
-                                 """, [row.program_code, row.org_org_family, row.nci_id]);
-}
-								   
+					   
 								   
 								   destinationConnection.close()
